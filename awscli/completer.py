a7f05dc09348baff333858cb9b781c97a1713b21
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2012-12-23 22:29:13 +0530

    Remove unused import.

--- awscli/completer.py
+++ awscli/completer.py
@@ -11,7 +11,6 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-import os
 import sys
 import botocore.session
 import botocore.base

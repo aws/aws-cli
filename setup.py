cee3f9a3a97fc4a88bdb8895fe1111ecf5964b54
Mitch Garnaat <mitch@garnaat.com>
2012-12-26 06:28:16 -0800

    Fixing Trove classifier for license in setup.py file.  Fixes #11.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',

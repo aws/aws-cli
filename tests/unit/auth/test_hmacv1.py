c5a2a0190e07fa100ddc9e32246e180838e260f1
Mitch Garnaat <mitch@garnaat.com>
2013-01-25 14:25:15 -0800

    Fix a byte/string problem in a test.  Change IAM to a membered_list service.

--- tests/unit/auth/test_hmacv1.py
+++ tests/unit/auth/test_hmacv1.py
@@ -24,9 +24,10 @@
 import unittest
 import botocore.auth
 import botocore.credentials
+import six
 
 CS1 = """PUT\nc8fdb181845a4ca6b8fec737b3581d76\ntext/html\nThu, 17 Nov 2005 18:49:58 GMT\nx-amz-magic:abracadabra\nx-amz-meta-author:foo@bar.com\n/quotes/nelson"""
-SIG1 = 'jZNOcbfWmD/A/f3hSvVzXZjM2HU='
+SIG1 = six.b('jZNOcbfWmD/A/f3hSvVzXZjM2HU=')
 
 
 class TestHMACV1(unittest.TestCase):

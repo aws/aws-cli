493e4dce23d672f59401ba50d7f5c321813ebf27
James Saryerwinnie <js@jamesls.com>
2013-02-06 12:18:23 -0800

    Update test deps to use unittest2/mock
    In order to get some of unittest assertion methods in
    python2.6 we have to use the unittest2 module instead.

--- botocore/compat.py
+++ botocore/compat.py
@@ -19,6 +19,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 #
+import sys
 import six
 if six.PY3:
     from six.moves import http_client
@@ -28,3 +29,12 @@ else:
     from email.message import Message
     class HTTPHeaders(Message):
         pass
+
+
+# The unittest module got a significant overhaul
+# in 2.7, so if we're in 2.6 we can use the backported
+# version unittest2.
+if sys.version_info[:2] == (2, 6):
+    import unittest2 as unittest
+else:
+    import unittest

a837cf4a5277aa66fbe9a7a26bde80e21846296b
James Saryerwinnie <js@jamesls.com>
2013-02-15 13:44:15 -0800

    Update import to use tests.unittest

--- tests/unit/test_translate.py
+++ tests/unit/test_translate.py
@@ -19,7 +19,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 #
-from botocore.compat import unittest
+from tests import unittest
 from botocore.translate import ModelFiles, translate, merge_dicts
 
 

239ff39a4a12b9d159d5285be52d864684523918
Mitch Garnaat <mitch@garnaat.com>
2013-02-19 08:27:10 -0800

    Adding some OpsWorks unit tests.

---
+++ tests/unit/opsworks/test_create_instance.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# Copyright 2012-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+import unittest
+import awscli.clidriver
+
+
+class TestCreateInstance(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = awscli.clidriver.CLIDriver()
+        self.prefix = 'aws opsworks create-instance'
+
+    def test_simple(self):
+        cmdline = self.prefix
+        cmdline += ' --stack-id f623987f-6303-4bba-a38e-63073e85c726'
+        cmdline += ' --layer-ids cb27894d-35f3-4435-b422-6641a785fa4a'
+        cmdline += ' --instance-type c1.medium'
+        cmdline += ' --hostname aws-client-instance'
+        result = {'StackId': 'f623987f-6303-4bba-a38e-63073e85c726',
+                  'Hostname': 'aws-client-instance',
+                  'LayerIds': ['cb27894d-35f3-4435-b422-6641a785fa4a'],
+                  'InstanceType': 'c1.medium'}
+        params = self.driver.test(cmdline)
+        self.assertEqual(params, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/unit/opsworks/test_describe_layers.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# Copyright 2012-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+import unittest
+import awscli.clidriver
+
+
+class TestDescribeLayers(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = awscli.clidriver.CLIDriver()
+        self.prefix = 'aws opsworks describe-layers'
+
+    def test_both_params(self):
+        cmdline = self.prefix
+        cmdline += ' --stack-id 35959772-cd1e-4082-8346-79096d4179f2'
+        result = {'StackId': '35959772-cd1e-4082-8346-79096d4179f2'}
+        params = self.driver.test(cmdline)
+        self.assertEqual(params, result)
+
+
+if __name__ == "__main__":
+    unittest.main()

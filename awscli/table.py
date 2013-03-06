7432136ee149ee06aa2f31d0b458deb8c7c01084
James Saryerwinnie <js@jamesls.com>
2013-03-06 08:07:46 -0800

    Add table module to generate multitables
    These are tables with multiple sections.  Each section
    has a title, a row of headers, and rows of data.  A
    section does not need a title or headers.  The table can also
    generates output with colors, though there are options to
    turn this off.
    
    This pulls in a dependency on blessings for terminal output
    as well as terminal width detection.

---
+++ awscli/table.py
@@ -0,0 +1,401 @@
+# Copyright 2012-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+
+#     http://aws.amazon.com/apache2.0/
+
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+import sys
+import blessings
+
+
+def determine_terminal_width():
+    # This is a best effort atttempt at finding the terminal width.  This
+    # approach can probably been improved.
+
+    # Maybe it just works?
+    t = blessings.Terminal()
+    width = t.width
+    if width is not None:
+        return width
+    # By default, Terminal() will assume stdout as the controlling
+    # terminal.  We're going to try stdin and stderr
+    # and see if any of those work.
+    for stream in (sys.__stdin__, sys.__stderr__):
+        width = blessings.Terminal(stream=stream).width
+        if width is not None:
+            return width
+    # As a last ditch effort, we can try /dev/tty which won't work
+    # on Windows, but will work on linux/Mac.
+    try:
+        with open('/dev/tty') as f:
+            width = blessings.Terminal(stream=f).width
+            return width
+    except Exception:
+        return None
+
+
+def center_text(text, length=80, left_edge='|', right_edge='|',
+                text_length=None):
+    """Center text with specified edge chars.
+
+    You can pass in the length of the text as an arg, otherwise it is computed
+    automatically for you.  This can allow you to center a string not based
+    on it's literal length (useful if you're using ANSI codes).
+    """
+    # postcondition: len(returned_text) == length
+    if text_length is None:
+        text_length = len(text)
+    output = []
+    char_start = (length // 2) - (text_length // 2) - 1
+    output.append(left_edge + ' ' * char_start + text)
+    length_so_far = len(left_edge) + char_start + text_length
+    right_side_spaces = length - len(right_edge) - length_so_far
+    output.append(' ' * right_side_spaces)
+    output.append(right_edge)
+    final = ''.join(output)
+    return final
+
+
+def align_left(text, length, left_edge='|', right_edge='|', text_length=None,
+               left_padding=2):
+    """Left align text."""
+    # postcondition: len(returned_text) == length
+    if text_length is None:
+        text_length = len(text)
+    computed_length = (
+        text_length + left_padding + len(left_edge) + len(right_edge))
+    if length - computed_length >= 0:
+        padding = left_padding
+    else:
+        padding = 0
+    output = []
+    length_so_far = 0
+    output.append(left_edge)
+    length_so_far += len(left_edge)
+    output.append(' ' * padding)
+    length_so_far += padding
+    output.append(text)
+    length_so_far += text_length
+    output.append(' ' * (length - length_so_far - len(right_edge)))
+    output.append(right_edge)
+    return ''.join(output)
+
+
+def convert_to_vertical_table(sections):
+    # Any section that only has a single row is
+    # inverted, so:
+    # header1 | header2 | header3
+    # val1    | val2    | val2
+    #
+    # becomes:
+    #
+    # header1 | val1
+    # header2 | val2
+    # header3 | val3
+    for i, section in enumerate(sections):
+        if len(section.rows) == 1 and section.headers:
+            headers = section.headers
+            new_section = Section()
+            new_section.title = section.title
+            new_section.indent_level = section.indent_level
+            for header, element in zip(headers, section.rows[0]):
+                new_section.add_row([header, element])
+            sections[i] = new_section
+
+
+class IndentedStream(object):
+    def __init__(self, stream, indent_level, left_indent_char='|',
+                 right_indent_char='|'):
+        self._stream = stream
+        self._indent_level = indent_level
+        self._left_indent_char = left_indent_char
+        self._right_indent_char = right_indent_char
+
+    def write(self, text):
+        self._stream.write(self._left_indent_char * self._indent_level)
+        if text.endswith('\n'):
+            self._stream.write(text[:-1])
+            self._stream.write(self._right_indent_char * self._indent_level)
+            self._stream.write('\n')
+        else:
+            self._stream.write(text)
+
+    def __getattr__(self, attr):
+        return getattr(self._stream, attr)
+
+
+class Styler(object):
+    def style_title(self, text):
+        return text
+
+    def style_header_column(self, text):
+        return text
+
+    def style_row_element(self, text):
+        return text
+
+
+class ColorizedStyler(Styler):
+    def __init__(self, terminal):
+        self._terminal = terminal
+
+    def style_title(self, text):
+        return self._terminal.bold_underline(text)
+
+    def style_header_column(self, text):
+        return self._terminal.underline(text)
+
+    def style_row_element(self, text):
+        return self._terminal.bright_blue(text)
+
+
+class MultiTable(object):
+    def __init__(self, terminal_width=None, initial_section=True,
+                 column_separator='|', terminal=None,
+                 styler=None, auto_reformat=None):
+        self._auto_reformat = auto_reformat
+        if initial_section:
+            self._current_section = Section()
+            self._sections = [self._current_section]
+        else:
+            self._current_section = None
+            self._sections = []
+        if terminal is None:
+            self._terminal = blessings.Terminal()
+        else:
+            self._terminal = terminal
+        if styler is None:
+            if self._terminal.is_a_tty:
+                self._styler = ColorizedStyler(self._terminal)
+            else:
+                self._styler = Styler()
+        else:
+            self._styler = styler
+        self._rendering_index = 0
+        self._column_separator = column_separator
+        if terminal_width is None:
+            self._terminal_width = determine_terminal_width()
+
+    def add_title(self, title):
+        self._current_section.add_title(title)
+
+    def add_row_header(self, headers):
+        self._current_section.add_header(headers)
+
+    def add_row(self, row_elements):
+        self._current_section.add_row(row_elements)
+
+    def new_section(self, title, indent_level=0):
+        self._current_section = Section()
+        self._sections.append(self._current_section)
+        self._current_section.add_title(title)
+        self._current_section.indent_level = indent_level
+
+    def render(self, stream):
+        max_width = self._calculate_max_width()
+        should_convert_table = self._determine_conversion_needed(max_width)
+        if should_convert_table:
+            convert_to_vertical_table(self._sections)
+            max_width = self._calculate_max_width()
+        stream.write('-' * max_width + '\n')
+        for section in self._sections:
+            self._render_section(section, max_width, stream)
+
+    def _determine_conversion_needed(self, max_width):
+        # If we don't know the width of the controlling terminal,
+        # then we don't try to resize the table.
+        if self._terminal_width is None:
+            return False
+        if max_width > self._terminal_width:
+            # Provided that calculated width of the table is greater than
+            # the terminal width then the auto_reformat flag will determine
+            # what we do.
+            if self._auto_reformat is None:
+                return self._terminal.is_a_tty
+            else:
+                return self._auto_reformat
+
+    def _calculate_max_width(self):
+        max_width = max(s.total_width(padding=4, with_border=True,
+                                      outer_padding=s.indent_level)
+                        for s in self._sections)
+        return max_width
+
+    def _render_section(self, section, max_width, stream):
+        stream = IndentedStream(stream, section.indent_level,
+                                self._terminal.bright_yellow('|'),
+                                self._terminal.bright_yellow('|'))
+        max_width -= (section.indent_level * 2)
+        self._render_title(section, max_width, stream)
+        self._render_column_titles(section, max_width, stream)
+        self._render_rows(section, max_width, stream)
+
+    def _render_title(self, section, max_width, stream):
+        # The title consists of:
+        # title        :  |   This is the title      |
+        # bottom_border:  ----------------------------
+        if section.title:
+            title = self._styler.style_title(section.title)
+            stream.write(center_text(title, max_width, '|', '|',
+                                     len(section.title)) + '\n')
+            if not section.headers and not section.rows:
+                stream.write('+%s+' % ('-' * (max_width - 2)) + '\n')
+
+    def _render_column_titles(self, section, max_width, stream):
+        if not section.headers:
+            return
+        # In order to render the column titles we need to know
+        # the width of each of the columns.
+        widths = section.calculate_column_widths(padding=4,
+                                                 max_width=max_width)
+        # TODO: Built a list instead of +=, it's more efficient.
+        current = ''
+        length_so_far = 0
+        # The first cell needs both left and right edges '|  foo  |'
+        # while subsequent cells only need right edges '  foo  |'.
+        first = True
+        for width, header in zip(widths, section.headers):
+            stylized_header = self._styler.style_header_column(header)
+            if first:
+                left_edge = '|'
+                first = False
+            else:
+                left_edge = ''
+            current += center_text(text=stylized_header, length=width,
+                                   left_edge=left_edge, right_edge='|',
+                                   text_length=len(header))
+            length_so_far += width
+        self._write_line_break(stream, widths)
+        stream.write(current + '\n')
+
+    def _write_line_break(self, stream, widths):
+        # Write out something like:
+        # +-------+---------+---------+
+        parts = []
+        first = True
+        for width in widths:
+            if first:
+                parts.append('+%s+' % ('-' * (width - 2)))
+                first = False
+            else:
+                parts.append('%s+' % ('-' * (width - 1)))
+        parts.append('\n')
+        stream.write(''.join(parts))
+
+    def _render_rows(self, section, max_width, stream):
+        if not section.rows:
+            return
+        widths = section.calculate_column_widths(padding=4,
+                                                 max_width=max_width)
+        self._write_line_break(stream, widths)
+        for row in section.rows:
+            # TODO: Built the string in a list then join instead of using +=,
+            # it's more efficient.
+            current = ''
+            length_so_far = 0
+            first = True
+            for width, element in zip(widths, row):
+                if first:
+                    left_edge = '|'
+                    first = False
+                else:
+                    left_edge = ''
+                stylized = self._styler.style_row_element(element)
+                current += align_left(text=stylized, length=width,
+                                      left_edge=left_edge,
+                                      right_edge=self._column_separator,
+                                      text_length=len(element))
+                length_so_far += width
+            stream.write(current + '\n')
+        self._write_line_break(stream, widths)
+
+
+class Section(object):
+    def __init__(self):
+        self.title = ''
+        self.headers = []
+        self.rows = []
+        self.indent_level = 0
+        self._num_cols = None
+        self._max_widths = []
+
+    def __repr__(self):
+        return ("Section(title=%s, headers=%s, indent_level=%s, num_rows=%s)" %
+                (self.title, self.headers, self.indent_level, len(self.rows)))
+
+    def calculate_column_widths(self, padding=0, max_width=None):
+        # postcondition: sum(widths) == max_width
+        unscaled_widths = [w + padding for w in self._max_widths]
+        if max_width is None:
+            return unscaled_widths
+        else:
+            # Compute scale factor for max_width.
+            scale_factor = max_width / float(sum(unscaled_widths))
+            scaled = [int(round(scale_factor * w)) for w in unscaled_widths]
+            # Once we've scaled the columns, we may be slightly over/under
+            # the amount we need so we have to adjust the columns.
+            off_by = sum(scaled) - max_width
+            while off_by != 0:
+                iter_order = range(len(scaled))
+                if off_by < 0:
+                    iter_order = reversed(iter_order)
+                for i in iter_order:
+                    if off_by > 0:
+                        scaled[i] -= 1
+                        off_by -= 1
+                    else:
+                        scaled[i] += 1
+                        off_by += 1
+                    if off_by == 0:
+                        break
+            return scaled
+
+    def total_width(self, padding=0, with_border=False, outer_padding=0):
+        total = 0
+        # One char on each side == 2 chars total to the width.
+        border_padding = 2
+        for w in self.calculate_column_widths():
+            total += w + padding
+        if with_border:
+            total += border_padding
+        total += outer_padding + outer_padding
+        return max(len(self.title) + border_padding + outer_padding +
+                   outer_padding, total)
+
+    def add_title(self, title):
+        self.title = title
+
+    def add_header(self, headers):
+        self._update_max_widths(headers)
+        if self._num_cols is None:
+            self._num_cols = len(headers)
+        self.headers = self._format_headers(headers)
+
+    def _format_headers(self, headers):
+        return headers
+
+    def add_row(self, row):
+        if self._num_cols is None:
+            self._num_cols = len(row)
+        if len(row) != self._num_cols:
+            raise ValueError("Row should have %s elements, instead "
+                             "it has %s" % (self._num_cols, len(row)))
+        row = self._format_row(row)
+        self.rows.append(row)
+        self._update_max_widths(row)
+
+    def _format_row(self, row):
+        return [str(r) for r in row]
+
+    def _update_max_widths(self, row):
+        if not self._max_widths:
+            self._max_widths = [len(el) for el in row]
+        else:
+            for i, el in enumerate(row):
+                self._max_widths[i] = max(len(el), self._max_widths[i])
---
+++ tests/unit/test_table.py
@@ -0,0 +1,117 @@
+# Copyright (c) 2013 Amazon.com, Inc. or its affiliates.  All Rights Reserved
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+import unittest
+
+from awscli.table import Section, MultiTable, convert_to_vertical_table
+
+
+class TestSection(unittest.TestCase):
+    def setUp(self):
+        self.section = Section()
+
+    def test_add_row_tracks_max_widths(self):
+        self.section.add_row(['one', 'two', 'three'])
+        self.assertEqual(self.section.calculate_column_widths(), [3, 3, 5])
+        self.section.add_row(['1234567', '1234567', '1234567'])
+        self.assertEqual(self.section.calculate_column_widths(), [7, 7, 7])
+        self.section.add_row(['a', 'a', 'a'])
+        self.assertEqual(self.section.calculate_column_widths(), [7, 7, 7])
+        self.section.add_row(['123456789', '1', '1'])
+        self.assertEqual(self.section.calculate_column_widths(), [9, 7, 7])
+        self.assertEqual(self.section.calculate_column_widths(1), [10, 8, 8])
+        self.assertEqual(self.section.total_width(), 23)
+
+    def test_add_row_also_tracks_header(self):
+        self.section.add_header(['123456789', '12345', '1234567'])
+        self.section.add_row(['a', 'a', 'a'])
+        self.section.add_row(['aa', 'aa', 'aa'])
+        self.section.add_row(['aaa', 'aaa', 'aaa'])
+        self.assertEqual(self.section.calculate_column_widths(), [9, 5, 7])
+        self.assertEqual(self.section.total_width(), 21)
+        self.section.add_row(['aaa', '123456789', 'aaa'])
+        self.assertEqual(self.section.calculate_column_widths(), [9, 9, 7])
+        self.assertEqual(self.section.total_width(padding=3, with_border=True),
+                         36)
+
+    def test_max_width_with_scaling_perfect_scaling(self):
+        self.section.add_row(['one', 'two', 'three'])
+        self.section.add_row(['1234567', '1234567', '1234567'])
+        # Perfect scaling, exactly double.
+        widths = self.section.calculate_column_widths(max_width=42)
+        self.assertEqual(widths, [14, 14, 14])
+
+    def test_max_width_scaling_one_unit_short(self):
+        self.section.add_row(['one', 'two', 'three'])
+        self.section.add_row(['1234567', '1234567', '1234567'])
+        widths = self.section.calculate_column_widths(max_width=41)
+        self.assertEqual(widths, [13, 14, 14])
+
+    def test_max_width_scaling_is_negative(self):
+        self.section.add_row(['12345', '12345'])
+        widths = self.section.calculate_column_widths(max_width=17)
+        self.assertEqual(widths, [8, 9])
+
+    def test_allow_sections_to_be_padded(self):
+        self.section.add_row(['one', 'two', 'three'])
+        self.section.add_row(['1234567', '1234567', '1234567'])
+        self.assertEqual(self.section.total_width(padding=2), 27)
+        self.assertEqual(self.section.total_width(padding=2, outer_padding=1), 29)
+
+    def test_title_accounts_for_outer_padding(self):
+        self.section.add_row(['a', 'b', 'c'])
+        self.section.add_title('123456789')
+        self.assertEqual(self.section.total_width(padding=2, outer_padding=3), 17)
+
+
+class TestMultiTable(unittest.TestCase):
+    def setUp(self):
+        self.table = MultiTable()
+
+    def test_max_width_calculation(self):
+        self.table.add_title('foo')
+        self.table.add_row_header(['one', 'two', 'three'])
+        self.table.add_row(['one', 'two', 'three'])
+        self.table.new_section('bar')
+        self.table.add_row_header(['one', 'two'])
+        self.table.add_row(['12345', '1234567'])
+
+
+class TestVerticalTableConversion(unittest.TestCase):
+    def setUp(self):
+        self.table = MultiTable()
+
+    def test_convert_section_to_vertical(self):
+        self.table.add_title('foo')
+        self.table.add_row_header(['key1', 'key2', 'key3'])
+        self.table.add_row(['val1', 'val2', 'val3'])
+        convert_to_vertical_table(self.table._sections)
+        # To convert to a vertical table, there should be no headers:
+        section = self.table._sections[0]
+        self.assertEqual(section.headers, [])
+        # Then we should create a two column row with key val pairs.
+        self.assertEqual(
+            section.rows,
+            [['key1', 'val1'], ['key2', 'val2'], ['key3', 'val3']])
+
+
+if __name__ == '__main__':
+    unittest.main()

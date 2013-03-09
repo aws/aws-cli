969ff0b8fe55dc116ff6bc16441c6cbba16d353e
Mitch Garnaat <mitch@garnaat.com>
2013-03-09 08:41:38 -0800

    Moving man pages and adding text files that will be used on Windows platform rather than man.

--- awscli/help.py
+++ awscli/help.py
@@ -12,21 +12,73 @@
 # language governing permissions and limitations under the License.
 import sys
 import os
+import platform
+
+
+def get_text_path():
+    textpath = os.path.split(__file__)[0]
+    textpath = os.path.split(textpath)[0]
+    textpath = os.path.join(textpath, 'doc')
+    textpath = os.path.join(textpath, 'text')
+    textpath = os.path.join(textpath, 'reference')
+    return textpath
+
+def do_text_provider(provider):
+    textpath = get_text_path()
+    textpath = os.path.join('index.txt')
+    fp = open(textpath, 'r')
+    sys.stdout.write(fp.read())
+    sys.stdout.write('\n')
+    fp.close()
+
+
+def do_text_service(service):
+    textpath = get_text_path()
+    textpath = os.path.join(textpath, service.cli_name)
+    textpath = os.path.join(textpath, 'index.txt')
+    fp = open(textpath, 'r')
+    sys.stdout.write(fp.read())
+    sys.stdout.write('\n')
+    fp.close()
+
+
+def do_text_operation(operation):
+    textpath = get_text_path()
+    textpath = os.path.join(textpath, operation.service.cli_name)
+    textpath = os.path.join(textpath, operation.cli_name + '.txt')
+    fp = open(textpath, 'r')
+    sys.stdout.write(fp.read())
+    sys.stdout.write('\n')
+    fp.close()
+
 
 def do_man(man_page):
     manpath = os.path.split(__file__)[0]
     manpath = os.path.split(manpath)[0]
+    manpath = os.path.join(manpath, 'doc')
     manpath = os.path.join(manpath, 'man')
     args = ['man', '-M', manpath, man_page]
     sys.stdout.flush()
     sys.stderr.flush()
     os.execvp('man', args)
 
+
 def get_provider_help(provider='aws'):
-    do_man(provider)
+    if platform.system == 'Windows':
+        do_text_provider(provider)
+    else:
+        do_man(provider)
+
 
 def get_service_help(service):
-    do_man(service.cli_name)
+    if platform.system == 'Windows':
+        do_text_service(service)
+    else:
+        do_man(service.cli_name)
+
 
 def get_operation_help(operation):
-    do_man(operation.service.cli_name + '-' + operation.cli_name)
+    if platform.system == 'Windows':
+        do_text_operation(operation)
+    else:
+        do_man(operation.service.cli_name + '-' + operation.cli_name)

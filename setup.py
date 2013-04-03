1732e0079684bc235493fba8bce86ff9ea5216d1
Mitch Garnaat <mitch@garnaat.com>
2013-04-03 05:45:45 -0700

    Hotfix to include new Redshift region.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ packages = [
     'awscli',
 ]
 
-requires = ['botocore>=0.8.2',
+requires = ['botocore>=0.8.3',
             'six>=1.1.0',
             'colorama==0.2.5',
             'argparse>=1.1']

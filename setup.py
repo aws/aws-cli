ec2a6f0a018b94a8116d39a7b1daf826ac6126c6
Mitch Garnaat <mitch@garnaat.com>
2013-05-05 12:56:09 -0700

    Generating docs for support service and bumping version number.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ packages = [
     'awscli',
 ]
 
-requires = ['botocore>=0.9.1',
+requires = ['botocore>=0.9.2',
             'six>=1.1.0',
             'colorama==0.2.5',
             'argparse>=1.1']

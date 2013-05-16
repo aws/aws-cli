e2e6ca8e98b09251fcd5e01b51b05cc50afa1882
Mitch Garnaat <mitch@garnaat.com>
2013-05-16 07:33:45 -0700

    Bumping version number, removing unneeded code in setup.py.

--- botocore/__init__.py
+++ botocore/__init__.py
@@ -23,7 +23,7 @@
 import re
 import logging
 
-__version__ = '0.9.2'
+__version__ = '0.10.0'
 
 
 class NullHandler(logging.Handler):
--- setup.py
+++ setup.py
@@ -14,10 +14,6 @@ try:
 except ImportError:
     from distutils.core import setup
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
-
 packages = [
     'botocore',
 ]

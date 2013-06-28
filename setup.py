1a6e2f062dc052a5f28b34d8e49b54ca24d88c50
James Saryerwinnie <js@jamesls.com>
2013-06-28 10:37:38 -0700

    Update to reflect supported python versions

--- setup.py
+++ setup.py
@@ -48,8 +48,6 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
     ),
 )

17f577b2d007afa9949f26020d4760fde0a38123
James Saryerwinnie <js@jamesls.com>
2013-07-01 23:15:32 -0700

    Fix for python3.3 compat, use absolute import

--- awscli/help.py
+++ awscli/help.py
@@ -14,14 +14,17 @@ import sys
 import os
 import platform
 from subprocess import Popen, PIPE
-from argprocess import ParamShorthand
+
+from docutils.core import publish_string
 from bcdoc.clidocs import ReSTDocument
 from bcdoc.clidocs import ProviderDocumentEventHandler
 from bcdoc.clidocs import ServiceDocumentEventHandler
 from bcdoc.clidocs import OperationDocumentEventHandler
 import bcdoc.clidocevents
 from bcdoc.textwriter import TextWriter
-from docutils.core import publish_string
+
+from awscli.argprocess import ParamShorthand
+
 
 class HelpRenderer(object):
     """

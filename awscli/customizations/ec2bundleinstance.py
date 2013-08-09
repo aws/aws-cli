b173056554f85e23ca0a3cdd00732be58eb674b7
Mitch Garnaat <mitch@garnaat.com>
2013-08-09 15:28:36 -0700

    Adding customizations for ec2 bundle-instance to allow users to enter scalar params and to generate a polciy if not provided and to sign the policy.

--- awscli/customizations/ec2bundleinstance.py
+++ awscli/customizations/ec2bundleinstance.py
@@ -86,7 +86,6 @@ def _add_params(argument_table, operation, **kwargs):
     argument_table['owner-sak'] = arg
     arg = BundleArgument(storage_param='UploadPolicy',
                          operation=operation, name='policy',
-                         documentation=POLICY_DOCS)
     argument_table['policy'] = arg
 
 

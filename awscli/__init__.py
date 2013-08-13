7e18022a357eebf4e2b507e287c5729c39a8c889
Mitch Garnaat <mitch@garnaat.com>
2013-08-13 06:15:08 -0700

    Update dependencies in setup.py file.  Bump version number.

--- awscli/__init__.py
+++ awscli/__init__.py
@@ -17,7 +17,7 @@ A Universal Command Line Environment for Amazon Web Services.
 """
 import os
 
-__version__ = '0.14.0'
+__version__ = '0.14.1'
 
 #
 # Get our data path to be added to botocore's search path

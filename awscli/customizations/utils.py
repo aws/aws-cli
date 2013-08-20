2df9991ff9ba6fd2ef89130cd5d5411095c70f5d
James Saryerwinnie <js@jamesls.com>
2013-08-20 16:52:50 -0700

    Split RDS modify command into add/remove commands
    The modify-option-group has been split into:
    
    * add-option-to-option-group
    * remove-option-from-option-group
    
    Part of this change included the ability for the argument name
    to be changed by an external client.  This is encapsulated in
    the awscli.customizations.utils.rename_argument function which
    I've added.

---
+++ awscli/customizations/utils.py
@@ -0,0 +1,21 @@
+# Copyright 2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""
+Utility functions to make it easier to work with customizations.
+
+"""
+def rename_argument(argument_table, existing_name, new_name):
+    current = argument_table[existing_name]
+    argument_table[new_name] = current
+    current.name = new_name
+    del argument_table[existing_name]
--- awscli/handlers.py
+++ awscli/handlers.py
@@ -28,6 +28,7 @@ from awscli.customizations.preview import register_preview_commands
 from awscli.customizations.ec2bundleinstance import register_bundleinstance
 from awscli.customizations.s3.s3 import s3_plugin_initialize
 from awscli.customizations.ec2runinstances import register_runinstances
+from awscli.customizations.rds import register_rds_modify_split
 
 
 def awscli_initialize(event_handlers):
@@ -58,3 +59,4 @@ def awscli_initialize(event_handlers):
     register_runinstances(event_handlers)
     register_removals(event_handlers)
     register_preview_commands(event_handlers)
+    register_rds_modify_split(event_handlers)

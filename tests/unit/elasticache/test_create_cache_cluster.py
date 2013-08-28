09db8c694180fac5ab9103ab9a7c3f26a8406ed3
James Saryerwinnie <js@jamesls.com>
2013-08-28 11:28:19 -0700

    Add test verifying when no bool option exists

--- tests/unit/elasticache/test_create_cache_cluster.py
+++ tests/unit/elasticache/test_create_cache_cluster.py
@@ -65,6 +65,30 @@ class TestCreateCacheCluster(BaseAWSCommandParamsTest):
                   'PreferredMaintenanceWindow': 'fri:08:00-fri:09:00'}
         self.assert_params_for_cmd(cmdline, result)
 
+    def test_minor_upgrade_arg_not_specified(self):
+        args = ('--cache-cluster-id cachecluster-us-east-1c '
+                '--num-cache-nodes 1 '
+                '--cache-node-type cache.m1.small '
+                '--engine memcached '
+                '--engine-version 1.4.5 '
+                '--cache-security-group-names group1 group2 '
+                '--preferred-availability-zone us-east-1c '
+                '--preferred-maintenance-window fri:08:00-fri:09:00')
+        cmdline = self.prefix + args
+        # Note how if neither '--auto-minor-version-upgrade' nor
+        # '--no-auto-minor-version-upgrade' is specified, then
+        # AutoMinorVersionUpgrade is not in the result dict.
+        result = {'CacheClusterId': 'cachecluster-us-east-1c',
+                  'CacheNodeType': 'cache.m1.small',
+                  'CacheSecurityGroupNames.member.1': 'group1',
+                  'CacheSecurityGroupNames.member.2': 'group2',
+                  'Engine': 'memcached',
+                  'EngineVersion': '1.4.5',
+                  'NumCacheNodes': '1',
+                  'PreferredAvailabilityZone': 'us-east-1c',
+                  'PreferredMaintenanceWindow': 'fri:08:00-fri:09:00'}
+        self.assert_params_for_cmd(cmdline, result)
+
 
 if __name__ == "__main__":
     unittest.main()

2ea1b3e9381f55121eb7ebf4cb096cad944711a9
Mitch Garnaat <mitch@garnaat.com>
2013-08-31 15:40:01 -0700

    WIP Commit.

--- awscli/completer.py
+++ awscli/completer.py
@@ -11,45 +11,110 @@
 # language governing permissions and limitations under the License.
 import awscli.clidriver
 import sys
+import logging
 
-def complete(cmdline, point=None):
-    if point is None:
-        point = len(cmdline)
-    service_name = None
-    driver = awscli.clidriver.create_clidriver()
-    main_hc = driver.create_help_command()
-    words = cmdline[0:point].split()
-    current_word = words[-1]
-    if len(words) >= 2:
-        previous_word = words[-2]
-    else:
-        previous_word = None
-    # First find all non-options words in command line
-    non_options = [w for w in words if not w.startswith('-')]
-    # Look for a service name in the non_options
-    for w in non_options:
-        if w in main_hc.command_table:
-            service_name = w
-            break
-    if current_word.startswith('-'):
-        current_word = current_word.lstrip('-')
-        l = ['--' + n for n in main_hc.arg_table if n.startswith(current_word)]
-        return l
-    if not service_name:
+LOG = logging.getLogger(__name__)
+
+
+class Completer(object):
+
+    def __init__(self):
+        self.driver = awscli.clidriver.create_clidriver()
+        self.main_hc = self.driver.create_help_command()
+        self.main_options = [n for n in self.main_hc.arg_table]
+        self.cmdline = None
+        self.point = None
+        self.command_hc = None
+        self.subcommand_hc = None
+        self.command_name = None
+        self.subcommand_name = None
+        self.current_word = None
+        self.previous_word = None
+        self.non_options = None
+
+    def complete(self, cmdline, point):
+        self.command_name = None
+        if point is None:
+            point = len(cmdline)
+        self.point = point
+        self.words = cmdline[0:point].split()
+        self.current_word = self.words[-1]
+        if len(self.words) >= 2:
+            self.previous_word = self.words[-2]
+        else:
+            self.previous_word = None
+        self.non_options = [w for w in self.words if not w.startswith('-')]
+        # Look for a service name in the non_options
+        for w in self.non_options:
+            if w in self.main_hc.command_table:
+                self.command_name = w
+                break
+        if not self.command_name:
+            # If we didn't find any command names in the cmdline
+            # lets try to complete provider options
+            return self._complete_provider()
+        return self._complete_command()
+
+    def _find_possible_options(self, arg_table=None):
+        all_options = self.main_options
+        if arg_table:
+            all_options = all_options + arg_table.keys()
+        cw = self.current_word.lstrip('-')
+        return ['--' + n for n in all_options if n.startswith(cw)]
+
+    def _complete_provider(self):
         retval = []
-        if current_word == 'aws':
-            retval = main_hc.command_table.keys()
-        # Otherwise, see if they have entered a partial service name
-        l = [n for n in main_hc.command_table if n.startswith(current_word)]
-        if l:
+        if self.current_word.startswith('-'):
+            cw = self.current_word.lstrip('-')
+            l = ['--' + n for n in self.main_options
+                 if n.startswith(cw)]
             retval = l
+        elif self.current_word == 'aws':
+            retval = self.main_hc.command_table.keys()
+        else:
+            # Otherwise, see if they have entered a partial command name
+            retval = [n for n in self.main_hc.command_table
+                      if n.startswith(self.current_word)]
         return retval
 
+    def _complete_command(self):
+        retval = []
+        self.subcommand_name = None
+        self.command_hc = self.main_hc.command_table[self.command_name].create_help_command()
+        # Look for complete subcommand name in cmdline
+        for w in self.non_options:
+            if w in self.command_hc.command_table:
+                self.subcommand_name = w
+                break
+        if self.subcommand_name:
+            return self._complete_subcommand()
+        if self.current_word == self.command_name:
+            retval = self.command_hc.command_table.keys()
+        elif self.current_word.startswith('-'):
+            retval = self._find_possible_options()
+        else:
+            # See if they have entered a partial command name
+            retval = [n for n in self.command_hc.command_table
+                      if n.startswith(self.current_word)]
+        return retval
 
+    def _complete_subcommand(self):
+        retval = []
+        self.subcommand_hc = self.command_hc.command_table[self.subcommand_name].create_help_command()
+        if self.current_word == self.subcommand_name:
+            retval = []
+        elif self.current_word.startswith('-'):
+            retval = self._find_possible_options(self.subcommand_hc.arg_table)
+        return retval
+        
 if __name__ == '__main__':
     if len(sys.argv) == 3:
-        print(complete(sys.argv[1], int(sys.argv[2])))
+        cmdline = sys.argv[1]
+        point = int(sys.argv[2])
     elif len(sys.argv) == 2:
-        print(complete(sys.argv[1]))
+        cmdline = sys.argv[1]
     else:
         print('usage: %s <cmdline> <point>' % sys.argv[0])
+        sys.exit(1)
+    completer = Completer()
+    print(completer.complete(cmdline, point))
--- tests/unit/test_completer.py
+++ tests/unit/test_completer.py
@@ -14,9 +14,13 @@ import pprint
 import logging
 import difflib
 
-from awscli.completer import complete
+from awscli.completer import Completer
 
 LOG = logging.getLogger(__name__)
+
+GLOBALOPTS = ['--debug', '--endpoint-url', '--no-verify-ssl',
+              '--no-paginate', '--output', '--profile',
+              '--region', '--version', '--color']
 
 COMPLETIONS = [
     ('aws ', -1, set(['autoscaling', 'cloudformation', 'cloudfront',
@@ -31,75 +35,28 @@ COMPLETIONS = [
     ('aws cloudf', -1, set(['cloudformation', 'cloudfront'])),
     ('aws cloudfr', -1, set(['cloudfront'])),
     ('aws foobar', -1, set([])),
-    ('aws  --', -1, set(['--debug', '--endpoint-url', '--no-verify-ssl',
-                         '--no-paginate', '--output', '--profile',
-                         '--region', '--version', '--color'])),
+    ('aws  --', -1, set(GLOBALOPTS)),
     ('aws  --re', -1, set(['--region'])),
     ('aws sts ', -1, set(['assume-role', 'get-federation-token',
                           'decode-authorization-message',
                           'assume-role-with-web-identity',
-                          'get-session-token', 'help'])),
+                          'get-session-token'])),
     ('aws sts de', -1, set(['decode-authorization-message'])),
-    ('aws sts --', -1, set(['--debug', '--endpoint-url', '--no-verify-ssl',
-                            '--no-paginate', '--output', '--profile',
-                            '--region', '--version', '--color'])),
-    ('aws sts decode-authorization-message ', -1, set(['--encoded-message'])),
+    ('aws sts --', -1, set(GLOBALOPTS)),
+    ('aws sts decode-authorization-message', -1, set([])),
     ('aws sts decode-authorization-message --encoded-message --re', -1,
-     set(['--region']))
+     set(['--region'])),
+    ('aws sts decode-authorization-message --encoded-message --enco', -1,
+     set(['--encoded-message'])),
+    ('aws s3', -1, set(['cp', 'mv', 'rm', 'mb', 'rb', 'ls', 'sync'])),
+    ('aws s3 m', -1, set(['mv', 'mb'])),
+    ('aws s3 cp -', -1, set(['--no-guess-mime-type', '--dryrun',
+                             '--recursive', '--quiet', '--acl',
+                             '--exclude', '--include'] + GLOBALOPTS)),
     ]
 
-_COMPLETIONS = [
-    ('aws ', -1, ('autoscaling', 'cloudformation', 'cloudfront',
-                  'cloudsearch', 'cloudwatch', 'datapipeline',
-                  'directconnect', 'dynamodb', 'ec2', 'elasticache',
-                  'elasticbeanstalk', 'elastictranscoder', 'elb', 'emr',
-                  'iam', 'importexport', 'opsworks', 'rds', 'redshift',
-                  'route53', 's3', 'ses', 'sns', 'sqs', 'storagegateway',
-                  'sts', 'support', 'swf')),
-    ('aws cloud', -1, ('cloudformation', 'cloudfront',
-                       'cloudsearch', 'cloudwatch')),
-    ('aws cloudf', -1, ('cloudformation', 'cloudfront')),
-    ('aws cloudfr', -1, ('cloudfront',)),
-    ('aws sts ', -1, ('assume-role', 'get-federation-token',
-                      'decode-authorization-message',
-                      'assume-role-with-web-identity',
-                      'get-session-token')),
-    ('aws sts de', -1, ('decode-authorization-message',)),
-    ('aws sts --', -1, ('--debug', '--endpoint-url', '--no-verify-ssl',
-                        '--no-paginate', '--output', '--profile',
-                        '--region', '--version', '--color')),
-    ('aws sts decode-authorization-message ', -1, ('--encoded-message',)),
-    ('aws sts decode-authorization-message --encoded-message --re', -1,
-     ('--region',))
-    ]
-
-
-def check_dicts(xmlfile, d1, d2):
-    if d1 != d2:
-        LOG.debug('-' * 40)
-        LOG.debug(xmlfile)
-        LOG.debug('-' * 40)
-        LOG.debug(pprint.pformat(d1))
-        LOG.debug('-' * 40)
-        LOG.debug(pprint.pformat(d2))
-    if not d1 == d2:
-        # Borrowed from assertDictEqual, though this doesn't
-        # handle the case when unicode literals are used in one
-        # dict but not in the other (and we want to consider them
-        # as being equal).
-        pretty_d1 = pprint.pformat(d1, width=1).splitlines()
-        pretty_d2 = pprint.pformat(d2, width=1).splitlines()
-        diff = ('\n' + '\n'.join(difflib.ndiff(pretty_d1, pretty_d2)))
-        raise AssertionError("Dicts are not equal:\n%s" % diff)
 
 def check_completer(cmdline, results, expected_results):
-    if results != expected_results:
-        LOG.debug('-' * 40)
-        LOG.debug(cmdline)
-        LOG.debug('-' * 40)
-        LOG.debug(pprint.pformat(results))
-        LOG.debug('-' * 40)
-        LOG.debug(pprint.pformat(expected_results))
     if not results == expected_results:
         # Borrowed from assertDictEqual, though this doesn't
         # handle the case when unicode literals are used in one
@@ -111,11 +68,11 @@ def check_completer(cmdline, results, expected_results):
         raise AssertionError("Results are not equal:\n%s" % diff)
     assert results == expected_results
 
+
 def test_completions():
+    completer = Completer()
     for cmdline, point, expected_results in COMPLETIONS:
         if point == -1:
             point = len(cmdline)
-        results = complete(cmdline, point)
-        if results is not None:
-            results = set(complete(cmdline, point))
+        results = set(completer.complete(cmdline, point))
         yield check_completer, cmdline, results, expected_results

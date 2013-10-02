9f3eade5520ac79f937a6c65c23210928277979f
James Saryerwinnie <js@jamesls.com>
2013-10-02 12:12:58 -0700

    Bumping version to 1.1.1

--- awscli/__init__.py
+++ awscli/__init__.py
@@ -17,7 +17,7 @@ A Universal Command Line Environment for Amazon Web Services.
 """
 import os
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 #
 # Get our data path to be added to botocore's search path

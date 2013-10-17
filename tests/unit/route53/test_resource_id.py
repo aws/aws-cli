5350cccddb63270e4c0a62a31de1371607e75f90
Mitch Garnaat <mitch@garnaat.com>
2013-10-17 06:40:56 -0700

    Allow resource ids for route53 to be passed in as returned by the service or in their canonical form.  Fixes #396.

---
+++ tests/unit/route53/test_resource_id.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# Copyright 2012-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+from tests.unit import BaseAWSCommandParamsTest
+
+
+class TestGetHostedZone(BaseAWSCommandParamsTest):
+
+    prefix = 'route53 get-hosted-zone'
+
+    def setUp(self):
+        super(TestGetHostedZone, self).setUp()
+
+    def test_full_resource_id(self):
+        args = ' --id /hostedzone/ZD3IYMVP1KDDM'
+        cmdline = self.prefix + args
+        result = {'uri_params': {'Id': 'ZD3IYMVP1KDDM'},
+                  'headers': {}}
+        self.assert_params_for_cmd(cmdline, result, expected_rc=0,
+                                   ignore_params=['payload'])[0]
+
+    def test_short_resource_id(self):
+        args = ' --id ZD3IYMVP1KDDM'
+        cmdline = self.prefix + args
+        result = {'uri_params': {'Id': 'ZD3IYMVP1KDDM'},
+                  'headers': {}}
+        self.assert_params_for_cmd(cmdline, result, expected_rc=0,
+                                   ignore_params=['payload'])[0]
+
+        
+class TestGetHealthCheck(BaseAWSCommandParamsTest):
+
+    prefix = 'route53 get-health-check'
+
+    def setUp(self):
+        super(TestGetHealthCheck, self).setUp()
+
+    def test_full_resource_id(self):
+        args = ' --health-check-id /healthcheck/ZD3IYMVP1KDDM'
+        cmdline = self.prefix + args
+        result = {'uri_params': {'HealthCheckId': 'ZD3IYMVP1KDDM'},
+                  'headers': {}}
+        self.assert_params_for_cmd(cmdline, result, expected_rc=0,
+                                   ignore_params=['payload'])[0]
+
+    def test_short_resource_id(self):
+        args = ' --health-check-id ZD3IYMVP1KDDM'
+        cmdline = self.prefix + args
+        result = {'uri_params': {'HealthCheckId': 'ZD3IYMVP1KDDM'},
+                  'headers': {}}
+        self.assert_params_for_cmd(cmdline, result, expected_rc=0,
+                                   ignore_params=['payload'])[0]
+
+
+class TestGetChange(BaseAWSCommandParamsTest):
+
+    prefix = 'route53 get-change'
+
+    def setUp(self):
+        super(TestGetChange, self).setUp()
+
+    def test_full_resource_id(self):
+        args = ' --id /change/ZD3IYMVP1KDDM'
+        cmdline = self.prefix + args
+        result = {'uri_params': {'Id': 'ZD3IYMVP1KDDM'},
+                  'headers': {}}
+        self.assert_params_for_cmd(cmdline, result, expected_rc=0,
+                                   ignore_params=['payload'])[0]
+
+    def test_short_resource_id(self):
+        args = ' --id ZD3IYMVP1KDDM'
+        cmdline = self.prefix + args
+        result = {'uri_params': {'Id': 'ZD3IYMVP1KDDM'},
+                  'headers': {}}
+        self.assert_params_for_cmd(cmdline, result, expected_rc=0,
+                                   ignore_params=['payload'])[0]
+
+        

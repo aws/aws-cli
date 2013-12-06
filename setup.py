b9fa85377a2a1fb0857724eddafb2cd14654282a
James Saryerwinnie <js@jamesls.com>
2013-12-05 16:06:00 -0800

    Bumping version to 1.2.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 import awscli
 
 
-requires = ['botocore>=0.26.0,<0.27.0',
+requires = ['botocore>=0.27.0,<0.28.0',
             'bcdoc>=0.12.0,<0.13.0',
             'six>=1.1.0',
             'colorama==0.2.5',

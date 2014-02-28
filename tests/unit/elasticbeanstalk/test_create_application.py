6d7adbfd85ce1992a77a42b8865890db45189ae2
James Saryerwinnie <js@jamesls.com>
2014-02-28 12:25:33 -0800

    Fix unicode argument processing for py2
    In python2, sys.argv is a bytestring of whatever encoding
    is used by the terminal.  In python3, sys.argv is a list of unicode
    strings.  This causes problems because the rest of the code assumes
    unicode.
    
    The fix is to automatically decode to unicode based on sys.stdin
    as soon as we parse the args.
    
    This was originally reported in #593, and
    https://github.com/boto/botocore/pull/218.
    
    I'll need to more investigation to see if this problem applies
    to JSON files via file://, this commit only fixes the case where
    unicode is specified on the command line.

---
+++ tests/unit/elasticbeanstalk/test_create_application.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Copyright 2012-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+from tests.unit import BaseAWSCommandParamsTest, unittest
+import sys
+import six
+
+
+class TestUpdateConfigurationTemplate(BaseAWSCommandParamsTest):
+
+    prefix = 'elasticbeanstalk create-application'
+
+    def test_ascii(self):
+        cmdline = self.prefix
+        cmdline += ' --application-name FooBar'
+        result = {'ApplicationName': 'FooBar',}
+        self.assert_params_for_cmd(cmdline, result)
+
+    @unittest.skipIf(
+        six.PY3, 'Unicode cmd line test only is relevant to python2.')
+    def test_py2_bytestring_unicode(self):
+        # In Python2, sys.argv is a list of bytestrings that are encoded
+        # in whatever encoding the terminal uses.  We have an extra step
+        # where we need to decode the bytestring into unicode.  In
+        # python3, sys.argv is a list of unicode objects so this test
+        # doesn't make sense for python3.
+        cmdline = self.prefix
+        app_name = u'\u2713'
+        cmdline += u' --application-name %s' % app_name
+        cmdline = cmdline.encode(getattr(sys.stdin, 'encoding', 'utf-8'))
+        result = {'ApplicationName': u'\u2713',}
+        self.assert_params_for_cmd(cmdline, result)

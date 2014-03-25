15f7fd82de742f2e7f09dd7031b3a8172c1409ed
Daniel G. Taylor <danielgtaylor@gmail.com>
2014-03-25 14:01:57 -0700

    Bumping version to 1.3.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 import awscli
 
 
-requires = ['botocore>=0.36.0,<0.37.0',
+requires = ['botocore>=0.37.0,<0.38.0',
             'bcdoc>=0.12.0,<0.13.0',
             'six>=1.1.0',
             'colorama==0.2.5',

231bb9c4341e5c47ee93b024befe3b7c5ec9d08b
James Saryerwinnie <js@jamesls.com>
2014-05-16 10:51:03 -0700

    Fix test for py26

--- tests/unit/customizations/s3/test_filters.py
+++ tests/unit/customizations/s3/test_filters.py
@@ -11,7 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 import os
-import unittest
+from tests import unittest
 import platform
 
 from awscli.customizations.s3.fileinfo import FileInfo

14edffd148f062832f379e0c5a0defaf5c6e485e
Todd Lunter <tlunter@gmail.com>
2014-07-30 10:35:53 -0400

    Fix completion tests

--- tests/unit/test_completer.py
+++ tests/unit/test_completer.py
@@ -72,7 +72,7 @@ COMPLETIONS = [
                              '--cache-control', '--content-type',
                              '--content-disposition',
                              '--content-encoding', '--content-language',
-                             '--expires', '--grants'] + GLOBALOPTS)),
+                             '--expires', '--grants', '--stdout'] + GLOBALOPTS)),
     ('aws s3 cp --quiet -', -1, set(['--no-guess-mime-type', '--dryrun',
                                      '--recursive', '--content-type',
                                      '--content-disposition', '--cache-control',
@@ -80,7 +80,7 @@ COMPLETIONS = [
                                      '--expires', '--website-redirect', '--acl',
                                      '--storage-class', '--sse',
                                      '--exclude', '--include',
-                                     '--grants'] + GLOBALOPTS)),
+                                     '--grants', '--stdout'] + GLOBALOPTS)),
     ('aws emr ', -1, set([])),
     ]
 

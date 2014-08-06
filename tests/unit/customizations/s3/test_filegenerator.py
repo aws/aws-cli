499d3afe7dcbdad521d9ccb016aabace5dc698f5
kyleknap <kyleknap@amazon.com>
2014-08-06 13:33:28 -0700

    Fixed a unit test.

--- tests/unit/customizations/s3/test_filegenerator.py
+++ tests/unit/customizations/s3/test_filegenerator.py
@@ -312,7 +312,9 @@ class TestListFilesLocally(unittest.TestCase):
         file_generator = FileGenerator(None, None, None)
         values = list(el[0] for el in file_generator.list_files(
             self.directory, dir_op=True))
-        self.assertEqual(values, list(sorted(values)))
+        ref_vals = list(sorted(values,
+                               key=lambda items: items.replace(os.sep, '/')))
+        self.assertEqual(values, ref_vals)
 
     def test_list_local_files_with_unicode_chars(self):
         p = os.path.join

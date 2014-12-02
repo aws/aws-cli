57ad680d49191cb7b34edd8fe194c31335d8e38e
James Saryerwinnie <js@jamesls.com>
2014-12-01 17:42:01 -0800

    Parse --statistic-values values into a decimal type
    This was failing the improved botocore param validation because
    we were previously setting this value to a string.  We now set
    this value to a decimal.Decimal to ensure we have a valid type
    that will also pass the botocore param validation.
    
    While I was in the code, I also updated the code to use the new
    botocore CamelCasing from metric_data to MetricData.  There was
    also some general test cleanup to switch the tests to
    assert_params_for_cmd2.
    
    Fixes #1036.

--- tests/unit/cloudwatch/test_put_metric_data.py
+++ tests/unit/cloudwatch/test_put_metric_data.py
@@ -11,6 +11,8 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+import decimal
+
 from awscli.testutils import BaseAWSCommandParamsTest
 
 
@@ -20,12 +22,12 @@ class TestPutMetricData(BaseAWSCommandParamsTest):
     prefix = 'cloudwatch put-metric-data '
 
     expected_output = {
-        'MetricData.member.1.MetricName': 'FreeMemoryBytes',
-        'MetricData.member.1.Timestamp': '2013-08-22T10:58:12.283000Z',
-        'MetricData.member.1.Unit': 'Bytes',
-        'MetricData.member.1.Value': 9130160128,
-        'Namespace': '"Foo/Bar"'
-    }
+        'MetricData': [
+            {'MetricName': 'FreeMemoryBytes',
+             'Unit': 'Bytes',
+             'Timestamp': '2013-08-22T10:58:12.283Z',
+             'Value': decimal.Decimal("9130160128")}],
+        'Namespace': '"Foo/Bar"'}
 
     def test_using_json(self):
         args = ('--namespace "Foo/Bar" '
@@ -34,7 +36,7 @@ class TestPutMetricData(BaseAWSCommandParamsTest):
                 '"Timestamp":"2013-08-22T10:58:12.283Z",'
                 '"Value":9130160128}]')
         cmdline = self.prefix + args
-        self.assert_params_for_cmd(cmdline, self.expected_output)
+        self.assert_params_for_cmd2(cmdline, self.expected_output)
 
     def test_using_promoted_params(self):
         # This is equivalent to the json version in test_using_json
@@ -45,4 +47,26 @@ class TestPutMetricData(BaseAWSCommandParamsTest):
                 '--timestamp 2013-08-22T10:58:12.283Z '
                 '--value 9130160128')
         cmdline = self.prefix + args
-        self.assert_params_for_cmd(cmdline, self.expected_output)
+        self.assert_params_for_cmd2(cmdline, self.expected_output)
+
+    def test_using_shorthand_syntax(self):
+        args = (
+            '--metric-name PageViewCount '
+            '--namespace MyService '
+            '--statistic-value Sum=11,Minimum=2,Maximum=5,SampleCount=3 '
+            '--timestamp 2014-02-14T12:00:00.000Z'
+        )
+        cmdline = self.prefix + args
+        expected = {
+            'MetricData': [
+                {'MetricName': 'PageViewCount',
+                 'StatisticValues': {
+                     'Maximum': decimal.Decimal('5'),
+                     'Minimum': decimal.Decimal('2'),
+                     'SampleCount': decimal.Decimal('3'),
+                     'Sum': decimal.Decimal('11')},
+                 'Timestamp': '2014-02-14T12:00:00.000Z'}
+            ],
+            'Namespace': 'MyService'
+        }
+        self.assert_params_for_cmd2(cmdline, expected)

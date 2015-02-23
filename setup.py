c1a7be2e2131c3256540c83a0141a2a4ec0d9ecd
kyleknap <kyleknap@amazon.com>
2015-02-23 10:34:22 -0800

    Bumped CLI dependancy of bcdoc

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import awscli
 
 
 requires = ['botocore>=0.92.0,<0.93.0',
-            'bcdoc>=0.12.0,<0.13.0',
+            'bcdoc>=0.13.0,<0.14.0',
             'colorama>=0.2.5,<=0.3.3',
             'docutils>=0.10',
             'rsa>=3.1.2,<=3.1.4']

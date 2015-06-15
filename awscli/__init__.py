7d41ab224eef8d9bc0229ada71fc1e478d322d78
James Saryerwinnie <js@jamesls.com>
2015-06-15 12:43:56 -0700

    Update env vars to sync with latest botocore changes
    * Need to add another member that includes a conversion func
    * Remove unnecessary var mappings.  These have since been moved
      down into botocore so this mapping no longer needs to be maintained
      at the CLI level.

--- awscli/__init__.py
+++ awscli/__init__.py
@@ -35,11 +35,9 @@ os.environ['AWS_DATA_PATH'] = os.pathsep.join(_awscli_data_path)
 
 
 EnvironmentVariables = {
-    'profile': (None, 'AWS_DEFAULT_PROFILE', None),
-    'region': ('region', 'AWS_DEFAULT_REGION', None),
-    'data_path': ('data_path', 'AWS_DATA_PATH', None),
-    'output': ('output', 'AWS_DEFAULT_OUTPUT', 'json'),
-    }
+    'output': ('output', 'AWS_DEFAULT_OUTPUT', 'json', None),
+}
+
 
 SCALAR_TYPES = set([
     'string', 'float', 'integer', 'long', 'boolean', 'double',

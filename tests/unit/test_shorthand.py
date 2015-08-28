553e5b08fc0e637b600a36bc5ecb1e9c0ef9b27d
James Saryerwinnie <js@jamesls.com>
2015-08-28 15:55:35 -0700

    Remove unused import in test module

--- tests/unit/test_shorthand.py
+++ tests/unit/test_shorthand.py
@@ -10,8 +10,6 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-import decimal
-
 from awscli import shorthand
 from awscli.testutils import unittest
 

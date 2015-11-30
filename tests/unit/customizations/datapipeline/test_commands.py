89737650af805a1ddf48743c025e0042e8997f28
Ivan Orlov <ivanorlo@amazon.com>
2015-11-30 15:12:14 -0800

    Allow output format in datapipeline list-runs

--- tests/unit/customizations/datapipeline/test_commands.py
+++ tests/unit/customizations/datapipeline/test_commands.py
@@ -11,144 +11,133 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+
 import copy
 import mock
+import unittest
 
-from awscli.compat import six
-from awscli.testutils import BaseAWSHelpOutputTest, BaseAWSCommandParamsTest, \
-        unittest
+from awscli.testutils import BaseAWSHelpOutputTest, BaseAWSCommandParamsTest
 
 from awscli.customizations.datapipeline import convert_described_objects
 from awscli.customizations.datapipeline import ListRunsCommand
-from awscli.customizations.datapipeline import ListRunsFormatter
 
 
 API_DESCRIBE_OBJECTS = [
     {"fields": [
-         {
-             "key": "@componentParent",
-             "refValue": "S3Input"
-         },
-         {
-             "key": "@scheduledStartTime",
-             "stringValue": "2013-08-19T20:00:00"
-         },
-         {
-             "key": "parent",
-             "refValue": "S3Input"
-         },
-         {
-             "key": "@sphere",
-             "stringValue": "INSTANCE"
-         },
-         {
-             "key": "type",
-             "stringValue": "S3DataNode"
-         },
-         {
-             "key": "@version",
-             "stringValue": "1"
-         },
-         {
-             "key": "@status",
-             "stringValue": "FINISHED"
-         },
-         {
-             "key": "@actualEndTime",
-             "stringValue": "2014-02-19T19:44:44"
-         },
-         {
-             "key": "@actualStartTime",
-             "stringValue": "2014-02-19T19:44:43"
-         },
-         {
-             "key": "output",
-             "refValue": "@MyCopyActivity_2013-08-19T20:00:00"
-         },
-         {
-             "key": "@scheduledEndTime",
-             "stringValue": "2013-08-19T21:00:00"
-         }
-        ],
-        "id": "@S3Input_2013-08-19T20:00:00",
-        "name": "@S3Input_2013-08-19T20:00:00"
+        {
+            "key": "@componentParent",
+            "refValue": "S3Input"
+        },
+        {
+            "key": "@scheduledStartTime",
+            "stringValue": "2013-08-19T20:00:00"
+        },
+        {
+            "key": "parent",
+            "refValue": "S3Input"
+        },
+        {
+            "key": "@sphere",
+            "stringValue": "INSTANCE"
+        },
+        {
+            "key": "type",
+            "stringValue": "S3DataNode"
+        },
+        {
+            "key": "@version",
+            "stringValue": "1"
+        },
+        {
+            "key": "@status",
+            "stringValue": "FINISHED"
+        },
+        {
+            "key": "@actualEndTime",
+            "stringValue": "2014-02-19T19:44:44"
+        },
+        {
+            "key": "@actualStartTime",
+            "stringValue": "2014-02-19T19:44:43"
+        },
+        {
+            "key": "output",
+            "refValue": "@MyCopyActivity_2013-08-19T20:00:00"
+        },
+        {
+            "key": "@scheduledEndTime",
+            "stringValue": "2013-08-19T21:00:00"
+        }
+    ],
+     "id": "@S3Input_2013-08-19T20:00:00",
+     "name": "@S3Input_2013-08-19T20:00:00"
     },
     {"fields": [
-         {
-             "key": "@componentParent",
-             "refValue": "MyEC2Resource"
-         },
-         {
-             "key": "@resourceId",
-             "stringValue": "i-12345"
-         },
-         {
-             "key": "@scheduledStartTime",
-             "stringValue": "2013-08-19T23:00:00"
-         },
-         {
-             "key": "parent",
-             "refValue": "MyEC2Resource"
-         },
-         {
-             "key": "@sphere",
-             "stringValue": "INSTANCE"
-         },
-         {
-             "key": "@attemptCount",
-             "stringValue": "1"
-         },
-         {
-             "key": "type",
-             "stringValue": "Ec2Resource"
-         },
-         {
-             "key": "@version",
-             "stringValue": "1"
-         },
-         {
-             "key": "@status",
-             "stringValue": "CREATING"
-         },
-         {
-             "key": "input",
-             "refValue": "@MyCopyActivity_2013-08-19T23:00:00"
-         },
-         {
-             "key": "@triesLeft",
-             "stringValue": "2"
-         },
-         {
-             "key": "@actualStartTime",
-             "stringValue": "2014-02-19T19:59:45"
-         },
-         {
-             "key": "@headAttempt",
-             "refValue": "@MyEC2Resource_2013-08-19T23:00:00_Attempt=1"
-         },
-         {
-             "key": "@scheduledEndTime",
-             "stringValue": "2013-08-20T00:00:00"
-         }
-        ],
-        "id": "@MyEC2Resource_2013-08-19T23:00:00",
-        "name": "@MyEC2Resource_2013-08-19T23:00:00"
+        {
+            "key": "@componentParent",
+            "refValue": "MyEC2Resource"
+        },
+        {
+            "key": "@resourceId",
+            "stringValue": "i-12345"
+        },
+        {
+            "key": "@scheduledStartTime",
+            "stringValue": "2013-08-19T23:00:00"
+        },
+        {
+            "key": "parent",
+            "refValue": "MyEC2Resource"
+        },
+        {
+            "key": "@sphere",
+            "stringValue": "INSTANCE"
+        },
+        {
+            "key": "@attemptCount",
+            "stringValue": "1"
+        },
+        {
+            "key": "type",
+            "stringValue": "Ec2Resource"
+        },
+        {
+            "key": "@version",
+            "stringValue": "1"
+        },
+        {
+            "key": "@status",
+            "stringValue": "CREATING"
+        },
+        {
+            "key": "input",
+            "refValue": "@MyCopyActivity_2013-08-19T23:00:00"
+        },
+        {
+            "key": "@triesLeft",
+            "stringValue": "2"
+        },
+        {
+            "key": "@actualStartTime",
+            "stringValue": "2014-02-19T19:59:45"
+        },
+        {
+            "key": "@headAttempt",
+            "refValue": "@MyEC2Resource_2013-08-19T23:00:00_Attempt=1"
+        },
+        {
+            "key": "@scheduledEndTime",
+            "stringValue": "2013-08-20T00:00:00"
+        }
+    ],
+     "id": "@MyEC2Resource_2013-08-19T23:00:00",
+     "name": "@MyEC2Resource_2013-08-19T23:00:00"
     }
 ]
 
-EMPTY_RUNS = """\
-       Name                                                Scheduled Start      Status
-       ID                                                  Started              Ended
----------------------------------------------------------------------------------------------------
-"""
+JSON_FORMATTER_PATH = 'awscli.formatter.JSONFormatter.__call__'
+LIST_FORMATTER_PATH = 'awscli.customizations.datapipeline.listrunsformatter.ListRunsFormatter.__call__'  # noqa
 
-SINGLE_ROW_RUN = """\
-       Name                                                Scheduled Start      Status
-       ID                                                  Started              Ended
----------------------------------------------------------------------------------------------------
-   1.  parent                                              now                  status
-       id                                                  actualStartTime      actualEndTime
-"""
 
 class TestConvertObjects(unittest.TestCase):
 
@@ -183,6 +172,8 @@ class FakeParsedArgs(object):
         self.endpoint_url = None
         self.region = None
         self.verify_ssl = None
+        self.output = None
+        self.query = None
         self.__dict__.update(kwargs)
 
 
@@ -195,22 +186,48 @@ class TestCommandsRunProperly(BaseAWSCommandParamsTest):
         self.client.get_paginator.return_value = self.query_objects
         self.client.describe_objects = self.describe_objects
 
-    def test_list_runs(self):
         self.driver.session = mock.Mock()
         self.driver.session.emit_first_non_none_response.return_value = None
         self.driver.session.create_client.return_value = self.client
         self.query_objects.paginate.return_value.build_full_result.\
-            return_value = {
-                'ids': ['object-ids']}
-        self.describe_objects.return_value = {'pipelineObjects': [
-            {'fields': [], 'id': 'id', 'name': 'name'}]}
+            return_value = {'ids': ['object-ids']}
+        self.describe_objects.return_value = \
+            {'pipelineObjects': API_DESCRIBE_OBJECTS}
+        self.expected_response = convert_described_objects(
+            API_DESCRIBE_OBJECTS,
+            sort_key_func=lambda x: (x['@scheduledStartTime'], x['name']))
 
-        command = ListRunsCommand(self.driver.session, formatter=mock.Mock())
+    def test_list_runs(self):
+        command = ListRunsCommand(self.driver.session)
         command(['--pipeline-id', 'my-pipeline-id'],
                 parsed_globals=FakeParsedArgs(region='us-east-1'))
         self.assertTrue(self.query_objects.paginate.called)
         self.describe_objects.assert_called_with(
             pipelineId='my-pipeline-id', objectIds=['object-ids'])
+
+    @mock.patch(JSON_FORMATTER_PATH)
+    @mock.patch(LIST_FORMATTER_PATH)
+    def test_list_runs_formatter_explicit_choice(
+            self, list_formatter, json_formatter):
+        command = ListRunsCommand(self.driver.session)
+        command(['--pipeline-id', 'my-pipeline-id'],
+                parsed_globals=FakeParsedArgs(
+                    region='us-east-1', output='json'))
+        json_formatter.assert_called_once_with(
+            'list-runs', self.expected_response)
+        self.assertFalse(list_formatter.called)
+
+    @mock.patch(JSON_FORMATTER_PATH)
+    @mock.patch(LIST_FORMATTER_PATH)
+    def test_list_runs_formatter_implicit_choice(
+            self, list_formatter, json_formatter):
+
+        command = ListRunsCommand(self.driver.session)
+        command(['--pipeline-id', 'my-pipeline-id'],
+                parsed_globals=FakeParsedArgs(region='us-east-1'))
+        list_formatter.assert_called_once_with(
+            'list-runs', self.expected_response)
+        self.assertFalse(json_formatter.called)
 
 
 class TestHelpOutput(BaseAWSHelpOutputTest):
@@ -219,36 +236,3 @@ class TestHelpOutput(BaseAWSHelpOutputTest):
         self.assert_contains('pipeline definition')
         # The previous API docs should not be in the output
         self.assert_not_contains('pipelineObjects')
-
-
-class TestListRunsFormatter(BaseAWSCommandParamsTest):
-    def test_no_runs_available(self):
-        stream = six.StringIO()
-        formatter = ListRunsFormatter(stream)
-        objects = []
-        formatter.display_objects_to_user(objects)
-        self.assertEqual(
-            [line.strip() for line in stream.getvalue().splitlines()],
-            [line.strip() for line in EMPTY_RUNS.splitlines()])
-
-    def test_single_row(self):
-        objects = [
-            {'@componentParent': 'parent',
-             '@id': 'id',
-             '@scheduledStartTime': 'now',
-             '@status': 'status',
-             '@actualStartTime': 'actualStartTime',
-             '@actualEndTime': 'actualEndTime',
-            }
-        ]
-        stream = six.StringIO()
-        formatter = ListRunsFormatter(stream)
-        formatter.display_objects_to_user(objects)
-        # Rather than stream.getvalue() == SINGLE_ROW_RUN
-        # we compare equality like this to avoid test failures
-        # for differences in leading/trailing whitespace and empty lines.
-        self.assertEqual(
-            [line.strip() for line in stream.getvalue().splitlines()
-             if line.strip()],
-            [line.strip() for line in SINGLE_ROW_RUN.splitlines()
-             if line.strip()])

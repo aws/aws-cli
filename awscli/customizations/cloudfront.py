fd0edd44ac4905571a411ab55d4941d79a1fff2f
Ray Luo <rayluo@amazon.com>
2015-11-30 23:05:17 -0800

    cloudfront create-invalidation --paths

---
+++ awscli/customizations/cloudfront.py
@@ -0,0 +1,46 @@
+# Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+from datetime import datetime
+
+from awscli.arguments import CustomArgument
+from awscli.customizations.utils import validate_mutually_exclusive_handler
+
+
+def register(event_handler):
+    """Provides a simpler --paths for ``aws cloudfront create-invalidation``"""
+
+    event_handler.register(
+        'building-argument-table.cloudfront.create-invalidation', _add_paths)
+    event_handler.register(
+        'operation-args-parsed.cloudfront.create-invalidation',
+        validate_mutually_exclusive_handler(['invalidation_batch'], ['paths']))
+
+
+def _add_paths(argument_table, **kwargs):
+    argument_table['invalidation-batch'].required = False
+    argument_table['paths'] = PathsArgument()
+
+
+class PathsArgument(CustomArgument):
+
+    def __init__(self):
+        help = """The space-separated paths to be invalidated.
+            It will automatically generate a CallerReference for you."""
+        super(PathsArgument, self).__init__('paths', nargs='+', help_text=help)
+
+    def add_to_params(self, parameters, value):
+        if value:
+            parameters['InvalidationBatch'] = {
+                "CallerReference": datetime.utcnow().strftime('%Y%m%d%H%M%S'),
+                "Paths": {"Quantity": len(value), "Items": value},
+                }
--- awscli/examples/cloudfront/create-invalidation.rst
+++ awscli/examples/cloudfront/create-invalidation.rst
@@ -1,5 +1,10 @@
 The following command creates an invalidation for a CloudFront distribution with the ID ``S11A16G5KZMEQD``::
 
+  aws cloudfront create-invalidation --distribution-id S11A16G5KZMEQD \
+    --paths /index.html /error.html
+
+Or you can use the following command to do the same thing. And you can have a chance to specify your own CallerReference here::
+
   aws cloudfront create-invalidation --invalidation-batch file://invbatch.json --distribution-id S11A16G5KZMEQD
 
 The distribution ID is available in the output of ``create-distribution`` and ``list-distributions``.
@@ -14,7 +19,7 @@ The file ``invbatch.json`` is a JSON document in the current folder that specifi
     "CallerReference": "my-invalidation-2015-09-01"
   }
 
-Output::
+Output of both commands::
 
   {
       "Invalidation": {

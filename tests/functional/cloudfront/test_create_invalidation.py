0fbdab252cf8bc303bf5ca29ee5e704527fd7163
Ray Luo <rayluo@amazon.com>
2015-12-01 09:56:17 -0800

    Cloudfront is currently still in preview mode

--- tests/functional/cloudfront/test_create_invalidation.py
+++ tests/functional/cloudfront/test_create_invalidation.py
@@ -10,7 +10,9 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-from awscli.testutils import BaseAWSCommandParamsTest
+from awscli.testutils import BaseAWSPreviewCommandParamsTest as \
+    BaseAWSCommandParamsTest
+
 
 
 class TestCreateInvalidation(BaseAWSCommandParamsTest):

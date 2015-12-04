3dddc8b356a3de7925f5bd9dd2814a7c191a4740
Ray Luo <rayluo@amazon.com>
2015-12-04 02:08:35 -0800

    Implement cloudfront sign

--- awscli/customizations/cloudfront.py
+++ awscli/customizations/cloudfront.py
@@ -13,13 +13,19 @@
 import time
 import random
 
+import rsa
+from botocore.utils import parse_to_aware_datetime
+from botocore.signers import CloudFrontSigner
+
 from awscli.arguments import CustomArgument
 from awscli.customizations.utils import validate_mutually_exclusive_handler
+from awscli.customizations.commands import BasicCommand
 
 
 def register(event_handler):
-    """Provides a simpler --paths for ``aws cloudfront create-invalidation``"""
+    event_handler.register('building-command-table.cloudfront', _add_sign)
 
+    # Provides a simpler --paths for ``aws cloudfront create-invalidation``
     event_handler.register(
         'building-argument-table.cloudfront.create-invalidation', _add_paths)
     event_handler.register(
@@ -49,3 +55,64 @@ class PathsArgument(CustomArgument):
                 "CallerReference": caller_reference,
                 "Paths": {"Quantity": len(value), "Items": value},
                 }
+
+
+def _add_sign(command_table, session, **kwargs):
+    command_table['sign'] = SignCommand(session)
+
+
+class SignCommand(BasicCommand):
+    NAME = 'sign'
+    DESCRIPTION = 'Sign a given url.'
+    DATE_HELP = """Supported formats include:
+        YYYY-MM-DD (which means 0AM UTC of that day),
+        YYYY-MM-DDThh:mm:ss (with default timezone as UTC),
+        YYYY-MM-DDThh:mm:ss+hh:mm or YYYY-MM-DDThh:mm:ss-hh:mm (with offset),
+        or EpochTime.
+        Do NOT use YYYYMMDD, because it will be treated as EpochTime."""
+    ARG_TABLE = [
+        {
+            'name': 'url',
+            'no_paramfile': True,  # To disable the default paramfile behavior
+            'required': True,
+            'help_text': 'The URL to be signed',
+        },
+        {
+            'name': 'key-pair-id',
+            'required': True,
+            'help_text': 'The ID of your CloudFront key pairs.',
+        },
+        {
+            'name': 'private-key',
+            'required': True,
+            'help_text': 'file://path/to/your/private-key.pem',
+        },
+        {'name': 'date-less-than', 'required': True, 'help_text': DATE_HELP},
+        {'name': 'date-greater-than', 'help_text': DATE_HELP},
+        {'name': 'ip-address', 'help_text': 'Format: x.x.x.x/x or x.x.x.x'},
+    ]
+
+    def _run_main(self, args, parsed_globals):
+        signer = CloudFrontSigner(
+            args.key_pair_id, RsaSigner(args.private_key).sign)
+        date_less_than = parse_to_aware_datetime(args.date_less_than)
+        if args.date_greater_than is not None:
+            date_greater_than = parse_to_aware_datetime(args.date_greater_than)
+        else:
+            date_greater_than = None
+        if date_greater_than is not None or args.ip_address is not None:
+            policy = signer.build_policy(
+                args.url, date_less_than, date_greater_than=date_greater_than,
+                ip_address=args.ip_address)
+            print(signer.generate_presigned_url(args.url, policy=policy))
+        else:
+            print(signer.generate_presigned_url(args.url, date_less_than))
+        return 0
+
+
+class RsaSigner(object):
+    def __init__(self, private_key):
+        self.priv_key = rsa.PrivateKey.load_pkcs1(private_key.encode('utf8'))
+
+    def sign(self, message):
+        return rsa.sign(message, self.priv_key, 'SHA-1')

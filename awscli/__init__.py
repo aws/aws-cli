aa41c2ed5acde2008aa3262221346b5c236d0aac
AWS <aws@amazon.com>
2016-01-06 23:04:15 +0000

    Bumping version to 1.9.17

--- awscli/__init__.py
+++ awscli/__init__.py
@@ -17,7 +17,7 @@ A Universal Command Line Environment for Amazon Web Services.
 """
 import os
 
-__version__ = '1.9.16'
+__version__ = '1.9.17'
 
 #
 # Get our data path to be added to botocore's search path

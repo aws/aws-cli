aws/aws-cli

commit 16a59ac0b55a40aa81428844d48659ea76ea91f4

Properly configure loader sharing in tests
The loader argument to the session has been a no op
for a while (being removed in
https://github.com/boto/botocore/pull/579).
The way to set a loader is to just use register_component().

This gives shaves about 4 seconds off of the total unit test time
for me.

awscli/testutils.py
@@ -182,7 +182,8 @@ class BaseCLIDriverTest(unittest.TestCase):
         self.environ_patch = mock.patch('os.environ', self.environ)
         self.environ_patch.start()
         emitter = HierarchicalEmitter()
-        session = Session(EnvironmentVariables, emitter, loader=_LOADER)
+        session = Session(EnvironmentVariables, emitter)
+        session.register_component('data_loader', _LOADER)
         load_plugins({}, event_hooks=emitter)
         driver = CLIDriver(session=session)
         self.session = session

commit 16a14995f989343d9b9c0d9d9ce90d99530817b9

Fix ordering issue with tests
Fixes an regression introduced in
16a59ac0b55a40aa81428844d48659ea76ea91f4.

The loaders are not being configured with the data path needed to
load the CLI data files.  This means that if you try to run any
test that used the base class where this loader was created,
you'll get DataNotFound errors.

However, this is a shared object to speed up tests.  This means
that as long as something comes along and configures the loader
properly (another base class does this properly), then the
other tests will pass as expected.

The fix here is to use the create_clidriver() function which
abstracts this logic for you.

Now you can run individual unit/functional tests without
getting these errors.

awscli/testutils.py
@@ -211,13 +211,8 @@ class BaseCLIDriverTest(unittest.TestCase):
         }
         self.environ_patch = mock.patch('os.environ', self.environ)
         self.environ_patch.start()
-        emitter = HierarchicalEmitter()
-        session = Session(EnvironmentVariables, emitter)
-        session.register_component('data_loader', _LOADER)
-        load_plugins({}, event_hooks=emitter)
-        driver = CLIDriver(session=session)
-        self.session = session
-        self.driver = driver
+        self.driver = create_clidriver()
+        self.session = self.driver.session
 
     def tearDown(self):
         self.environ_patch.stop()
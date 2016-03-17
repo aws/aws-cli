74b51950c4b57955a85a0562b808ac5106c5f4d8
JordonPhillips <phjordon@amazon.com>
2016-03-16 17:18:13 -0700

    Fix spelling error in the add model command docs

--- awscli/customizations/configure/addmodel.py
+++ awscli/customizations/configure/addmodel.py
@@ -85,7 +85,7 @@ def get_model_location(session, service_definition, service_name=None):
 
 class AddModelCommand(BasicCommand):
     NAME = 'add-model'
-    DESCRITPION = (
+    DESCRIPTION = (
         'Adds a service JSON model to the appropriate location in '
         '~/.aws/models. Once the model gets added, CLI commands and Boto3 '
         'clients will be immediately available for the service JSON model '

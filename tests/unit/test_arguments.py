cd10c78fa26d78671af84bfe6fcc90c8c55b839d
JordonPhillips <phjordon@amazon.com>
2016-05-04 16:21:03 -0700

    Use service_name in process-cli-arg event
    Currently we use the endpoint prefix when generating this event, which
    means service whose names we have manually changed will emit events with
    the old name. For example, elb will emit as elasticloadbalancing. This
    makes registering event handlers confusing, especially since most other
    event handlers use the service name. Furthermore, multiple services can
    use the same endpoint, so this could be a source of bugs if not changed.
    
    This changes the event from
    process-cli-arg.endpoint-prefix.operation-name to
    process-cli-arg.service-name.operation-name

--- tests/unit/test_arguments.py
+++ tests/unit/test_arguments.py
@@ -10,8 +10,12 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+import mock
+
 from awscli.testutils import unittest
 from awscli import arguments
+from botocore.model import StringShape, OperationModel, ServiceModel
+
 
 class DemoArgument(arguments.CustomArgument):
     pass
@@ -23,3 +27,48 @@ class TestArgumentClasses(unittest.TestCase):
         self.assertFalse(arg.required)
         arg.required = True
         self.assertTrue(arg.required)
+
+
+class TestCLIArgument(unittest.TestCase):
+    def setUp(self):
+        self.service_name = 'baz'
+        self.service_model = ServiceModel({
+            'metadata': {
+                'endpointPrefix': 'bad',
+            },
+            'operations': {
+                'SampleOperation': {
+                    'name': 'SampleOperation',
+                    'input': {'shape': 'Input'}
+                }
+            },
+            'shapes': {
+                'StringShape': {'type': 'string'},
+                'Input': {
+                    'type': 'structure',
+                    'members': {
+                        'Foo': {'shape': 'StringShape'}
+                    }
+                }
+            }
+        }, self.service_name)
+        self.operation_model = self.service_model.operation_model(
+            'SampleOperation')
+        self.argument_model = self.operation_model.input_shape.members['Foo']
+        self.event_emitter = mock.Mock()
+
+    def create_argument(self):
+        return arguments.CLIArgument(
+            self.argument_model.name, self.argument_model,
+            self.operation_model, self.event_emitter)
+
+    def test_unpack_uses_service_name_in_event(self):
+        self.event_emitter.emit.return_value = ['value']
+        argument = self.create_argument()
+        params = {}
+        argument.add_to_params(params, 'value')
+        expected_event_name = 'process-cli-arg.%s.%s' % (
+            self.service_name, 'sample-operation')
+        actual_event_name = self.event_emitter.emit.call_args[0][0]
+        self.assertEqual(actual_event_name, expected_event_name)
+

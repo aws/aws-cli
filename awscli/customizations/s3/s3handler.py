807a3d5d226e3a58454f10fdbf414295fba91b34
kyleknap <kyleknap@amazon.com>
2016-08-22 14:24:09 -0700

    Add warnings to transfer request submitter
    This allows the submitters to warn of potential fileinfo it receives and
    potentially skip them if it know it will not be able to transfer it

--- awscli/customizations/s3/s3handler.py
+++ awscli/customizations/s3/s3handler.py
@@ -19,7 +19,8 @@ import sys
 from s3transfer.manager import TransferManager
 
 from awscli.customizations.s3.utils import (
-    find_chunksize, adjust_chunksize_to_upload_limits, MAX_UPLOAD_SIZE,
+    find_chunksize, human_readable_size,
+    adjust_chunksize_to_upload_limits, MAX_UPLOAD_SIZE,
     find_bucket_key, relative_path, PrintTask, create_warning,
     NonSeekableStream)
 from awscli.customizations.s3.executor import Executor
@@ -541,18 +542,21 @@ class BaseTransferRequestSubmitter(object):
         self._transfer_manager = transfer_manager
         self._result_queue = result_queue
         self._cli_params = cli_params
+        self._warning_hanlders = []
 
     def submit(self, fileinfo):
         """Submits a transfer request based on the FileInfo provided
 
+        There is no guarantee that the transfer request will be made on
+        behalf of the fileinfo as a fileinfo may be skipped based on
+        circumstances in which the transfer is not possible.
+
         :param fileinfo: The FileInfo to be used to submit a transfer
             request to the underlying transfer manager.
         """
-        extra_args = {}
-        self.REQUEST_MAPPER_METHOD(extra_args, self._cli_params)
-        subscribers = [self.RESULT_SUBSCRIBER_CLASS(self._result_queue)]
-        self._add_additional_subscribers(subscribers, fileinfo)
-        self._submit_transfer_request(fileinfo, extra_args, subscribers)
+        should_skip = self._warn_and_signal_if_skip(fileinfo)
+        if not should_skip:
+            self._do_submit(fileinfo)
 
     def is_valid_submission(self, fileinfo):
         """Checks whether it is valid to submit a particular FileInfo
@@ -565,17 +569,59 @@ class BaseTransferRequestSubmitter(object):
         """
         raise NotImplementedError('is_valid_to_submit()')
 
+    def _do_submit(self, fileinfo):
+        extra_args = {}
+        self.REQUEST_MAPPER_METHOD(extra_args, self._cli_params)
+        subscribers = [self.RESULT_SUBSCRIBER_CLASS(self._result_queue)]
+        self._add_additional_subscribers(subscribers, fileinfo)
+        self._submit_transfer_request(fileinfo, extra_args, subscribers)
+
     def _add_additional_subscribers(self, subscribers, fileinfo):
         pass
 
     def _submit_transfer_request(self, fileinfo, extra_args, subscribers):
         raise NotImplementedError('_submit_transfer_request()')
 
+    def _warn_and_signal_if_skip(self, fileinfo):
+        for warning_handler in self._get_warning_handlers():
+            if warning_handler(fileinfo):
+                # On the first warning handler that returns a signal to skip
+                # immediately propogate this signal and no longer check
+                # the other warning handlers as no matter what the file will
+                # be skipped.
+                return True
+
+    def _get_warning_handlers(self):
+        # Returns a list of warning handlers, which are callables that
+        # take in a single parameter representing a FileInfo. It will then
+        # add a warning to result_queue if needed and return True if
+        # that FileInfo should be skipped.
+        return []
+
     def _should_inject_content_type(self):
         return (
             self._cli_params.get('guess_mime_type') and
             not self._cli_params.get('content_type')
         )
+
+    def _warn_glacier(self, fileinfo):
+        if not self._cli_params.get('force_glacier_transfer'):
+            if not fileinfo.is_glacier_compatible():
+                LOGGER.debug(
+                    'Encountered glacier object s3://%s. Not performing '
+                    '%s on object.' % (fileinfo.src, fileinfo.operation_name))
+                if not self._cli_params.get('ignore_glacier_warnings'):
+                    warning = create_warning(
+                        's3://'+fileinfo.src,
+                        'Object is of storage class GLACIER. Unable to '
+                        'perform %s operations on GLACIER objects. You must '
+                        'restore the object to be able to the perform '
+                        'operation.' %
+                        fileinfo.operation_name
+                    )
+                    self._result_queue.put(warning)
+                return True
+        return False
 
 
 class UploadRequestSubmitter(BaseTransferRequestSubmitter):
@@ -597,6 +643,19 @@ class UploadRequestSubmitter(BaseTransferRequestSubmitter):
             extra_args=extra_args, subscribers=subscribers
         )
 
+    def _get_warning_handlers(self):
+        return [self._warn_if_too_large]
+
+    def _warn_if_too_large(self, fileinfo):
+        if getattr(fileinfo, 'size') and fileinfo.size > MAX_UPLOAD_SIZE:
+            file_path = relative_path(fileinfo.src)
+            warning_message = (
+                "File %s exceeds s3 upload limit of %s." % (
+                    file_path, human_readable_size(MAX_UPLOAD_SIZE)))
+            warning = create_warning(
+                file_path, warning_message, skip_file=False)
+            self._result_queue.put(warning)
+
 
 class DownloadRequestSubmitter(BaseTransferRequestSubmitter):
     REQUEST_MAPPER_METHOD = RequestParamsMapper.map_get_object_params
@@ -617,6 +676,9 @@ class DownloadRequestSubmitter(BaseTransferRequestSubmitter):
             fileobj=fileinfo.dest, bucket=bucket, key=key,
             extra_args=extra_args, subscribers=subscribers
         )
+
+    def _get_warning_handlers(self):
+        return [self._warn_glacier]
 
 
 class CopyRequestSubmitter(BaseTransferRequestSubmitter):
@@ -640,3 +702,6 @@ class CopyRequestSubmitter(BaseTransferRequestSubmitter):
             extra_args=extra_args, subscribers=subscribers,
             source_client=fileinfo.source_client
         )
+
+    def _get_warning_handlers(self):
+        return [self._warn_glacier]

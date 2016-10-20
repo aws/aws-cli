a4aefb6a53889a21819fab1b14dc40fe0312e7d4
JordonPhillips <phjordon@amazon.com>
2016-10-20 16:52:09 -0700

    Update minimum required s3transfer version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requires = ['botocore==1.4.64',
             'colorama>=0.2.5,<=0.3.7',
             'docutils>=0.10',
             'rsa>=3.1.2,<=3.5.0',
-            's3transfer>=0.1.5,<0.2.0']
+            's3transfer>=0.1.9,<0.2.0']
 
 
 if sys.version_info[:2] == (2, 6):

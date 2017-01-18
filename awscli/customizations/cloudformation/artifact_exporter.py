ee55cedf67d80647acd55d2d2b8ce525b87667bf
kyleknap <kyleknap@amazon.com>
2017-01-18 14:54:26 -0800

    Fix an import
    Was importing from indirect six dependency when should have been using the
    vendored dependency or the urlparse module from compat

--- awscli/customizations/cloudformation/artifact_exporter.py
+++ awscli/customizations/cloudformation/artifact_exporter.py
@@ -19,7 +19,7 @@ import contextlib
 import uuid
 from awscli.compat import six
 
-from six.moves.urllib.parse import urlparse, parse_qs
+from awscli.compat import urlparse
 from contextlib import contextmanager
 from awscli.customizations.cloudformation import exceptions
 from awscli.customizations.cloudformation.yamlhelper import yaml_dump, \
@@ -66,8 +66,8 @@ def parse_s3_url(url,
         # Python < 2.7.10 don't parse query parameters from URI with custom
         # scheme such as s3://blah/blah. As a workaround, remove scheme
         # altogether to trigger the parser "s3://foo/bar?v=1" =>"//foo/bar?v=1"
-        parsed = urlparse(url[3:])
-        query = parse_qs(parsed.query)
+        parsed = urlparse.urlparse(url[3:])
+        query = urlparse.parse_qs(parsed.query)
 
         if parsed.netloc and parsed.path:
             result = dict()

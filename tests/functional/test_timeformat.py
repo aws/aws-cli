3343a05e08271dc40bac9bc4ac22426bd48eec64
jcarlyl <jcarlyl@amazon.com>
2017-01-24 17:01:29 -0800

    Clean tests

--- tests/functional/test_timeformat.py
+++ tests/functional/test_timeformat.py
@@ -22,12 +22,6 @@ from awscli.clidriver import create_clidriver
 class TestCLITimestampParser(unittest.TestCase):
     def setUp(self):
         self.files = FileCreator()
-        self.files.create_file('iso',
-                               '[default]\ncli_timestamp_format = iso8601\n')
-        self.files.create_file('none',
-                               '[default]\ncli_timestamp_format = none\n')
-        self.files.create_file('blank', '')
-
         self.environ = {
             'AWS_DATA_PATH': os.environ['AWS_DATA_PATH'],
             'AWS_DEFAULT_REGION': 'us-east-1',
@@ -38,29 +32,28 @@ class TestCLITimestampParser(unittest.TestCase):
         self.environ_patch = mock.patch('os.environ', self.environ)
         self.environ_patch.start()
 
-        self.epoch = datetime.datetime.fromtimestamp(0)
         self.wire_response = json.dumps({
             'builds': [{
                 'startTime': 0,
             }]
-        })
+        }).encode()
 
     def tearDown(self):
         self.environ_patch.stop()
         self.files.remove_all()
 
-    def load_config(self, config_name):
-        self.environ['AWS_CONFIG_FILE'] = self.files.full_path(config_name)
-        self.driver = create_clidriver()
-
     def test_iso(self):
-        self.load_config('iso')
-        expected_time = self.epoch.replace(tzinfo=tzlocal()).isoformat()
+        self.environ['AWS_CONFIG_FILE'] = self.files.create_file(
+            'iso',
+            '[default]\ncli_timestamp_format = iso8601\n')
+        self.driver = create_clidriver()
+        expected_time = datetime.datetime.fromtimestamp(0).replace(
+            tzinfo=tzlocal()).isoformat()
 
         with capture_output() as captured:
             with mock.patch('botocore.endpoint.Session.send') as _send:
                 _send.return_value = mock.Mock(status_code=200, headers={},
-                                               content=self.wire_response.encode())
+                                               content=self.wire_response)
                 self.driver.main(['codebuild', 'batch-get-builds',
                                   '--ids', 'foo'])
                 json_response = json.loads(captured.stdout.getvalue())
@@ -68,27 +61,30 @@ class TestCLITimestampParser(unittest.TestCase):
                 self.assertEqual(expected_time, start_time)
 
     def test_none(self):
-        self.load_config('none')
+        self.environ['AWS_CONFIG_FILE'] = self.files.create_file(
+            'none',
+            '[default]\ncli_timestamp_format = none\n')
+        self.driver = create_clidriver()
         expected_time = 0
 
         with capture_output() as captured:
             with mock.patch('botocore.endpoint.Session.send') as _send:
                 _send.return_value = mock.Mock(status_code=200, headers={},
-                                               content=self.wire_response.encode())
+                                               content=self.wire_response)
                 self.driver.main(['codebuild', 'batch-get-builds',
                                   '--ids', 'foo'])
                 json_response = json.loads(captured.stdout.getvalue())
                 start_time = json_response["builds"][0]["startTime"]
                 self.assertEqual(expected_time, start_time)
 
-    def test_absent(self):
-        self.load_config('blank')
+    def test_default(self):
+        self.driver = create_clidriver()
         expected_time = 0
 
         with capture_output() as captured:
             with mock.patch('botocore.endpoint.Session.send') as _send:
                 _send.return_value = mock.Mock(status_code=200, headers={},
-                                               content=self.wire_response.encode())
+                                               content=self.wire_response)
                 self.driver.main(['codebuild', 'batch-get-builds',
                                   '--ids', 'foo'])
                 json_response = json.loads(captured.stdout.getvalue())

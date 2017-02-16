9e096d56d6a7839869443731cea0071f32c415d9
AWS <aws@amazon.com>
2017-02-16 15:00:17 -0800

    Bumping version to 1.11.50

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 import awscli
 
 
-requires = ['botocore==1.5.12',
+requires = ['botocore==1.5.13',
             'colorama>=0.2.5,<=0.3.7',
             'docutils>=0.10',
             'rsa>=3.1.2,<=3.5.0',

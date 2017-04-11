6305cf52bd8f15c4a9813f9df0310524626814e1
Donald Stufft <donald@stufft.io>
2017-04-11 13:09:40 -0400

    Isolate the unicode test from the environment

--- tests/unit/test_clidriver.py
+++ tests/unit/test_clidriver.py
@@ -298,7 +298,8 @@ class TestCliDriver(unittest.TestCase):
         fake_client.can_paginate.return_value = False
         driver.session.create_client = mock.Mock(return_value=fake_client)
         with mock.patch("sys.stderr", stderr):
-            rc = driver.main('s3 list-objects --bucket foo'.split())
+            with mock.patch("locale.getpreferredencoding", lambda: "UTF-8"):
+                rc = driver.main('s3 list-objects --bucket foo'.split())
         stderr.flush()
         self.assertEqual(rc, 255)
         self.assertEqual(stderr_b.getvalue(), u"\n☃\n".encode("UTF-8"))

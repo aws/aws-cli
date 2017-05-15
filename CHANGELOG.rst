f24fc4040a28843a56a91c67d9a9786c275d0dd2
awstools <aws-dr-tools-github-user@amazon.com>
2017-05-15 13:46:42 -0700

    Bumping version to 1.11.86

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,12 @@
 CHANGELOG
 =========
 
+1.11.86
+=======
+
+* api-change:``ssm``: Update ssm command to latest version
+
+
 1.11.85
 =======
 
--- awscli/__init__.py
+++ awscli/__init__.py
@@ -17,7 +17,7 @@ A Universal Command Line Environment for Amazon Web Services.
 """
 import os
 
-__version__ = '1.11.85'
+__version__ = '1.11.86'
 
 #
 # Get our data path to be added to botocore's search path

d21c4a0937ceb098467a8455e34e6668d89ccf2e
awstools <aws-dr-tools-github-user@amazon.com>
2017-08-23 12:44:37 -0700

    Bumping version to 1.11.139

--- awscli/__init__.py
+++ awscli/__init__.py
@@ -17,7 +17,7 @@ A Universal Command Line Environment for Amazon Web Services.
 """
 import os
 
-__version__ = '1.11.138'
+__version__ = '1.11.139'
 
 #
 # Get our data path to be added to botocore's search path

4ff6adaa398bac7f2812fa1ef303af148973abc7
kyleknap <kyleknap@amazon.com>
2017-11-26 00:21:57 -0800

    Fix tests related to making module recorder

--- tests/unit/customizations/history/test_history.py
+++ tests/unit/customizations/history/test_history.py
@@ -32,8 +32,9 @@ class TestAttachHistoryHander(unittest.TestCase):
         self.files.remove_all()
 
     @mock.patch('awscli.customizations.history.sqlite3')
-    @mock.patch('awscli.customizations.history.get_global_history_recorder')
-    def test_attach_history_handler(self, mock_get_recorder, mock_sqlite3):
+    @mock.patch('awscli.customizations.history.HISTORY_RECORDER',
+                spec=HistoryRecorder)
+    def test_attach_history_handler(self, mock_recorder, mock_sqlite3):
         mock_session = mock.Mock(Session)
         mock_session.get_scoped_config.return_value = {
             'cli_history': 'enabled'
@@ -42,18 +43,16 @@ class TestAttachHistoryHander(unittest.TestCase):
         parsed_args = argparse.Namespace()
         parsed_args.command = 's3'
 
-        mock_recorder = mock.Mock(HistoryRecorder)
-        mock_get_recorder.return_value = mock_recorder
-
         attach_history_handler(session=mock_session, parsed_args=parsed_args)
         self.assertEqual(mock_recorder.add_handler.call_count, 1)
         self.assertIsInstance(
             mock_recorder.add_handler.call_args[0][0], DatabaseHistoryHandler)
 
     @mock.patch('awscli.customizations.history.sqlite3')
-    @mock.patch('awscli.customizations.history.get_global_history_recorder')
+    @mock.patch('awscli.customizations.history.HISTORY_RECORDER',
+                spec=HistoryRecorder)
     def test_no_attach_history_handler_when_history_not_configured(
-            self, mock_get_recorder, mock_sqlite3):
+            self, mock_recorder, mock_sqlite3):
         mock_session = mock.Mock(Session)
         mock_session.get_scoped_config.return_value = {}
 
@@ -61,12 +60,13 @@ class TestAttachHistoryHander(unittest.TestCase):
         parsed_args.command = 's3'
 
         attach_history_handler(session=mock_session, parsed_args=parsed_args)
-        self.assertFalse(mock_get_recorder.called)
+        self.assertFalse(mock_recorder.add_handler.called)
 
     @mock.patch('awscli.customizations.history.sqlite3')
-    @mock.patch('awscli.customizations.history.get_global_history_recorder')
+    @mock.patch('awscli.customizations.history.HISTORY_RECORDER',
+                spec=HistoryRecorder)
     def test_no_attach_history_handler_when_command_is_history(
-            self, mock_get_recorder, mock_sqlite3):
+            self, mock_recorder, mock_sqlite3):
         mock_session = mock.Mock(Session)
         mock_session.get_scoped_config.return_value = {
             'cli_history': 'enabled'
@@ -76,12 +76,13 @@ class TestAttachHistoryHander(unittest.TestCase):
         parsed_args.command = 'history'
 
         attach_history_handler(session=mock_session, parsed_args=parsed_args)
-        self.assertFalse(mock_get_recorder.called)
+        self.assertFalse(mock_recorder.add_handler.called)
 
     @mock.patch('awscli.customizations.history.sqlite3', None)
-    @mock.patch('awscli.customizations.history.get_global_history_recorder')
+    @mock.patch('awscli.customizations.history.HISTORY_RECORDER',
+                spec=HistoryRecorder)
     def test_no_attach_history_handler_when_no_sqlite3(
-            self, mock_get_recorder):
+            self, mock_recorder):
         mock_session = mock.Mock(Session)
         mock_session.get_scoped_config.return_value = {
             'cli_history': 'enabled'
@@ -95,11 +96,12 @@ class TestAttachHistoryHander(unittest.TestCase):
                 session=mock_session, parsed_args=parsed_args)
             self.assertIn(
                 'enabled but sqlite3 is unavailable', mock_stderr.getvalue())
-        self.assertFalse(mock_get_recorder.called)
+        self.assertFalse(mock_recorder.add_handler.called)
 
     @mock.patch('awscli.customizations.history.sqlite3')
-    @mock.patch('awscli.customizations.history.get_global_history_recorder')
-    def test_create_directory_no_exists(self, mock_get_recorder, mock_sqlite3):
+    @mock.patch('awscli.customizations.history.HISTORY_RECORDER',
+                spec=HistoryRecorder)
+    def test_create_directory_no_exists(self, mock_recorder, mock_sqlite3):
         mock_session = mock.Mock(Session)
         mock_session.get_scoped_config.return_value = {
             'cli_history': 'enabled'
@@ -107,9 +109,6 @@ class TestAttachHistoryHander(unittest.TestCase):
 
         parsed_args = argparse.Namespace()
         parsed_args.command = 's3'
-
-        mock_recorder = mock.Mock(HistoryRecorder)
-        mock_get_recorder.return_value = mock_recorder
 
         directory_to_create = os.path.join(self.files.rootdir, 'create-dir')
         db_filename = os.path.join(directory_to_create, 'name.db')

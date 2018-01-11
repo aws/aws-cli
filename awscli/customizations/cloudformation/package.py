548407d8465367db39d1485c4cc044cd553a7b02
Lessa <lessa@8c85903f95a3.ant.amazon.com>
2018-01-11 14:49:58 +0000

    Fixed review comment on somehow incomplete .get()

--- awscli/customizations/cloudformation/package.py
+++ awscli/customizations/cloudformation/package.py
@@ -116,9 +116,8 @@ class PackageCommand(BasicCommand):
     ]
 
     def _get_bucket_region(self, s3_bucket, s3_client):
-        s3_loc = s3_client.get_bucket_location(
-            Bucket=s3_bucket).get("LocationConstraint", "us-east-1")
-        return s3_loc.get
+        s3_loc = s3_client.get_bucket_location(Bucket=s3_bucket)
+        return s3_loc.get("LocationConstraint", "us-east-1")
 
     def _run_main(self, parsed_args, parsed_globals):
         region = parsed_globals.region if parsed_globals.region else "us-east-1"

3661e4229a600d292149701969faacc59bdbd286
Lessa <lessa@8c85903f95a3.ant.amazon.com>
2018-01-20 20:52:05 +0000

    Clean up, refactor even original tests to reach 100% coverage

--- tests/unit/customizations/cloudformation/test_package.py
+++ tests/unit/customizations/cloudformation/test_package.py
@@ -18,10 +18,12 @@ import tempfile
 from io import StringIO
 from mock import patch, Mock, MagicMock
 from awscli.testutils import unittest, BaseAWSCommandParamsTest
-from awscli.customizations.cloudformation.package import PackageCommand
-from awscli.customizations.cloudformation.artifact_exporter import Template
+from awscli.customizations.cloudformation.package import (
+    PackageCommand, Template, json)
 from awscli.customizations.cloudformation.yamlhelper import yaml_dump
-from awscli.customizations.cloudformation.exceptions import PackageFailedRegionMismatchError, PackageEmptyRegionError
+from awscli.customizations.cloudformation.exceptions import (
+    PackageFailedRegionMismatchError, PackageEmptyRegionError,
+    InvalidTemplatePathError)
 from botocore.exceptions import ClientError
 
 
@@ -60,103 +62,75 @@ class TestPackageCommand(unittest.TestCase):
                                        verify_ssl=None)
         self.package_command = PackageCommand(self.session)
 
+    @patch("awscli.customizations.cloudformation.package.json")
+    @patch("awscli.customizations.cloudformation.package.Template")
     @patch("awscli.customizations.cloudformation.package.yaml_dump")
-    def test_main(self, mock_yaml_dump):
+    def test_main(self, mock_yaml_dump, mock_export_template, mock_json_dump):
         exported_template_str = "hello"
-
-        self.package_command.write_output = Mock()
-        self.package_command._export = Mock()
         mock_yaml_dump.return_value = exported_template_str
+        mock_json_dump.dumps.return_value = "template_json_format"
 
         # Create a temporary file and make this my template
         with tempfile.NamedTemporaryFile() as handle:
+            self.package_command._get_bucket_region = MagicMock(
+                return_value="us-east-1")
+
             for use_json in (False, True):
                 filename = handle.name
                 self.parsed_args.template_file = filename
                 self.parsed_args.use_json = use_json
-
-                self.package_command._get_bucket_region = MagicMock(
-                    return_value="us-east-1")
 
                 rc = self.package_command._run_main(
                     self.parsed_args, self.parsed_globals)
                 self.assertEquals(rc, 0)
 
-                self.package_command._export.assert_called_once_with(
-                    filename, use_json)
-                self.package_command.write_output.assert_called_once_with(
-                    self.parsed_args.output_template_file, mock.ANY)
+                self.package_command.write_output(
+                    self.parsed_args.output_template_file, "some data")
 
-                self.package_command._export.reset_mock()
-                self.package_command.write_output.reset_mock()
+            # No output file specified
+            self.parsed_args.output_template_file = None
+            rc_no_output = self.package_command._run_main(
+                self.parsed_args, self.parsed_globals)
+            self.assertEquals(rc_no_output, 0)
 
-    @patch("awscli.customizations.cloudformation.package.yaml_dump")
-    def test_main_without_bucket(self, mock_yaml_dump):
-        exported_template_str = "hello"
-
+    @patch("os.path.isfile")
+    def test_main_without_bucket(self, mock_os_isfile):
         self.package_command.write_output = Mock()
         self.package_command._export = Mock()
-        mock_yaml_dump.return_value = exported_template_str
+        mock_os_isfile.return_value = True
 
-        # Create a temporary file and make this my template
-        with tempfile.NamedTemporaryFile() as handle:
-            for use_json in (False, True):
-                filename = handle.name
-                self.parsed_args.template_file = filename
-                self.parsed_args.use_json = use_json
+        self.parsed_args.s3_bucket = None
+        self.package_command._get_bucket_region = MagicMock(
+            return_value="us-east-1")
+        rc = self.package_command._run_main(
+            self.parsed_args, self.parsed_globals)
 
-                # This can be deleted as it's not needed...
-                self.package_command._get_bucket_region = MagicMock(
-                    return_value="us-east-1")
-                self.parsed_args.s3_bucket = None
+        self.assertEquals(rc, 0)
 
-                rc = self.package_command._run_main(
-                    self.parsed_args, self.parsed_globals)
-                self.assertEquals(rc, 0)
+    @patch("os.path.isfile")
+    def test_main_bucket_region_mismatch(self, mock_os_isfile):
+        mock_os_isfile.return_value = True
+        self.parsed_globals.region = "eu-west-1"
+        self.parsed_args.s3_bucket = "bucket-in-different-region"
 
-                self.package_command._export.assert_called_once_with(
-                    filename, use_json)
-                self.package_command.write_output.assert_called_once_with(
-                    self.parsed_args.output_template_file, mock.ANY)
-
-                self.package_command._export.reset_mock()
-                self.package_command.write_output.reset_mock()
-
-    @patch("awscli.customizations.cloudformation.package.yaml_dump")
-    def test_main_bucket_different_deployment_region(self, mock_yaml_dump):
-        exported_template_str = "hello"
-
-        self.package_command.write_output = Mock()
-        self.package_command._export = Mock()
-        mock_yaml_dump.return_value = exported_template_str
-
-        # Create a temporary file and make this my template
-        with tempfile.NamedTemporaryFile() as handle:
-            for use_json in (False, True):
-                filename = handle.name
-                self.parsed_args.template_file = filename
-                self.parsed_args.use_json = use_json
-
-                self.parsed_args.s3_bucket = "bucket-in-different-region"
-                self.parsed_globals.region = "eu-west-1"
-
-                with self.assertRaises(PackageFailedRegionMismatchError):
-                    self.package_command._run_main(
-                        self.parsed_args, self.parsed_globals)
+        with self.assertRaises(PackageFailedRegionMismatchError):
+            self.package_command._run_main(
+                self.parsed_args, self.parsed_globals
+            )
 
     def test_main_empty_region_error(self):
-        # Create a temporary file and make this my template
-        with tempfile.NamedTemporaryFile() as handle:
-            for use_json in (False, True):
-                filename = handle.name
-                self.parsed_args.template_file = filename
-                self.parsed_args.use_json = use_json
-                # None is what package receives if no --region is defined/passed
-                self.parsed_globals.region = None
+        self.parsed_globals.region = None
+        with self.assertRaises(PackageEmptyRegionError):
+            self.package_command._run_main(
+                self.parsed_args, self.parsed_globals
+            )
 
-            with self.assertRaises(PackageEmptyRegionError):
-                self.package_command._run_main(
-                    self.parsed_args, self.parsed_globals)
+    def test_main_invalid_template_error(self):
+        self.parsed_args.template_file = "invalid-template-file"
+        with self.assertRaises(InvalidTemplatePathError):
+            self.package_command._run_main(
+                self.parsed_args, self.parsed_globals
+            )
 
     def test_main_error(self):
 
@@ -185,18 +159,25 @@ class TestPackageCommand(unittest.TestCase):
         rc_default_region = self.package_command._create_sam_bucket(
             s3_client, sts_client, self.parsed_globals)
 
+        self.assertIn("sam-", rc_default_region)
+
         self.parsed_globals.region = "eu-west-1"
         rc_custom_region = self.package_command._create_sam_bucket(
             s3_client, sts_client, self.parsed_globals)
 
-        self.assertIn("sam-", rc_default_region)
         self.assertIn("sam-", rc_custom_region)
 
+        # Covers non-404 use case
+        err_response['Error']['Code'] = "418"
+        err_response['Error']['Message'] = "Easter egg, return bucket"
+        rc_custom_region = self.package_command._create_sam_bucket(
+            s3_client, sts_client, self.parsed_globals)
+
+        self.assertIn("sam-", rc_custom_region)
 
     @patch("awscli.customizations.cloudformation.package.sys.stdout")
     def test_write_output_to_stdout(self, stdoutmock):
         data = u"some data"
-        filename = None
 
-        self.package_command.write_output(filename, data)
-        stdoutmock.write.assert_called_once_with(data)
+        for filename in (self.parsed_args.output_template_file, None):
+            rc = self.package_command.write_output(filename, data)

7cf60d32e3034d9c52dda80206eea1e76a4b72ac
Terry Warwar <terry.warwar@gmail.com>
2018-01-27 16:03:56 +1100

    Updated test_copy_to_temp_dir

--- tests/unit/customizations/cloudformation/test_artifact_exporter.py
+++ tests/unit/customizations/cloudformation/test_artifact_exporter.py
@@ -804,7 +804,7 @@ class TestArtifactExporter(unittest.TestCase):
                 os.remove(zipfile_name)
             test_file_creator.remove_all()
 
-    @patch("shutil.copyfile")
+    @patch("shutil.copy")
     @patch("tempfile.mkdtemp")
     def test_copy_to_temp_dir(self, mkdtemp_mock, copyfile_mock):
         temp_dir = "/tmp/foo/"

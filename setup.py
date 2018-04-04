2527977258007f9b4d71828811c43bfccd9ab4b1
awstools <aws-dr-tools-github-user@amazon.com>
2018-04-04 11:54:48 -0700

    Bumping version to 1.15.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-requires = ['botocore==1.9.23',
+requires = ['botocore==1.10.0',
             'colorama>=0.2.5,<=0.3.7',
             'docutils>=0.10',
             'rsa>=3.1.2,<=3.5.0',

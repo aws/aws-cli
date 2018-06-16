b28e006254919461e507af66e9d2b1e263119f38
Jon Dufresne <jon.dufresne@gmail.com>
2018-06-15 19:01:08 -0700

    Add trove classifier for general Python 2 support

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup_options = dict(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',

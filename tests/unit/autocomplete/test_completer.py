065d6ae417537fa7cad2c429afaa462f8739b90a
James Saryerwinnie <js@jamesls.com>
2018-11-01 20:50:50 -0700

    Update tests to use completer
    We're technically testing more than a unit here, but this
    will make it easier to verify our changes as the parser and
    parsed results will likely we change as the design is fleshed out.

--- tests/unit/autocomplete/test_completer.py
+++ tests/unit/autocomplete/test_completer.py
@@ -11,8 +11,9 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 from awscli.testutils import unittest, mock
-from awscli.autocomplete import completer, parser, model
-from awscli.clidriver import CLIDriver
+from awscli.autocomplete import completer, parser
+
+from tests.unit.autocomplete import InMemoryIndex
 
 
 class TestAutoCompleter(unittest.TestCase):
@@ -63,22 +64,32 @@ class TestAutoCompleter(unittest.TestCase):
 
 class TestModelIndexCompleter(unittest.TestCase):
     def setUp(self):
-        self.index = mock.Mock(spec=model.ModelIndex)
+        self.index = InMemoryIndex({
+            'command_names': {
+                '': ['aws'],
+                'aws': ['ec2', 'ecs', 's3'],
+                'aws.ec2': ['describe-instances'],
+            },
+            'arg_names': {
+                '': {
+                    'aws': ['region', 'endpoint-url'],
+                },
+                'aws.ec2': {
+                    'describe-instances': ['instance-ids', 'reserve'],
+                }
+            },
+        })
+        self.parser = parser.CLIParser(self.index)
+
+
         self.completer = completer.ModelIndexCompleter(self.index)
 
     def test_does_not_complete_if_unparsed_items(self):
-        parsed = parser.ParsedResult(
-            current_command='aws', lineage=[],
-            unparsed_items=['foo'], last_fragment='')
+        parsed = self.parser.parse('aws foo ')
         self.assertIsNone(self.completer.complete(parsed))
 
     def test_does_complete_if_last_fragment_is_none(self):
-        parsed = parser.ParsedResult(
-            lineage=[],
-            unparsed_items=[],
-            current_command='aws',
-            last_fragment=None,
-        )
+        parsed = self.parser.parse('aws')
         self.assertIsNone(self.completer.complete(parsed))
 
     def test_can_prefix_match_services(self):
@@ -86,56 +97,22 @@ class TestModelIndexCompleter(unittest.TestCase):
             current_command='aws', lineage=[],
             last_fragment='e',
         )
-        self.index.command_names.return_value = ['ec2', 'ecs', 's3']
+        parsed = self.parser.parse('aws e')
         self.assertEqual(self.completer.complete(parsed), ['ec2', 'ecs'])
 
     def test_returns_all_results_when_last_fragment_empty(self):
-        parsed = parser.ParsedResult(
-            current_command='aws', lineage=[],
-            last_fragment='',
-        )
-        self.index.command_names.return_value = ['ec2', 'ecs', 's3']
+        parsed = self.parser.parse('aws ')
         self.assertEqual(self.completer.complete(parsed), ['ec2', 'ecs', 's3'])
 
     def test_can_autocomplete_global_param(self):
-        parsed = parser.ParsedResult(
-            lineage=[],
-            unparsed_items=[],
-            current_command=u'aws',
-            last_fragment=u'--re',
-        )
-        self.index.arg_names.return_value = ['region', 'endpoint-url']
+        parsed = self.parser.parse('aws --re')
         self.assertEqual(self.completer.complete(parsed), ['--region'])
 
     def test_can_combine_global_and_command_params(self):
-        parsed = parser.ParsedResult(
-            lineage=['aws', 'ec2'],
-            unparsed_items=[],
-            current_params={},
-            current_command='describe-instances',
-            last_fragment='--r',
-        )
-        self.index.arg_names.side_effect = [
-            ['foo', 'reserve'],
-            ['region', 'endpoint-url'],
-        ]
+        parsed = self.parser.parse('aws ec2 describe-instances --r')
         self.assertEqual(self.completer.complete(parsed),
                          ['--reserve', '--region'])
-        # Because there were multiple calls to arg_names() we'll also
-        # assert the call args and the call orders.
-        self.assertEqual(
-            self.index.arg_names.call_args_list,
-            [mock.call(lineage=['aws', 'ec2'],
-                       command_name='describe-instances'),
-             mock.call(lineage=[], command_name='aws')]
-        )
 
     def test_no_autocompletions_if_nothing_matches(self):
-        parsed = parser.ParsedResult(
-            lineage=[],
-            unparsed_items=[],
-            current_command=u'aws',
-            last_fragment=u'--foo',
-        )
-        self.index.arg_names.return_value = ['region', 'endpoint-url']
+        parsed = self.parser.parse('aws --foo')
         self.assertEqual(self.completer.complete(parsed), [])

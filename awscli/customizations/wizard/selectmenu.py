eccb6c5413fbc5309115c311df82dfdbd1416a7f
James Saryerwinnie <js@jamesls.com>
2018-11-23 20:22:23 -0800

    Fix unicode bug in py2
    Prompt toolkit expects unicode for all string types.

--- awscli/customizations/wizard/selectmenu.py
+++ awscli/customizations/wizard/selectmenu.py
@@ -10,6 +10,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+from __future__ import unicode_literals
 from prompt_toolkit import Application
 from prompt_toolkit.utils import get_cwidth
 from prompt_toolkit.layout import Layout, FloatContainer, Float

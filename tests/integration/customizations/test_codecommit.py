dabbdec75aca21aca7f47898239bab133fe8d272
David Jackson <jacdavid@amazon.com>
2019-03-13 10:37:57 -0700

    Add support for VPC and FIPS endpoints in AWS CodeCommit credential helper customization

--- tests/integration/customizations/test_codecommit.py
+++ tests/integration/customizations/test_codecommit.py
@@ -32,6 +32,14 @@ class TestCodeCommitCredentialHelper(unittest.TestCase):
                           'host=git-codecommit.us-east-1.amazonaws.com\n'
                           'path=/v1/repos/myrepo')
 
+    FIPS_PROTOCOL_HOST_PATH = ('protocol=https\n'
+                               'host=git-codecommit-fips.us-east-1.amazonaws.com\n'
+                               'path=/v1/repos/myrepo')
+
+    VPC_PROTOCOL_HOST_PATH = ('protocol=https\n'
+                              'host=vpce-0b47ea360adebf88a-jkl88hez.git-codecommit.us-east-1.vpce.amazonaws.com\n'
+                              'path=/v1/repos/myrepo')
+
     def setUp(self):
         self.orig_id = os.environ.get('AWS_ACCESS_KEY_ID')
         self.orig_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
@@ -60,6 +68,36 @@ class TestCodeCommitCredentialHelper(unittest.TestCase):
             ('username=foo\n'
              'password=20101008T000000Z'
              '7dc259e2d505af354a1219b9bcd784bd384dc706efa0d9aefc571f214be4c89c'),
+             output)
+        self.assertEquals(0, rc)
+
+    @patch('sys.stdin', StringIO(FIPS_PROTOCOL_HOST_PATH))
+    @patch('sys.stdout', new_callable=StringIOWithFileNo)
+    @patch.object(awscli.customizations.codecommit.datetime, 'datetime')
+    def test_integration_fips_using_cli_driver(self, dt_mock, stdout_mock):
+        dt_mock.utcnow.return_value = datetime(2010, 10, 8)
+        driver = create_clidriver()
+        rc = driver.main('codecommit credential-helper get'.split())
+        output = stdout_mock.getvalue().strip()
+        self.assertEquals(
+            ('username=foo\n'
+             'password=20101008T000000Z'
+             '500037cb3514b3fe01ebcda7c80973f5b4c0d8199a7a6563b85fd6edf272d460'),
+             output)
+        self.assertEquals(0, rc)
+
+    @patch('sys.stdin', StringIO(VPC_PROTOCOL_HOST_PATH))
+    @patch('sys.stdout', new_callable=StringIOWithFileNo)
+    @patch.object(awscli.customizations.codecommit.datetime, 'datetime')
+    def test_integration_vpc_using_cli_driver(self, dt_mock, stdout_mock):
+        dt_mock.utcnow.return_value = datetime(2010, 10, 8)
+        driver = create_clidriver()
+        rc = driver.main('codecommit credential-helper get'.split())
+        output = stdout_mock.getvalue().strip()
+        self.assertEquals(
+            ('username=foo\n'
+             'password=20101008T000000Z'
+             '9ed987cc6336c3de2d9f06b9236c7a9fd76b660b080db15983290e636dbfbd6b'),
              output)
         self.assertEquals(0, rc)
 

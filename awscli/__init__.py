540664e9485321d4a5a07f329f87f2f5a343edb2
aws-sdk-python-automation <github-aws-sdk-python-automation@amazon.com>
2019-09-20 22:05:33 +0000

    Bumping version to 1.16.243

--- awscli/__init__.py
+++ awscli/__init__.py
@@ -17,7 +17,7 @@ A Universal Command Line Environment for Amazon Web Services.
 """
 import os
 
-__version__ = '1.16.242'
+__version__ = '1.16.243'
 
 #
 # Get our data path to be added to botocore's search path

ee237a321313b394d0a510b86130b978a869dfce
stealthycoin <stealthycoin@users.noreply.github.com>
2019-10-16 17:27:19 -0800

    Add patch

--- awscli/compat.py
+++ awscli/compat.py
@@ -354,65 +354,8 @@ try:
     from platform import linux_distribution
 except ImportError:
     _UNIXCONFDIR = '/etc'
-    _supported_dists = (
-        'SuSE', 'debian', 'fedora', 'redhat', 'centos',
-        'mandrake', 'mandriva', 'rocks', 'slackware', 'yellowdog', 'gentoo',
-        'UnitedLinux', 'turbolinux', 'arch', 'mageia')
-    _release_filename = re.compile(r'(\w+)[-_](release|version)', re.ASCII)
-    def linux_distribution(distname='', version='', id='',
-                           supported_dists=_supported_dists,
-                           full_distribution_name=1):
-        return _linux_distribution(distname, version, id, supported_dists,
-                                   full_distribution_name)
-
-    def _linux_distribution(distname, version, id, supported_dists,
-                            full_distribution_name):
-
-        """ Tries to determine the name of the Linux OS distribution name.
-            The function first looks for a distribution release file in
-            /etc and then reverts to _dist_try_harder() in case no
-            suitable files are found.
-            supported_dists may be given to define the set of Linux
-            distributions to look for. It defaults to a list of currently
-            supported Linux distributions identified by their release file
-            name.
-            If full_distribution_name is true (default), the full
-            distribution read from the OS is returned. Otherwise the short
-            name taken from supported_dists is used.
-            Returns a tuple (distname, version, id) which default to the
-            args given as parameters.
-        """
-        try:
-            etc = os.listdir(_UNIXCONFDIR)
-        except OSError:
-            # Probably not a Unix system
-            return distname, version, id
-        etc.sort()
-        for file in etc:
-            m = _release_filename.match(file)
-            if m is not None:
-                _distname, dummy = m.groups()
-                if _distname in supported_dists:
-                    distname = _distname
-                    break
-        else:
-            return _dist_try_harder(distname, version, id)
-
-        # Read the first line
-        with open(os.path.join(_UNIXCONFDIR, file), 'r',
-                  encoding='utf-8', errors='surrogateescape') as f:
-            firstline = f.readline()
-        _distname, _version, _id = _parse_release_file(firstline)
-
-        if _distname and full_distribution_name:
-            distname = _distname
-        if _version:
-            version = _version
-        if _id:
-            id = _id
-        return distname, version, id
-
     def _dist_try_harder(distname, version, id):
+
         """ Tries some special tricks to get the distribution
             information in case the default method fails.
             Currently supports older SuSE Linux, Caldera OpenLinux and
@@ -457,4 +400,128 @@ except ImportError:
                 version = verfiles[-1][14:]
                 return distname, version, id
 
+        return distname, version, id
+
+    _release_filename = re.compile(r'(\w+)[-_](release|version)', re.ASCII)
+    _lsb_release_version = re.compile(r'(.+)'
+                                      r' release '
+                                      r'([\d.]+)'
+                                      r'[^(]*(?:\((.+)\))?', re.ASCII)
+    _release_version = re.compile(r'([^0-9]+)'
+                                  r'(?: release )?'
+                                  r'([\d.]+)'
+                                  r'[^(]*(?:\((.+)\))?', re.ASCII)
+
+    # See also http://www.novell.com/coolsolutions/feature/11251.html
+    # and http://linuxmafia.com/faq/Admin/release-files.html
+    # and http://data.linux-ntfs.org/rpm/whichrpm
+    # and http://www.die.net/doc/linux/man/man1/lsb_release.1.html
+
+    _supported_dists = (
+        'SuSE', 'debian', 'fedora', 'redhat', 'centos',
+        'mandrake', 'mandriva', 'rocks', 'slackware', 'yellowdog', 'gentoo',
+        'UnitedLinux', 'turbolinux', 'arch', 'mageia')
+
+    def _parse_release_file(firstline):
+
+        # Default to empty 'version' and 'id' strings.  Both defaults are used
+        # when 'firstline' is empty.  'id' defaults to empty when an id can not
+        # be deduced.
+        version = ''
+        id = ''
+
+        # Parse the first line
+        m = _lsb_release_version.match(firstline)
+        if m is not None:
+            # LSB format: "distro release x.x (codename)"
+            return tuple(m.groups())
+
+        # Pre-LSB format: "distro x.x (codename)"
+        m = _release_version.match(firstline)
+        if m is not None:
+            return tuple(m.groups())
+
+        # Unknown format... take the first two words
+        l = firstline.strip().split()
+        if l:
+            version = l[0]
+            if len(l) > 1:
+                id = l[1]
+        return '', version, id
+
+    _distributor_id_file_re = re.compile("(?:DISTRIB_ID\s*=)\s*(.*)", re.I)
+    _release_file_re = re.compile("(?:DISTRIB_RELEASE\s*=)\s*(.*)", re.I)
+    _codename_file_re = re.compile("(?:DISTRIB_CODENAME\s*=)\s*(.*)", re.I)
+
+    def linux_distribution(distname='', version='', id='',
+                           supported_dists=_supported_dists,
+                           full_distribution_name=1):
+        return _linux_distribution(distname, version, id, supported_dists,
+                                   full_distribution_name)
+
+    def _linux_distribution(distname, version, id, supported_dists,
+                            full_distribution_name):
+
+        """ Tries to determine the name of the Linux OS distribution name.
+            The function first looks for a distribution release file in
+            /etc and then reverts to _dist_try_harder() in case no
+            suitable files are found.
+            supported_dists may be given to define the set of Linux
+            distributions to look for. It defaults to a list of currently
+            supported Linux distributions identified by their release file
+            name.
+            If full_distribution_name is true (default), the full
+            distribution read from the OS is returned. Otherwise the short
+            name taken from supported_dists is used.
+            Returns a tuple (distname, version, id) which default to the
+            args given as parameters.
+        """
+        # check for the Debian/Ubuntu /etc/lsb-release file first, needed so
+        # that the distribution doesn't get identified as Debian.
+        # https://bugs.python.org/issue9514
+        try:
+            with open("/etc/lsb-release", "r") as etclsbrel:
+                for line in etclsbrel:
+                    m = _distributor_id_file_re.search(line)
+                    if m:
+                        _u_distname = m.group(1).strip()
+                    m = _release_file_re.search(line)
+                    if m:
+                        _u_version = m.group(1).strip()
+                    m = _codename_file_re.search(line)
+                    if m:
+                        _u_id = m.group(1).strip()
+                if _u_distname and _u_version:
+                    return (_u_distname, _u_version, _u_id)
+        except (EnvironmentError, UnboundLocalError):
+                pass
+
+        try:
+            etc = os.listdir(_UNIXCONFDIR)
+        except OSError:
+            # Probably not a Unix system
+            return distname, version, id
+        etc.sort()
+        for file in etc:
+            m = _release_filename.match(file)
+            if m is not None:
+                _distname, dummy = m.groups()
+                if _distname in supported_dists:
+                    distname = _distname
+                    break
+        else:
+            return _dist_try_harder(distname, version, id)
+
+        # Read the first line
+        with open(os.path.join(_UNIXCONFDIR, file), 'r',
+                  encoding='utf-8', errors='surrogateescape') as f:
+            firstline = f.readline()
+        _distname, _version, _id = _parse_release_file(firstline)
+
+        if _distname and full_distribution_name:
+            distname = _distname
+        if _version:
+            version = _version
+        if _id:
+            id = _id
         return distname, version, id

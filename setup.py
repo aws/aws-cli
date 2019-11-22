9acbbaa5b18a260bca9f962790d0f4e0714e7c8a
aws-sdk-python-automation <github-aws-sdk-python-automation@amazon.com>
2019-11-22 19:40:31 +0000

    Bumping version to 1.16.290

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-install_requires = ['botocore==1.13.25',
+install_requires = ['botocore==1.13.26',
                     'docutils>=0.10,<0.16',
                     'rsa>=3.1.2,<=3.5.0',
                     's3transfer>=0.2.0,<0.3.0']

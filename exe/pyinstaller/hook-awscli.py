4d6e2b2372ddc5eca0f1f69e9a4937b701559452
kyleknap <kyknapp1@gmail.com>
2020-01-20 10:40:22 -0800

    Add hiddenimports for some CLI plugins
    Because PyInstaller does tree shaking some packages and modules
    are not included from the standard library. However, some of the
    more popular CLI plugins use these modules. So we include them
    as hiddenimports so they will be available in our Python runtime.

--- exe/pyinstaller/hook-awscli.py
+++ exe/pyinstaller/hook-awscli.py
@@ -1,7 +1,23 @@
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils import hooks
 
-hiddenimports = ['docutils', 'urllib', 'httplib', 'html.parser',
-                 'configparser', 'xml.etree', 'pipes', 'colorama',
-                 'awscli.handlers']
-datas = collect_data_files('awscli')
 
+hiddenimports = [
+    'docutils',
+    'urllib',
+    'httplib',
+    'html.parser',
+    'configparser',
+    'xml.etree',
+    'pipes',
+    'colorama',
+    'awscli.handlers',
+]
+
+imports_for_legacy_plugins = (
+    hooks.collect_submodules('http') +
+    hooks.collect_submodules('logging')
+)
+
+hiddenimports += imports_for_legacy_plugins
+
+datas = hooks.collect_data_files('awscli')

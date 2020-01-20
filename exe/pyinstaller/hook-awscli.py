33ea908ccf2c3fc684a698ffa2b697e38065f63e
kyleknap <kyknapp1@gmail.com>
2020-01-20 10:48:27 -0800

    Add temporary fix for setuptools missing import

--- exe/pyinstaller/hook-awscli.py
+++ exe/pyinstaller/hook-awscli.py
@@ -11,6 +11,10 @@ hiddenimports = [
     'pipes',
     'colorama',
     'awscli.handlers',
+    # NOTE: This can be removed once this hidden import issue related to
+    # setuptools and PyInstaller is resolved:
+    # https://github.com/pypa/setuptools/issues/1963
+    'pkg_resources.py2_warn'
 ]
 
 imports_for_legacy_plugins = (

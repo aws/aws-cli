8f756755ee0c434f3b3e96f85a4a1866ca7af8eb
James Saryerwinnie <js@jamesls.com>
2020-01-23 11:45:41 -0800

    Update customization to not use pyyaml
    This lets us remove pyyaml entirely as a dep.

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ requires = ['botocore==2.0.0dev2',
             'docutils>=0.10',
             'cryptography>=2.8.0,<=2.9.0',
             's3transfer>=0.3.0,<0.4.0',
-            'PyYAML>=3.10,<5.2',
             'ruamel.yaml>=0.15.0,<0.16.0',
             'prompt-toolkit>=2.0.0,<3.0.0',
             ]

a132fbdbfc94de52adb5500cb52e789332f30e75
kyleknap <kyknapp1@gmail.com>
2020-01-28 12:17:57 -0800

    Remove ecr get-login command
    The command was deprecated in v1 and replaced with the
    ecr get-login-password command

--- awscli/customizations/ecr.py
+++ awscli/customizations/ecr.py
@@ -22,68 +22,7 @@ def register_ecr_commands(cli):
 
 
 def _inject_commands(command_table, session, **kwargs):
-    command_table['get-login'] = ECRLogin(session)
     command_table['get-login-password'] = ECRGetLoginPassword(session)
-
-
-class ECRLogin(BasicCommand):
-    """Log in with 'docker login'"""
-    NAME = 'get-login'
-
-    DESCRIPTION = BasicCommand.FROM_FILE('ecr/get-login_description.rst')
-
-    ARG_TABLE = [
-        {
-            'name': 'registry-ids',
-            'help_text': 'A list of AWS account IDs that correspond to the '
-                         'Amazon ECR registries that you want to log in to.',
-            'required': False,
-            'nargs': '+'
-        },
-        {
-            'name': 'include-email',
-            'action': 'store_true',
-            'group_name': 'include-email',
-            'dest': 'include_email',
-            'default': True,
-            'required': False,
-            'help_text': (
-                "Specify if the '-e' flag should be included in the "
-                "'docker login' command.  The '-e' option has been deprecated "
-                "and is removed in Docker version 17.06 and later.  You must "
-                "specify --no-include-email if you're using Docker version "
-                "17.06 or later.  The default behavior is to include the "
-                "'-e' flag in the 'docker login' output."),
-        },
-        {
-            'name': 'no-include-email',
-            'help_text': 'Include email arg',
-            'action': 'store_false',
-            'default': True,
-            'group_name': 'include-email',
-            'dest': 'include_email',
-            'required': False,
-        },
-    ]
-
-    def _run_main(self, parsed_args, parsed_globals):
-        ecr_client = create_client_from_parsed_globals(
-            self._session, 'ecr', parsed_globals)
-        if not parsed_args.registry_ids:
-            result = ecr_client.get_authorization_token()
-        else:
-            result = ecr_client.get_authorization_token(
-                registryIds=parsed_args.registry_ids)
-        for auth in result['authorizationData']:
-            auth_token = b64decode(auth['authorizationToken']).decode()
-            username, password = auth_token.split(':')
-            command = ['docker', 'login', '-u', username, '-p', password]
-            if parsed_args.include_email:
-                command.extend(['-e', 'none'])
-            command.append(auth['proxyEndpoint'])
-            sys.stdout.write(' '.join(command))
-            sys.stdout.write('\n')
-        return 0
 
 
 class ECRGetLoginPassword(BasicCommand):

5178fce85ca762efb85783422d3bef3bd321468a
Illia Batozskyi <ivz@amazon.com>
2020-10-09 14:04:48 -0400

    Move prompt from customizations and add arguments to cli.json

--- tests/functional/autoprompt/test_doc.py
+++ tests/functional/autoprompt/test_doc.py
@@ -11,7 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 from awscli.clidriver import create_clidriver, ServiceCommand
-from awscli.customizations.autoprompt.doc import (
+from awscli.autoprompt.doc import (
     AwsTopLevelDocsGetter, DocsGetter, ServiceCommandDocsGetter,
     ServiceOperationDocsGetter
 )

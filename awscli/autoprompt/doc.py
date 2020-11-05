86592308e67b1ac02434e3afddb80d798502f299
vz10 <i.batozskiy@gmail.com>
2020-11-05 17:20:58 -0500

    Refactor autoprompt docs getting to use parser

--- awscli/autoprompt/doc.py
+++ awscli/autoprompt/doc.py
@@ -13,8 +13,6 @@
 import io
 from docutils.core import publish_string
 
-import awscli
-from awscli.argparser import ArgTableArgParser
 from awscli.bcdoc import docevents, textwriter
 
 
@@ -25,72 +23,6 @@ class DocsGetter:
     service commands and service operations.
 
     """
-    def __init__(self, driver, aws_top_level_docs_getter=None,
-                 service_command_docs_getter=None):
-        self._driver = driver
-        self._service_command_table = self._driver.subcommand_table
-        if aws_top_level_docs_getter is None:
-            aws_top_level_docs_getter = AwsTopLevelDocsGetter(self._driver)
-        self._aws_top_level_docs_getter = aws_top_level_docs_getter
-        if service_command_docs_getter is None:
-            service_command_docs_getter = \
-                ServiceCommandDocsGetter(self._driver)
-        self._service_command_docs_getter = service_command_docs_getter
-
-    @property
-    def aws_top_level_docs_getter(self):
-        return self._aws_top_level_docs_getter
-
-    @property
-    def service_command_docs_getter(self):
-        return self._service_command_docs_getter
-
-    def get_docs(self, command_text):
-        args = command_text.split()
-        if not args:
-            return self._aws_top_level_docs_getter.get_docs(self._driver)
-        args = self._filter_out_options(args)
-        try:
-            if self._is_valid_service(args):
-                return self._service_command_docs_getter.get_docs(args)
-            else:
-                # At this point, one of two things has happened:
-                # 1. An invalid service command has been entered.
-                # 2. The user has cleared the buffer and has started typing
-                #    out a command. The in-process command won't get parsed
-                #    correctly yet (e.g. `aws ec`).
-                return self._aws_top_level_docs_getter.get_docs(self._driver)
-        except:
-            # If there are any errors in the documentation retrieval process,
-            # we pull up the default top-level aws docs. This can happen if
-            # the buffer is cleared and `aws help` is entered.
-            return self._aws_top_level_docs_getter.get_docs(self._driver)
-
-    def _is_valid_service(self, args):
-        self._valid_services = \
-            {name for name, _ in self._service_command_table.items()}
-        service, *_ = args
-        return service in self._valid_services
-
-    def _filter_out_options(self, args):
-        # Because the lowest level of documentation retrieval is at the
-        # service operation level, we can ignore all `--options` in the
-        # passed-in arguments for the purposes of grabbing documentation. This
-        # allows us to bypass the problem of getting parsing errors in
-        # self.get_docs() when there are `--options` that require arguments to
-        # be passed in (e.g. `aws ec2 describe-instances --output`). We also
-        # need to filter out any specified files at the command line, as these
-        # are positional args that don't make sense after `--options` are
-        # removed.
-        file_path_chars = '.:\\/'
-        return [
-            arg for arg in args
-            if not arg.startswith('--')
-            and not any(char in arg for char in file_path_chars)
-        ]
-
-
-class BaseDocsGetter:
     def __init__(self, driver):
         self._driver = driver
         self._cache = {}
@@ -141,78 +73,22 @@ class BaseDocsGetter:
         instance.unregister()
         return content
 
-
-class AwsTopLevelDocsGetter(BaseDocsGetter):
-    """Getter for the top-level `aws` command."""
-    def get_docs(self, driver):
-        if 'aws' not in self._cache:
-            help_command = driver.create_help_command()
-            self._cache['aws'] = self.get_doc_content(help_command)
-        return self._cache['aws']
-
-
-class ServiceCommandDocsGetter(BaseDocsGetter):
-    def __init__(self, driver, service_operation_docs_getter=None):
-        super(ServiceCommandDocsGetter, self).__init__(driver)
-        self._service_command_table = self._driver.subcommand_table
-        if service_operation_docs_getter is None:
-            service_operation_docs_getter = \
-                ServiceOperationDocsGetter(self._driver)
-        self._service_operation_docs_getter = service_operation_docs_getter
-
-    def get_docs(self, args):
-        service_command, remaining = self._get_service_command(args)
-        if not remaining \
-                or not self._is_valid_operation(service_command, remaining):
-            help_command = service_command.create_help_command()
-            if help_command.name not in self._cache:
-                self._cache[help_command.name] = \
-                     self.get_doc_content(help_command)
-            return self._cache[help_command.name]
-        else:
-            return self._service_operation_docs_getter.get_docs(
-                service_command, remaining)
-
-    def _get_service_command(self, args):
-        service_name, remaining = self._get_service_command_name(args)
-        return self._service_command_table[service_name], remaining
-
-    def _get_service_command_name(self, args):
-        parser = self._driver.create_parser(self._service_command_table)
-        parsed_args, remaining = parser.parse_known_args(args)
-        return parsed_args.command, remaining
-
-    def _is_valid_operation(self, service_command, remaining):
-        subcommand_table = service_command.subcommand_table
-        self._valid_operations = {name for name, _ in subcommand_table.items()}
-        operation, *_ = remaining
-        return operation in self._valid_operations
-
-
-class ServiceOperationDocsGetter(BaseDocsGetter):
-    def get_docs(self, service_command, remaining):
-        subcommand_table = service_command.subcommand_table
-        operation_name = self._get_operation_name(service_command, remaining)
-        if (service_command.name, operation_name) not in self._cache:
-            service_operation = subcommand_table[operation_name]
-            help_command = service_operation.create_help_command()
-            self._cache[(service_command.name, operation_name)] = \
-                self.get_doc_content(help_command)
-        return self._cache[(service_command.name, operation_name)]
-
-    def _get_operation_name(self, service_command, remaining):
-        # We need to differentiate between `awscli.clidriver.ServiceCommand`
-        # and `awscli.customizations` objects, as they use different parsers.
-        if isinstance(service_command, awscli.clidriver.ServiceCommand):
-            service_parser = service_command.create_parser()
-            parsed_args, remaining = service_parser.parse_known_args(remaining)
-            operation_name = parsed_args.operation
-        else:
-            service_parser = ArgTableArgParser(
-                service_command.arg_table, service_command.subcommand_table)
-            parsed_args, remaining = service_parser.parse_known_args(remaining)
-            operation_name = parsed_args.subcommand
-        return operation_name
+    def get_docs(self, parsed):
+        lineage = parsed.lineage
+        current_command = parsed.current_command
+        if (tuple(lineage), current_command) not in self._cache:
+            if current_command == 'aws':
+                help_command = self._driver.create_help_command()
+            else:
+                subcommand_table = self._driver.subcommand_table
+                for arg in lineage[1:]:
+                    subcommand = subcommand_table[arg]
+                    subcommand_table = subcommand.subcommand_table
+                command = subcommand_table.get(current_command)
+                help_command = command.create_help_command()
+            docs = self.get_doc_content(help_command)
+            self._cache[(tuple(lineage), current_command)] = docs
+        return self._cache[(tuple(lineage), current_command)]
 
 
 class FileRenderer:

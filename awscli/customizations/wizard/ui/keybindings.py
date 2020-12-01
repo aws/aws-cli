4811c5f63975677708838b3858b8b757b0a52413
vz10 <i.batozskiy@gmail.com>
2020-12-01 11:12:32 -0500

    Add details panel to wizard UI

--- awscli/customizations/wizard/ui/keybindings.py
+++ awscli/customizations/wizard/ui/keybindings.py
@@ -10,10 +10,22 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+from prompt_toolkit.application import get_app
+from prompt_toolkit.filters import Condition
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.keys import Keys
 
 from awscli.customizations.wizard.ui.utils import get_ui_control_by_buffer_name
+
+
+@Condition
+def details_visible():
+    return get_app().details_visible
+
+
+@Condition
+def prompt_has_details():
+    return get_app().traverser.current_prompt_has_details()
 
 
 def get_default_keybindings():
@@ -36,6 +48,7 @@ def get_default_keybindings():
         next_prompt = event.app.traverser.next_prompt()
         layout = event.app.layout
         next_control = get_ui_control_by_buffer_name(layout, next_prompt)
+        event.app.details_visible = False
         layout.focus(next_control)
 
     @kb.add(Keys.BackTab)
@@ -45,6 +58,34 @@ def get_default_keybindings():
         layout = event.app.layout
         previous_control = get_ui_control_by_buffer_name(
             layout, previous_prompt)
+        event.app.details_visible = False
         layout.focus(previous_control)
+
+    @kb.add(Keys.F2)
+    def focus_on_details_panel(event):
+        if event.app.details_visible:
+            layout = event.app.layout
+            if layout.current_buffer and \
+                    layout.current_buffer.name == 'details_buffer':
+                current_prompt = event.app.traverser.get_current_prompt()
+                current_control = get_ui_control_by_buffer_name(
+                    layout, current_prompt)
+                layout.focus(current_control)
+            else:
+                details_buffer = layout.get_buffer_by_name('details_buffer')
+                layout.focus(details_buffer)
+
+    @kb.add(Keys.F3, filter=prompt_has_details)
+    def show_details(event):
+        event.app.details_visible = not event.app.details_visible
+        layout = event.app.layout
+        current_prompt = event.app.traverser.get_current_prompt()
+        current_control = get_ui_control_by_buffer_name(
+            layout, current_prompt)
+        if not event.app.details_visible:
+            layout.focus(current_control)
+        else:
+            if callable(current_control.on_toggle):
+                current_control.on_toggle(current_control.buffer.text)
 
     return kb
--- awscli/customizations/wizard/ui/section.py
+++ awscli/customizations/wizard/ui/section.py
@@ -15,10 +15,10 @@ from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.document import Document
 from prompt_toolkit.filters import Condition
 from prompt_toolkit.layout.containers import (
-    Window, HSplit, Dimension, ConditionalContainer, WindowAlign
+    Window, HSplit, Dimension, ConditionalContainer
 )
 from prompt_toolkit.layout.controls import BufferControl
-from prompt_toolkit.widgets import Label, Box
+from prompt_toolkit.widgets import Box
 
 from awscli.customizations.wizard.ui.prompt import WizardPrompt
 

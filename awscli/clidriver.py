71e8a0db1779c1b5cb0753b853f926f25c29ece1
kyleknap <kyknapp1@gmail.com>
2021-01-12 21:18:36 -0800

    Add support for CRT logging

--- awscli/clidriver.py
+++ awscli/clidriver.py
@@ -17,7 +17,6 @@ import sys
 import logging
 
 import distro
-
 import botocore.session
 from botocore import xform_name
 from botocore.compat import copy_kwargs, OrderedDict
@@ -50,7 +49,10 @@ from awscli.arguments import UnknownArgumentError
 from awscli.argprocess import unpack_argument
 from awscli.alias import AliasLoader
 from awscli.alias import AliasCommandInjector
-from awscli.logger import set_stream_logger, remove_stream_logger
+from awscli.logger import (
+    set_stream_logger, remove_stream_logger, enable_crt_logging,
+    disable_crt_logging,
+)
 from awscli.utils import emit_top_level_args_parsed_event
 from awscli.utils import OutputStreamFactory
 from awscli.utils import IMDSRegionProvider
@@ -496,6 +498,7 @@ class CLIDriver(object):
             for logger_name in loggers_list:
                 set_stream_logger(logger_name, logging.DEBUG,
                                   format_string=LOG_FORMAT)
+            enable_crt_logging()
             LOG.debug("CLI version: %s", self.session.user_agent())
             LOG.debug("Arguments entered to CLI: %s", sys.argv[1:])
         else:
@@ -504,6 +507,7 @@ class CLIDriver(object):
             # debug handlers
             for logger_name in loggers_list:
                 remove_stream_logger(logger_name)
+            disable_crt_logging()
             set_stream_logger(logger_name='awscli',
                               log_level=logging.ERROR)
 

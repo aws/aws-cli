e70bf9634a32a4e85c15abe4e1e34cd5a1df6ee1
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-09-28 22:31:38 +0300

    Add support for Python 3.9 (#6340)

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup_options = dict(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
 

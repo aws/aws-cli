235e9e7a12128b1e990b1fa8ea834c97f96272c6
kyleknap <kyknapp1@gmail.com>
2021-09-28 13:38:09 -0700

    Revert "Add support for Python 3.9 (#6340)"
    This reverts commit e70bf9634a32a4e85c15abe4e1e34cd5a1df6ee1.

--- setup.py
+++ setup.py
@@ -63,7 +63,6 @@ setup_options = dict(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
     ],
 )
 

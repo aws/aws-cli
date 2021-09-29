365c57be75514948105849702b9dbe98dfcb57d3
Nate Prewitt <nate.prewitt@gmail.com>
2021-09-29 09:42:15 -0700

    Add Python 3.10 support (#6430)

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup_options = dict(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
 

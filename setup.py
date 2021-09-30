2a172df4befa97d0402b1d82b0957f65765180e6
kyleknap <kyknapp1@gmail.com>
2021-09-30 14:49:20 -0700

    Revert "Add Python 3.10 support (#6430)"
    This reverts commit 365c57be75514948105849702b9dbe98dfcb57d3.

--- setup.py
+++ setup.py
@@ -64,7 +64,6 @@ setup_options = dict(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
     ],
 )
 

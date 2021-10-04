9a30775aa0a8f91f8aa95c9ebbb95fbbc2136422
kyleknap <kyknapp1@gmail.com>
2021-10-03 21:23:05 -0700

    Centralize default auto-complete index gen logic
    There were a couple of places where the default auto-complete
    index generation was being built-up. This makes it so that tests,
    scripts, and any future build logic all use the same index generator.

--- tests/functional/autocomplete/test_generator.py
+++ tests/functional/autocomplete/test_generator.py
@@ -10,27 +10,34 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+import glob
 import os
 import tempfile
 
-from awscli.testutils import unittest, create_clidriver
+from awscli.testutils import unittest
 from awscli.autocomplete import generator
-from awscli.autocomplete.local import model, indexer
-from awscli.autocomplete.serverside.indexer import create_apicall_indexer
+from awscli.autocomplete.local import model
 
 
 class TestCanGenerateEntireIndex(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
         self.tempfile = os.path.join(self.temp_dir, 'temp.db')
-        self.model_indexer = indexer.create_model_indexer(self.tempfile)
-        self.api_call_indexer = create_apicall_indexer(self.tempfile)
         self.model_index = model.ModelIndex(self.tempfile)
 
     def tearDown(self):
-        self.model_indexer._db_connection.close()
-        self.api_call_indexer._db_connection.close()
         self.model_index._db_connection.close()
+
+    def assert_no_temp_index_files(self):
+        # When generating the index, it generates it in a file different from
+        # its final location so that only a completely built index is ever at
+        # the final location. This intermediary file shares the same name as
+        # final location except it has an additional suffix extension (e.g.,
+        # ``.temp`` at the end). So this check ensures there are no lingering
+        # intermediary files from building the index (even if the suffix
+        # extension changes in the future).
+        possible_matches = glob.glob(f'{self.tempfile}.*')
+        self.assertEqual(possible_matches, [])
 
     def test_can_generate_entire_index(self):
         # The point of this test is to make sure generating the index
@@ -38,16 +45,14 @@ class TestCanGenerateEntireIndex(unittest.TestCase):
         # generation are tested in tests/unit/autocomplete.  Here we just
         # generate the entire index and perform basic sanity checks.  It's
         # just a functional smoke test.
-        driver = create_clidriver()
-        index_generator = generator.IndexGenerator([
-            self.model_indexer,
-            self.api_call_indexer,
-        ])
-        index_generator.generate_index(driver)
+        generator.generate_index(self.tempfile)
 
         # Basic sanity checks.  Index generation for the entire CLI
         # takes a while so all the sanity checks are combined in a single
         # test method.
+        self.assertTrue(os.path.exists(self.tempfile))
+        self.assert_no_temp_index_files()
+
         commands = self.model_index.command_names(lineage=['aws'])
         self.assertIn('ec2', commands)
         self.assertIn('s3', commands)

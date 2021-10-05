e3027f9f51e9e03a625649e84fdab99ae2dded68
c22 <c22@users.noreply.github.com>
2021-10-05 20:33:30 +1100

    Suppress unnecessary error output in aws.cmd
    The call to "assoc .py" produces unnecessary stderr output every time aws.cmd is invoked if there are no system-level associations with .py files, eg.
    
    File association not found for extension .py
    
    We can suppress the error output by redirecting the stderr output to the NUL device (similar to what you would do in POSIX environments).
    
    This has no effect on the "happy case" where file associations do exist.

--- bin/aws.cmd
+++ bin/aws.cmd
@@ -9,7 +9,7 @@ for %%i in (cmd bat exe) do (
         call :SetPythonExe "%%~$PATH:j"
     )
 )
-for /f "tokens=2 delims==" %%i in ('assoc .py') do (
+for /f "tokens=2 delims==" %%i in ('assoc .py 2^>NUL') do (
     for /f "tokens=2 delims==" %%j in ('ftype %%i') do (
         for /f "tokens=1" %%k in ("%%j") do (
             call :SetPythonExe %%k

dafff80f3b87f9f08aa2b54ffb0a126c7a9dc725
Di Zhang <dzhg@amazon.com>
2021-12-07 14:50:55 -0800

    Add S3 GLACIER_IR storage class
    Add support for S3 GLACIER_IR storage class in various sub commands

--- tests/functional/s3/test_cp_command.py
+++ tests/functional/s3/test_cp_command.py
@@ -174,6 +174,21 @@ class TestCPCommand(BaseCPCommandTest):
         self.assertEqual(args['Bucket'], 'bucket')
         self.assertEqual(args['StorageClass'], 'DEEP_ARCHIVE')
 
+    def test_upload_glacier_ir(self):
+        full_path = self.files.create_file('foo.txt', 'mycontent')
+        cmdline = ('%s %s s3://bucket/key.txt --storage-class GLACIER_IR' %
+                   (self.prefix, full_path))
+        self.parsed_responses = \
+            [{'ETag': '"c8afdb36c52cf4727836669019e69222"'}]
+        self.run_cmd(cmdline, expected_rc=0)
+        self.assertEqual(len(self.operations_called), 1,
+                         self.operations_called)
+        self.assertEqual(self.operations_called[0][0].name, 'PutObject')
+        args = self.operations_called[0][1]
+        self.assertEqual(args['Key'], 'key.txt')
+        self.assertEqual(args['Bucket'], 'bucket')
+        self.assertEqual(args['StorageClass'], 'GLACIER_IR')
+
     def test_operations_used_in_download_file(self):
         self.parsed_responses = [
             {"ContentLength": "100", "LastModified": "00:00:00Z"},

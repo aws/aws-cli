55df5501db1b2353f6482e4464becc926923bb8e
Uri <uri@speedy.net>
2021-12-11 05:57:19 +0200

    Changed order of values in STORAGE_CLASS, according to https://aws.amazon.com/s3/storage-classes/?nc=sn&loc=3.

--- awscli/customizations/s3/subcommands.py
+++ awscli/customizations/s3/subcommands.py
@@ -247,14 +247,14 @@ SSE_C_COPY_SOURCE_KEY = {
 
 
 STORAGE_CLASS = {'name': 'storage-class',
-                 'choices': ['STANDARD', 'REDUCED_REDUNDANCY', 'STANDARD_IA',
-                             'ONEZONE_IA', 'INTELLIGENT_TIERING', 'GLACIER',
-                             'DEEP_ARCHIVE', 'GLACIER_IR'],
+                 'choices': ['STANDARD', 'INTELLIGENT_TIERING', 'STANDARD_IA',
+                             'ONEZONE_IA', 'GLACIER_IR', 'GLACIER',
+                             'DEEP_ARCHIVE', 'REDUCED_REDUNDANCY'],
                  'help_text': (
                      "The type of storage to use for the object. "
-                     "Valid choices are: STANDARD | REDUCED_REDUNDANCY "
-                     "| STANDARD_IA | ONEZONE_IA | INTELLIGENT_TIERING "
-                     "| GLACIER | DEEP_ARCHIVE | GLACIER_IR. "
+                     "Valid choices are: STANDARD | INTELLIGENT_TIERING "
+                     "| STANDARD_IA | ONEZONE_IA | GLACIER_IR "
+                     "| GLACIER | DEEP_ARCHIVE | REDUCED_REDUNDANCY. "
                      "Defaults to 'STANDARD'")}
 
 

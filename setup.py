c810bd0d060263c3542320fbafabb07d30ca54ab
aws-sdk-python-automation <github-aws-sdk-python-automation@amazon.com>
2022-02-22 19:06:51 +0000

    Bumping version to 1.22.60

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def find_version(*file_paths):
 
 
 install_requires = [
-    'botocore==1.24.4',
+    'botocore==1.24.5',
     'docutils>=0.10,<0.16',
     's3transfer>=0.5.0,<0.6.0',
     'PyYAML>=3.10,<5.5',

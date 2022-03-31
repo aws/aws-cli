cacb21f82fcf9db1026f8c7ede4328aefd686b76
Damans227 <61474540+Damans227@users.noreply.github.com>
2022-03-30 20:28:07 -0400

    remove a typo

--- awscli/botocore/configprovider.py
+++ awscli/botocore/configprovider.py
@@ -9,7 +9,7 @@
 # or in the "license" file accompanying this file. This file is
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the Licenscae.
+# language governing permissions and limitations under the License.
 """This module contains the inteface for controlling how configuration
 is loaded.
 """

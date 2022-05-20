9e84f69751528a63e056f83e4c819300dfe9230d
stealthycoin <stealthycoin@users.noreply.github.com>
2022-05-19 17:19:47 -0700

    Bump docutils upper bound to <0.17
    Change Sphinx to 1.3.2 which is compatible with docutils > 0.15.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def find_version(*file_paths):
 
 install_requires = [
     'botocore==1.26.4',
-    'docutils>=0.10,<0.16',
+    'docutils>=0.10,<0.17',
     's3transfer>=0.5.0,<0.6.0',
     'PyYAML>=3.10,<5.5',
     'colorama>=0.2.5,<0.4.5',

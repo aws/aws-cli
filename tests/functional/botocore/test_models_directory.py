741edfafa9064625ea156364d98198de40628cb8
kyleknap <kyknapp1@gmail.com>
2022-06-28 13:49:29 -0700

    Remove all examples-1.json models
    These files are not used by the AWS CLI and were unnecessarily
    increasing the total space required by the AWS CLI.

---
+++ tests/functional/botocore/test_models_directory.py
@@ -0,0 +1,24 @@
+# Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+import pytest
+
+import botocore.session
+
+
+@pytest.fixture
+def loader():
+    return botocore.session.Session().get_component('data_loader')
+
+
+def test_models_contains_no_examples_files(loader):
+    assert loader.list_available_services('examples-1') == []

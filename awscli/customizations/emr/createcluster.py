a742f2d2d36074116b3cf3e85f9ec3ad72b177a9
FomalhautWeisszwerg <weisszwerg@gmail.com>
2023-11-23 03:34:41 +0900

    fix: "invalid escape sequence" error in some regexp and strings.
    Fixed "invalid escape sequence" errors caused by missing 'r' modifiers
    or incomplete escaping in some strings.

--- awscli/customizations/emr/createcluster.py
+++ awscli/customizations/emr/createcluster.py
@@ -196,7 +196,7 @@ class CreateCluster(Command):
 
         if (parsed_args.release_label is None and
                 parsed_args.ami_version is not None):
-            is_valid_ami_version = re.match('\d?\..*', parsed_args.ami_version)
+            is_valid_ami_version = re.match(r'\d?\..*', parsed_args.ami_version)
             if is_valid_ami_version is None:
                 raise exceptions.InvalidAmiVersionError(
                     ami_version=parsed_args.ami_version)

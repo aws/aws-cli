ee19a34bd60b01e5683757eddae4cb17130ab0ac
Kenneth Daily <kdaily@amazon.com>
2024-01-23 10:09:24 -0800

    Fix tests so they do not create local directories

--- tests/functional/s3/test_sync_command.py
+++ tests/functional/s3/test_sync_command.py
@@ -472,12 +472,12 @@ class TestSyncCommandWithS3Express(BaseS3TransferCommandTest):
     prefix = 's3 sync '
 
     def test_incompatible_with_sync_upload(self):
-        cmdline = '%s localdirectory/ s3://testdirectorybucket--usw2-az1--x-s3/' % self.prefix
+        cmdline = '%s %s s3://testdirectorybucket--usw2-az1--x-s3/' % (self.prefix, self.files.rootdir)
         stderr = self.run_cmd(cmdline, expected_rc=252)[1]
         self.assertIn('Cannot use sync command with a directory bucket.', stderr)
 
     def test_incompatible_with_sync_download(self):
-        cmdline = '%s s3://testdirectorybucket--usw2-az1--x-s3/ localdirectory/' % self.prefix
+        cmdline = '%s s3://testdirectorybucket--usw2-az1--x-s3/ %s' % (self.prefix, self.files.rootdir)
         stderr = self.run_cmd(cmdline, expected_rc=252)[1]
         self.assertIn('Cannot use sync command with a directory bucket.', stderr)
 

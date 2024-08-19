bccef3fe2761b5a47f62174e6aed1c2dbc9a4f56
Ahmed Moustafa <35640105+aemous@users.noreply.github.com>
2024-08-19 17:29:55 -0400

    [v2] Load smoke tests from data file (#8846)

--- tests/integration/test_smoke.py
+++ tests/integration/test_smoke.py
@@ -12,109 +12,20 @@
 # language governing permissions and limitations under the License.
 import re
 import os
-import random
 import time
+import json
 
 import pytest
 
 from awscli.testutils import aws
 
+INTEG_TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# These are a list of commands that we should run.
-# We're just verifying that we can properly send a no-arg request
-# and that we can parse any response that comes back.
-COMMANDS = [
-    'autoscaling describe-account-limits',
-    'autoscaling describe-adjustment-types',
-    'cloudformation describe-stacks',
-    'cloudformation list-stacks',
-    'cloudsearch describe-domains',
-    'cloudsearch list-domain-names',
-    'cloudtrail describe-trails',
-    'cloudwatch list-metrics',
-    'cognito-identity list-identity-pools --max-results 1',
-    'datapipeline list-pipelines',
-    'directconnect describe-connections',
-    'dynamodb list-tables',
-    'ec2 describe-instances',
-    'ec2 describe-regions',
-    'elasticache describe-cache-clusters',
-    'elb describe-load-balancers',
-    'emr list-clusters',
-    # Smoke test for timestamp parsing.
-    'emr list-clusters --created-after 2014-11-24T00:00:00',
-    'iam list-users',
-    'kinesis list-streams',
-    'kms generate-random --number-of-bytes 128',
-    'logs describe-log-groups',
-    # 'opsworks describe-stacks',
-    'rds describe-db-instances',
-    'redshift describe-clusters',
-    'route53 list-hosted-zones',
-    'route53domains list-domains',
-    's3api list-buckets',
-    's3 ls',
-    'ses list-identities',
-    'sns list-topics',
-    'sqs list-queues',
-    'storagegateway list-gateways',
-    'swf list-domains --registration-status REGISTERED',
-    ('swf list-open-workflow-executions --domain foo '
-     '--start-time-filter oldestDate=1970-01-01'),
-
-    # Verify waiters as well.  We're picking the
-    # "resource does not exist" type waiters so we can
-    # give an identifier that doesn't exist and verify we have
-    # a 0 rc.
-    'rds wait db-instance-deleted --db-instance-identifier foo-123',
-]
-
-
-# A list of commands that generate error messages.  The idea is to try to have
-# at least one command for each service.
-#
-# This verifies that service errors are properly displayed to the user, as
-# opposed to either silently failing or inproperly handling the error responses
-# and not displaying something useful.  Each command tries to call an operation
-# with an identifier that does not exist, and part of the identifier is also
-# randomly generated to help ensure that is the case.
-ERROR_COMMANDS = [
-    'autoscaling attach-instances --auto-scaling-group-name %s',
-    'cloudformation cancel-update-stack --stack-name %s',
-    'cloudsearch describe-suggesters --domain-name %s',
-    'cloudtrail get-trail-status --name %s',
-    'cognito-identity delete-identity-pool --identity-pool-id %s',
-    'datapipeline delete-pipeline --pipeline-id %s',
-    'directconnect delete-connection --connection-id %s',
-    'dynamodb delete-table --table-name %s',
-    'ec2 terminate-instances --instance-ids %s',
-    'elasticache delete-cache-cluster --cache-cluster-id %s',
-    'elb describe-load-balancers --load-balancer-names %s',
-    'emr list-instances --cluster-id %s',
-    'iam delete-user --user-name %s',
-    'kinesis delete-stream --stream-name %s',
-    'logs delete-log-group --log-group-name %s',
-    # 'opsworks delete-app --app-id %s',
-    'rds delete-db-instance --db-instance-identifier %s',
-    'redshift delete-cluster --cluster-identifier %s',
-    'route53 delete-hosted-zone --id %s',
-    'route53domains get-domain-detail --domain-name %s',
-    's3api head-bucket --bucket %s',
-    'ses set-identity-dkim-enabled --identity %s --dkim-enabled',
-    'sns delete-endpoint --endpoint-arn %s',
-    'sqs delete-queue --queue-url %s',
-    # --gateway-arn has min length client side validation
-    # so we have to generate an identifier that's long enough.
-    ('storagegateway delete-gateway --gateway-arn '
-     'foo-cli-test-foo-cli-test-foo-cli-test-%s'),
-    'swf deprecate-domain --name %s',
-]
-
-
-# These services require a particular region to run.
-REGION_OVERRIDES = {
-    'route53domains': 'us-east-1'
-}
+with open(os.path.join(INTEG_TESTS_DIR, "smoke-tests.json"), "r") as definitions_file:
+    SMOKE_TESTS = json.load(definitions_file)
+COMMANDS = [" ".join(cmd) for cmd in SMOKE_TESTS["Commands"]]
+ERROR_COMMANDS = [" ".join(cmd) for cmd in SMOKE_TESTS["ErrorCommands"]]
+REGION_OVERRIDES = SMOKE_TESTS.get("RegionOverrides", {})
 
 
 def _aws(command_string, max_attempts=1, delay=5, target_rc=0):
@@ -141,8 +52,6 @@ def test_can_make_success_request(cmd):
 
 @pytest.mark.parametrize('cmd', ERROR_COMMANDS)
 def test_display_error_message(cmd):
-    identifier = 'foo-awscli-test-%s' % random.randint(1000, 100000)
-    cmd = cmd % identifier
     result = _aws(cmd, target_rc=254)
     assert result.rc == 254
     error_message = re.compile(

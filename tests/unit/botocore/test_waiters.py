4c1cc3b372f7edef84740d870f8777fd719fe376
aemous <amoustafa0518@gmail.com>
2024-09-03 14:03:57 -0400

    Add Python 3.12 support and update related tests, scripts, and config files.

--- tests/unit/botocore/test_waiters.py
+++ tests/unit/botocore/test_waiters.py
@@ -678,7 +678,7 @@ class TestCreateWaiter(unittest.TestCase):
             ('    Polls :py:meth:`MyService.Client.foo` every 1 '
              'seconds until a successful state is reached. An error '
              'is returned after 1 failed checks.'),
-            '    **Request Syntax** ',
+            '    **Request Syntax**',
             '    ::',
             '      waiter.wait(',
             "          bar='string'",

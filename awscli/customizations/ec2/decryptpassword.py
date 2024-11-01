6a8a922e53dcdcc92d7eedfe88c08f6969b080ba
Kenneth Daily <kdaily@amazon.com>
2024-11-01 09:19:43 -0700

    Remove unused variable

--- awscli/customizations/ec2/decryptpassword.py
+++ awscli/customizations/ec2/decryptpassword.py
@@ -80,8 +80,6 @@ class LaunchKeyArgument(BaseCLIArgument):
             path = os.path.expanduser(path)
             if os.path.isfile(path):
                 self._key_path = path
-                endpoint_prefix = \
-                    self._operation_model.service_model.endpoint_prefix
                 service_id = self._operation_model.service_model.service_id
                 event = 'after-call.%s.%s' % (service_id.hyphenize(),
                                               self._operation_model.name)

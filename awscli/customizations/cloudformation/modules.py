9eac23a48565fa890974d4d31d967e37d28833cc
Eric Beard <ericzbeard@gmail.com>
2024-12-04 15:22:24 -0800

    Start resolving refs

--- awscli/customizations/cloudformation/modules.py
+++ awscli/customizations/cloudformation/modules.py
@@ -28,6 +28,10 @@ CONDITION = "Condition"
 DEFAULT = "Default"
 NAME = "Name"
 SOURCE = "Source"
+REF = "Ref"
+SUB = "Fn::Sub"
+GETATT = "Fn::GetAtt"
+PARAMETERS = "Parameters"
 
 
 def read_source(source):
@@ -38,6 +42,56 @@ def read_source(source):
 
     with open(source, "r") as s:
         return s.read()
+
+
+def merge_props(original, overrides):
+    """
+    Merge props merges dicts, replacing values in the original with overrides.
+    This function is recursive and can act on lists and scalars.
+
+    :return A new value with the overridden properties
+
+    Original:
+
+    A:
+      B: foo
+      C:
+        D: bar
+
+    Override:
+
+    A:
+      B: baz
+      C:
+        E: qux
+
+    Result:
+
+    A:
+      B: baz
+      C:
+        D: bar
+        E: qux
+    """
+    original_type = type(original)
+    override_type = type(overrides)
+    if override_type is not dict and override_type is not list:
+        return overrides
+
+    if original_type is not override_type:
+        return overrides
+
+    if original_type is dict:
+        retval = original.copy()
+        for k in original:
+            if k in overrides:
+                retval[k] = merge_props(retval[k], overrides[k])
+        for k in overrides:
+            if k not in original:
+                retval[k] = overrides[k]
+        return retval
+    else:
+        return original + overrides
 
 
 class Module:
@@ -87,10 +141,14 @@ class Module:
         self.source = module_config[SOURCE]
 
         # The Properties from the parent template
-        self.props = module_config[PROPERTIES]
+        self.props = {}
+        if PROPERTIES in module_config:
+            self.props = module_config[PROPERTIES]
 
         # The Overrides from the parent template
-        self.overrides = module_config[OVERRIDES]
+        self.overrides = {}
+        if OVERRIDES in module_config:
+            self.overrides = module_config[OVERRIDES]
 
         # Resources defined in the module
         self.resources = {}
@@ -107,9 +165,7 @@ class Module:
 
     def process(self):
         """
-        Read the module source and return a dictionary that looks like a
-        template, with keys such as 'Resources' that have the processed
-        elements to be injected into the parent.
+        Read the module source process it.
 
         :return: The modified parent template dictionary
         """
@@ -120,11 +176,14 @@ class Module:
         if RESOURCES not in module_dict:
             msg = "Modules must have a Resources section"
             raise exceptions.InvalidModuleError(msg=msg)
+        self.resources = module_dict[RESOURCES]
+
+        if PARAMETERS in module_dict:
+            self.params = module_dict[PARAMETERS]
 
         self.validate_overrides()
 
-        # TODO - For each resource in the module:
-        for logical_id, resource in module_dict[RESOURCES].items():
+        for logical_id, resource in self.resources.items():
             self.process_resource(logical_id, resource)
 
         return self.template
@@ -136,16 +195,126 @@ class Module:
     def process_resource(self, logical_id, resource):
         "Process a single resource"
 
-        # TODO - For each property (and property-like attribute),
-        #        replace the value if it appears in parent overrides.
-        # (Properties, CreationPolicy, Metadata, UpdatePolicy)
+        # For each property (and property-like attribute),
+        # replace the value if it appears in parent overrides.
+        attrs = [
+            PROPERTIES,
+            CREATIONPOLICY,
+            METADATA,
+            UPDATEPOLICY,
+            DELETIONPOLICY,
+            CONDITION,
+            UPDATEREPLACEPOLICY,
+            DEPENDSON,
+        ]
+        for a in attrs:
+            self.process_overrides(logical_id, resource, a)
 
-        # TODO - Replace scalar attributes with overrides
-        # (DeletionPolicy, UpdateReplacePolicy, Condition)
+        # Resolve refs, subs, and getatts
+        #    (Process module Parameters and parent Properties)
+        self.resolve(logical_id, resource)
 
-        # TODO - Replace DependsOn with overrides
+        self.template[RESOURCES][self.name + logical_id] = resource
 
-        # TODO - Resolve refs, subs, and getatts
-        #        (Process module Parameters and parent Properties)
+    def process_overrides(self, logical_id, resource, attr_name):
+        """
+        Replace overridden values in a property-like attribute of a resource.
 
-        self.template[RESOURCES]["Prefix?" + logical_id] = resource
+        (Properties, Metadata, CreationPolicy, and UpdatePolicy)
+
+        Overrides are a way to customize modules without needing a Parameter.
+
+        Example template.yaml:
+
+        Modules:
+          Foo:
+            Source: ./module.yaml
+            Overrides:
+              Bar:
+                Properties:
+                  Name: bbb
+
+        Example module.yaml:
+
+        Resources:
+          Bar:
+            Type: A::B::C
+            Properties:
+              Name: aaa
+
+        Output yaml:
+
+        Resources:
+          Bar:
+            Type: A::B::C
+            Properties:
+              Name: bbb
+        """
+
+        if logical_id not in self.overrides:
+            return
+
+        resource_overrides = self.overrides[logical_id]
+        if attr_name not in resource_overrides:
+            return
+
+        # Might be overriding something that's not in the module at all,
+        # like a Bucket with no Properties
+        if attr_name not in resource:
+            if attr_name in resource_overrides:
+                resource[attr_name] = resource_overrides[attr_name]
+            else:
+                return
+
+        original = resource[attr_name]
+        overrides = resource_overrides[attr_name]
+        resource[attr_name] = merge_props(original, overrides)
+
+    def resolve(self, k, v):
+        """
+        Resolve Refs, Subs, and GetAtts recursively.
+
+        :param k The name of the node
+        :param v The value of the node
+
+        """
+        if k == REF:
+            self.resolve_ref(v)
+        elif k == SUB:
+            self.resolve_sub(v)
+        elif k == GETATT:
+            self.resolve_getatt(v)
+        else:
+            if type(v) is dict:
+                for k2, v2 in v.items():
+                    self.resolve(k2, v2)
+            elif type(v) is list:
+                for v2 in v:
+                    if type(v2) is dict:
+                        for k3, v3 in v2.items():
+                            self.resolve(k3, v3)
+
+    def resolve_ref(v):
+        """
+        Look for the Ref in the parent template Properties if it matches
+        a module Parameter name. If it's not there, use the default if
+        there is one. If not, raise an error.
+
+        If there is no matching Parameter, look for a resource with that
+        name in this module and fix the logical id so it has the prefix.
+
+        Otherwise just leave it be and assume the module author is
+        expecting the parent template to have that Reference.
+        """
+        if type(v) is not str:
+            msg = f"Ref should be a string: {v}"
+            raise exceptions.InvalidModuleError(msg=msg)
+        # TODO
+
+    def resolve_sub(v):
+        pass
+        # TODO
+
+    def resolve_getatt(v):
+        pass
+        # TODO
--- tests/unit/customizations/cloudformation/test_modules.py
+++ tests/unit/customizations/cloudformation/test_modules.py
@@ -10,11 +10,18 @@ class TestPackageModules(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_merge_props(self):
+        original = {"b": "c", "d": {"e": "f", "i": [1, 2, 3]}}
+        overrides = {"b": "cc", "d": {"e": "ff", "g": "h", "i": [4, 5]}}
+        expect = {"b": "cc", "d": {"e": "ff", "g": "h", "i": [1, 2, 3, 4, 5]}}
+        merged = modules.merge_props(original, overrides)
+        self.assertEqual(merged, expect)
+        # TODO: More complex examples (especially merging Policies)
+
     def test_main(self):
         base = "tests/unit/customizations/cloudformation/modules"
         t = modules.read_source(f"{base}/basic-template.yaml")
         td = yamlhelper.yaml_parse(t)
-        # m = modules.read_source(f"{base}/basic-module.yaml")
         e = modules.read_source(f"{base}/basic-expect.yaml")
 
         for module_name, module_config in td[MODULES].items():
@@ -25,5 +32,3 @@ class TestPackageModules(unittest.TestCase):
             td2 = module.process()
             t2 = yamlhelper.yaml_dump(td2)
             self.assertEqual(e, t2)
-
-        # self.assertEqual(1, 0)

bcfce69b973188dffad137a788cf53017145ea9b
Kenneth Daily <kdaily@amazon.com>
2024-12-19 14:06:06 -0800

    Run yaml, end of file, and whitespace checks

--- awscli/customizations/cloudtrail/validation.py
+++ awscli/customizations/cloudtrail/validation.py
@@ -949,10 +949,10 @@ class CloudTrailValidateLogs(BasicCommand):
             else:
                 self._valid_logs += 1
                 self._write_status(
-                    
+
                         'Log file\ts3://%s/%s\tvalid'
                         % (log['s3Bucket'], log['s3Object'])
-                    
+
                 )
         except ClientError as e:
             if e.response['Error']['Code'] != 'NoSuchKey':

7af769aefce545ab3a0d06518489cacfc87a5bd5
Ahmed Moustafa <35640105+aemous@users.noreply.github.com>
2025-01-14 17:33:06 -0500

    [v2] Modifies botocore tests to use the defaults created in configprovider.py (#9211)

--- tests/unit/botocore/test_args.py
+++ tests/unit/botocore/test_args.py
@@ -14,7 +14,7 @@
 import socket
 
 import botocore.config
-from tests import mock, unittest
+from tests import get_botocore_default_config_mapping, mock, unittest
 
 from botocore import args
 from botocore import exceptions
@@ -29,7 +29,8 @@ from botocore.useragent import UserAgentString
 class TestCreateClientArgs(unittest.TestCase):
     def setUp(self):
         self.event_emitter = mock.Mock(HierarchicalEmitter)
-        self.config_store = ConfigValueStore()
+        default_config_mapping = get_botocore_default_config_mapping()
+        self.config_store = ConfigValueStore(mapping=default_config_mapping)
         user_agent_creator = UserAgentString(
             platform_name=None,
             platform_version=None,
@@ -321,11 +322,17 @@ class TestCreateClientArgs(unittest.TestCase):
         )['client_config']
         self.assertEqual(config.retries['max_attempts'], 2)
 
-    def test_max_attempts_unset_if_retries_is_none(self):
+    def test_max_attempts_matches_config_store_if_retries_is_none(self):
         config = self.call_get_client_args(
                 client_config=Config(retries=None)
         )['client_config']
-        self.assertEqual(config.retries, {'mode': 'standard'})
+        self.assertEqual(
+            config.retries,
+            {
+                'max_attempts': self.config_store.get_config_variable('max_attempts'),
+                'mode': 'standard'
+            }
+        )
 
     def test_retry_mode_set_on_config_store(self):
         self.config_store.set_config_variable('retry_mode', 'standard')

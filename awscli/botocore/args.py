e764f66a34495f9557f7f544bb88acc71bb76e39
Kenneth Daily <kdaily@amazon.com>
2025-02-11 12:55:59 -0800

    Add configuration to disable host prefix injection

--- awscli/botocore/args.py
+++ awscli/botocore/args.py
@@ -28,7 +28,7 @@ from botocore.regions import EndpointResolverBuiltins as EPRBuiltins
 from botocore.regions import EndpointRulesetResolver
 from botocore.signers import RequestSigner
 from botocore.useragent import UserAgentString
-from botocore.utils import ensure_boolean, is_s3_accelerate_url
+from botocore.utils import DEFAULT_TRUE, ensure_boolean, is_s3_accelerate_url
 
 logger = logging.getLogger(__name__)
 
@@ -236,6 +236,7 @@ class ClientArgsCreator(object):
         self._compute_user_agent_appid_config(config_kwargs)
         self._compute_sigv4a_signing_region_set_config(config_kwargs)
         self._compute_checksum_config(config_kwargs)
+        self._compute_inject_host_prefix(config_kwargs)
         s3_config = self.compute_s3_config(client_config)
 
         is_s3_service = self._is_s3_service(service_name)
@@ -255,6 +256,30 @@ class ClientArgsCreator(object):
             's3_config': s3_config,
             'socket_options': self._compute_socket_options(scoped_config)
         }
+
+    def _compute_inject_host_prefix(self, config_kwargs):
+        # If the value isn't available, that is because the user didn't pass in a Config
+        # object. UNSET is used to differentiate between other currently allowed values,
+        #  like None.
+        inject_host_prefix = config_kwargs.get('inject_host_prefix', 'UNSET')
+
+        # A value of DEFAULT_TRUE means the user did not supply a value
+        # specifically for inject_host_prefix; a value of UNSET means the user
+        # didn't provide a Config object to the client constructor. In these
+        # cases we want to fall back to the order of precedence for resolving
+        # the value.
+        if inject_host_prefix is DEFAULT_TRUE or inject_host_prefix == 'UNSET':
+            configured_disable_host_prefix_injection = (
+                self._config_store.get_config_variable(
+                    'disable_host_prefix_injection'
+                )
+            )
+            if configured_disable_host_prefix_injection is not None:
+                config_kwargs[
+                    'inject_host_prefix'
+                ] = not configured_disable_host_prefix_injection
+            else:
+                config_kwargs['inject_host_prefix'] = True
 
     def _compute_configured_endpoint_url(self, client_config, endpoint_url):
         if endpoint_url is not None:

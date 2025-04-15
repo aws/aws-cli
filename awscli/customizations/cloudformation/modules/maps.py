eefa26e8c0d6d40710d3c224c05c2d1296cba203
Eric Z. Beard <ericzbeard@gmail.com>
2025-04-15 16:52:08 -0700

    Replace Map with ForEach

--- awscli/customizations/cloudformation/modules/maps.py
+++ awscli/customizations/cloudformation/modules/maps.py
@@ -14,6 +14,9 @@
 """
 This file implements looping over module content.
 
+We originally called the attribute "Map" and renamed it to "ForEach" for
+consistency with Fn::ForEach.
+
 See tests/unit/customizations/cloudformation/modules/map*.yaml
 """
 
@@ -24,10 +27,10 @@ from awscli.customizations.cloudformation.modules.visitor import Visitor
 from awscli.customizations.cloudformation.modules.parse_sub import WordType
 from awscli.customizations.cloudformation.modules.parse_sub import parse_sub
 
-MAP_PLACEHOLDER = "$MapValue"
-INDEX_PLACEHOLDER = "$MapIndex"
+MAP_PLACEHOLDER = "$Identifier"
+INDEX_PLACEHOLDER = "$Index"
 SUB = "Fn::Sub"
-MAP = "Map"
+MAP = "ForEach"  # Renamed for consistency with Fn::ForEach
 REF = "Ref"
 PROPERTIES = "Properties"
 MODULES = "Modules"
@@ -36,6 +39,7 @@ RESOURCES = "Resources"
 OUTPUTS = "Outputs"
 ORIGINAL_MAP_NAME = "OriginalMapName"
 VALUE = "Value"
+FOREACH_PREFIX = "Fn::ForEach::"
 
 
 def replace_str(s, m, i):
@@ -50,8 +54,23 @@ def replace_str(s, m, i):
     return s.replace(INDEX_PLACEHOLDER, f"{i}")
 
 
+def replace_identifier(s, identifier, value):
+    """
+    Replace ${identifier} or &{identifier} in a string with the value.
+
+    s: The string to alter
+    identifier: The identifier to replace
+    value: The value to replace it with
+    """
+    # Replace ${identifier} with value
+    s = s.replace(f"${{{identifier}}}", value)
+    # Replace &{identifier} with value (for non-alphanumeric characters)
+    s = s.replace(f"&{{{identifier}}}", value)
+    return s
+
+
 def map_placeholders(i, token, val):
-    "Replace $MapValue and $MapIndex"
+    "Replace $Identifier and $Index"
 
     if isinstance(val, (dict, OrderedDict)):
         if SUB in val:
@@ -69,8 +88,8 @@ def map_placeholders(i, token, val):
                 return r
         elif GETATT in val:
             getatt = val[GETATT]
-            # Handle expressions like !GetAtt Content[$MapIndex].Name
-            # All we can do here is replace $MapIndex with a number.
+            # Handle expressions like !GetAtt Content[$Index].Name
+            # All we can do here is replace $Index with a number.
             # Later, when we resolve outputs, we need to fix it
             new_getatt = []
             for item in getatt:
@@ -81,7 +100,69 @@ def map_placeholders(i, token, val):
     return val
 
 
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-locals
+def foreach_placeholders(identifier, value, val):
+    """
+    Replace ${identifier} and &{identifier} in values.
+
+    identifier: The identifier to replace
+    value: The value to replace it with
+    val: The value to process
+    """
+    if isinstance(val, (dict, OrderedDict)):
+        if SUB in val:
+            sub = val[SUB]
+            if f"${{{identifier}}}" in sub or f"&{{{identifier}}}" in sub:
+                r = replace_identifier(sub, identifier, value)
+                words = parse_sub(r)
+                need_sub = False
+                for word in words:
+                    if word.t != WordType.STR:
+                        need_sub = True
+                        break
+                if need_sub:
+                    return {SUB: r}
+                return r
+        elif GETATT in val:
+            getatt = val[GETATT]
+            new_getatt = []
+            for item in getatt:
+                if isinstance(item, str) and (
+                    f"${{{identifier}}}" in item
+                    or f"&{{{identifier}}}" in item
+                ):
+                    new_getatt.append(
+                        replace_identifier(item, identifier, value)
+                    )
+                else:
+                    new_getatt.append(item)
+            return {GETATT: new_getatt}
+        else:
+            # Process nested dictionaries
+            result = {}
+            for k, v in val.items():
+                if isinstance(k, str) and (
+                    f"${{{identifier}}}" in k or f"&{{{identifier}}}" in k
+                ):
+                    new_key = replace_identifier(k, identifier, value)
+                    result[new_key] = foreach_placeholders(
+                        identifier, value, v
+                    )
+                else:
+                    result[k] = foreach_placeholders(identifier, value, v)
+            return result
+    elif isinstance(val, list):
+        # Process lists
+        return [foreach_placeholders(identifier, value, item) for item in val]
+    elif isinstance(val, str):
+        return replace_identifier(val, identifier, value)
+    return val
+
+
 # pylint: disable=cell-var-from-loop
+# pylint: disable=too-many-statements
 def process_module_maps(template, parent_module):
     """
     Loop over Maps in modules.
@@ -95,6 +176,8 @@ def process_module_maps(template, parent_module):
     """
     retval = {}
     modules = template[MODULES]
+
+    # Process Map attribute
     for k, v in modules.copy().items():
         if MAP in v:
             # Expect Map to be a CSV or ref to a CSV
@@ -102,7 +185,7 @@ def process_module_maps(template, parent_module):
             if isdict(m) and REF in m:
                 m = parent_module.find_ref(m[REF])
                 if m is None:
-                    msg = f"{k} has an invalid Map Ref"
+                    msg = f"{k} has an invalid ForEach Ref"
                     raise exceptions.InvalidModuleError(msg=msg)
             tokens = m.split(",")
             retval[k] = tokens
@@ -112,7 +195,7 @@ def process_module_maps(template, parent_module):
                 copied_module = copy.deepcopy(v)
                 copied_module[ORIGINAL_MAP_NAME] = k
                 del copied_module[MAP]
-                # Replace $Map and $Index placeholders
+                # Replace $Identifier and $Index placeholders
                 if PROPERTIES in copied_module:
 
                     def vf(vis):
@@ -123,6 +206,91 @@ def process_module_maps(template, parent_module):
 
                 modules[module_id] = copied_module
 
+            del modules[k]
+
+    # Process Fn::ForEach
+    for k, v in modules.copy().items():
+        if k.startswith(FOREACH_PREFIX):
+            prefix_len = len(FOREACH_PREFIX)
+            loop_name = k[prefix_len:]
+
+            # Validate ForEach structure
+            if not isinstance(v, list) or len(v) != 3:
+                msg = (
+                    f"Fn::ForEach::{loop_name} must be a list "
+                    + "with 3 elements: [identifier, collection, template]"
+                )
+                raise exceptions.InvalidModuleError(msg=msg)
+
+            identifier = v[0]
+            collection = v[1]
+            template_fragment = v[2]
+
+            # Handle collection that might be a Ref to a CommaDelimitedList
+            if isdict(collection) and REF in collection:
+                collection = parent_module.find_ref(collection[REF])
+                if collection is None:
+                    msg = f"Fn::ForEach::{loop_name} invalid collection Ref"
+                    raise exceptions.InvalidModuleError(msg=msg)
+
+            # Convert string to list if it's a comma-delimited string
+            if isinstance(collection, str):
+                collection = collection.split(",")
+
+            # Ensure collection is a list
+            if not isinstance(collection, list):
+                msg = (
+                    f"Fn::ForEach::{loop_name} collection must be a "
+                    + "list or a reference to a CommaDelimitedList"
+                )
+                raise exceptions.InvalidModuleError(msg=msg)
+
+            # Track the mapping for GetAtt resolution
+            retval[loop_name] = []
+
+            # Process each item in the collection
+            for i, value in enumerate(collection):
+                # Add to the mapping list
+                retval[loop_name].append(value)
+
+                # Process each key in the template fragment
+                for template_key, template_value in template_fragment.items():
+                    # Replace ${identifier} in the key
+                    if isinstance(template_key, str):
+                        new_key = replace_identifier(
+                            template_key, identifier, value
+                        )
+                    else:
+                        new_key = template_key
+
+                    # Check if the identifier is part of the object key
+                    identifier_in_key = isinstance(template_key, str) and (
+                        f"${{{identifier}}}" in template_key
+                        or f"&{{{identifier}}}" in template_key
+                    )
+
+                    # If identifier is in the key, use the key value itself
+                    # Otherwise, add an index to ensure unique module names
+                    if identifier_in_key:
+                        module_id = new_key
+                    else:
+                        module_id = f"{new_key}{i}"
+
+                    # Create a deep copy of the template value
+                    new_value = copy.deepcopy(template_value)
+
+                    # Replace ${identifier} in the value recursively
+                    processed_value = foreach_placeholders(
+                        identifier, value, new_value
+                    )
+
+                    # Add the processed module to the modules dictionary
+                    modules[module_id] = processed_value
+
+                    # Remember the original name for GetAtt resolution
+                    modules[module_id][ORIGINAL_MAP_NAME] = loop_name
+
+            # Remove the ForEach entry
             del modules[k]
 
     return retval

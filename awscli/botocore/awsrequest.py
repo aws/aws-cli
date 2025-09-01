9f52936d67799bed507a79dfc0f7e49a5de6dd3a
Arne Franken <franken@adobe.com>
2025-09-01 11:29:13 +0200

    Fix HTTP 100 Continue parsing to accept optional reason phrase
    Fix 100-continue behaviour with servers that do not send the optional
    reason phrase on 100-continue responses.
    See https://www.rfc-editor.org/rfc/rfc9110.html#section-15.1

--- awscli/botocore/awsrequest.py
+++ awscli/botocore/awsrequest.py
@@ -219,9 +219,9 @@ class AWSConnection:
 
     def _is_100_continue_status(self, maybe_status_line):
         parts = maybe_status_line.split(None, 2)
-        # Check for HTTP/<version> 100 Continue\r\n
+        # Check for HTTP/<version> 100 Continue\r\n or HTTP/<version> 100\r\n
         return (
-            len(parts) >= 3
+            len(parts) >= 2
             and parts[0].startswith(b'HTTP/')
             and parts[1] == b'100'
         )

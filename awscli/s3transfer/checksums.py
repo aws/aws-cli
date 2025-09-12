13cf5b1471352f96ec95a572d443a438ca3d66ac
Steve Yoo <hssyoo@amazon.com>
2025-09-12 15:04:05 -0400

    Implement checksum classes for full object calculation

--- awscli/s3transfer/checksums.py
+++ awscli/s3transfer/checksums.py
@@ -15,7 +15,95 @@ NOTE: All classes and functions in this module are considered private and are
 subject to abrupt breaking changes. Please do not use them directly.
 """
 
+import base64
 from copy import copy
+from functools import cached_property
+
+from botocore.httpchecksum import CrtCrc32cChecksum
+
+
+class PartStreamingChecksumBody:
+    def __init__(self, stream, starting_index, full_object_checksum):
+        self._stream = stream
+        self._starting_index = starting_index
+        self._checksum = CRC_CHECKSUM_CLS[
+            full_object_checksum.checksum_algorithm
+        ]()
+        self._full_object_checksum = full_object_checksum
+        # If the underlying stream already has a checksum object
+        # it's updating (eg `botocore.httpchecksum.StreamingChecksumBody`),
+        # reuse its calculated value.
+        self._reuse_checksum = hasattr(self._stream, 'checksum')
+
+    def read(self, *args, **kwargs):
+        value = self._stream.read(*args, **kwargs)
+        if not self._reuse_checksum:
+            self._checksum.update(value)
+        if not value:
+            self._set_part_checksum()
+        return value
+
+    def _set_part_checksum(self):
+        if not self._reuse_checksum:
+            value = self._checksum.int_crc
+        else:
+            value = self._stream.checksum.int_crc
+        self._full_object_checksum.set_part_checksum(
+            self._starting_index,
+            value,
+        )
+
+
+class FullObjectChecksum:
+    def __init__(self, checksum_algorithm, content_length):
+        self.checksum_algorithm = checksum_algorithm
+        self._content_length = content_length
+        self._combine_function = _CRC_CHECKSUM_TO_COMBINE_FUNCTION[
+            self.checksum_algorithm
+        ]
+        self._stored_checksum = None
+        self._part_checksums = None
+        self._calculated_checksum = None
+
+    @cached_property
+    def calculated_checksum(self):
+        if self._calculated_checksum is None:
+            self._combine_part_checksums()
+        return self._calculated_checksum
+
+    def set_stored_checksum(self, stored_checksum):
+        self._stored_checksum = stored_checksum
+
+    def set_part_checksum(self, offset, checksum):
+        if self._part_checksums is None:
+            self._part_checksums = {}
+        self._part_checksums[offset] = checksum
+
+    def _combine_part_checksums(self):
+        if self._part_checksums is None:
+            return
+        sorted_keys = sorted(self._part_checksums.keys())
+        combined = self._part_checksums[sorted_keys[0]]
+        for i, offset in enumerate(sorted_keys[1:]):
+            part_checksum = self._part_checksums[offset]
+            if i + 1 == len(sorted_keys) - 1:
+                next_offset = self._content_length
+            else:
+                next_offset = sorted_keys[i + 2]
+            offset_len = next_offset - offset
+            combined = self._combine_function(
+                combined, part_checksum, offset_len
+            )
+        self._calculated_checksum = base64.b64encode(
+            combined.to_bytes(4, byteorder='big')
+        ).decode('ascii')
+
+    def validate(self):
+        if self.calculated_checksum != self._stored_checksum:
+            raise ValueError(
+                f"Calculated checksum {self.calculated_checksum} does not match "
+                f"stored checksum {self._stored_checksum}"
+            )
 
 
 def provide_checksum_to_meta(response, transfer_meta):
@@ -105,4 +193,9 @@ def combine_crc32(crc1, crc2, len2):
 
 _CRC_CHECKSUM_TO_COMBINE_FUNCTION = {
     "ChecksumCRC32": combine_crc32,
+}
+
+
+CRC_CHECKSUM_CLS = {
+    "ChecksumCRC32": CrtCrc32Checksum,
 }

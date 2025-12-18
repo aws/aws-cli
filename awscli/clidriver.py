4232a974269eb8a8fa004098f65d4705140bb1a8
Andrew Asseily <nyandrew@amazon.com>
2025-12-18 13:12:40 -0500

    Restore error handler fallback

--- awscli/clidriver.py
+++ awscli/clidriver.py
@@ -246,6 +246,10 @@ class CLIDriver:
         else:
             self.session = session
         self._error_handler = error_handler
+        if self._error_handler is None:
+            self._error_handler = construct_cli_error_handlers_chain(
+                self.session
+            )
         if debug:
             self._set_logging(debug)
         self._update_config_chain()
@@ -532,11 +536,6 @@ class CLIDriver:
         command_table = self._get_command_table()
         parser = self.create_parser(command_table)
         self._add_aliases(command_table, parser)
-
-        if self._error_handler is None:
-            self._error_handler = construct_cli_error_handlers_chain(
-                self.session
-            )
 
         try:
             # Because _handle_top_level_args emits events, it's possible

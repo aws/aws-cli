1e1236aa226ea076c4a6c5349cc48e20538db2fc
aemous <amoustafa0518@gmail.com>
2025-12-31 14:39:46 -0500

    Formatting and linting.

--- awsclilinter/awsclilinter/rules/hidden_aliases.py
+++ awsclilinter/awsclilinter/rules/hidden_aliases.py
@@ -211,10 +211,7 @@ class HiddenAliasRule(LintRule):
                                     "regex": f"\\A--{self._hidden_alias}\\z",
                                 },
                             },
-                            {
-                                "kind": "raw_string",
-                                "regex": f"--{self._hidden_alias}"
-                            },
+                            {"kind": "raw_string", "regex": f"--{self._hidden_alias}"},
                         ]
                     }
                 },

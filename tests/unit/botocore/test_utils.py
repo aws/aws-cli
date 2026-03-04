ed67e7d9c7511006add3c748c8df362f8ef759ae
Steve Yoo <106777148+hssyoo@users.noreply.github.com>
2026-03-04 15:58:09 -0500

    Validate region name from redirect (#10115)

--- tests/unit/botocore/test_utils.py
+++ tests/unit/botocore/test_utils.py
@@ -62,6 +62,7 @@ from botocore.utils import (
     IMDSFetcher,
     InstanceMetadataFetcher,
     InvalidArnException,
+    InvalidRegionError,
     S3ArnParamHandler,
     S3EndpointSetter,
     S3RegionRedirectorv2,
@@ -2115,6 +2116,47 @@ class TestS3RegionRedirector(unittest.TestCase):
             request_dict, response, self.operation
         )
         self.assertEqual(redirect_response, None)
+
+    def test_get_region_validates_region_from_header(self):
+        response = (
+            None,
+            {
+                'Error': {'Code': 'PermanentRedirect'},
+                'ResponseMetadata': {
+                    'HTTPHeaders': {'x-amz-bucket-region': 'invalid region!'}
+                },
+            },
+        )
+        with self.assertRaises(InvalidRegionError):
+            self.redirector.get_bucket_region('foo', response)
+
+    def test_get_region_validates_region_from_error_body(self):
+        response = (
+            None,
+            {
+                'Error': {
+                    'Code': 'PermanentRedirect',
+                    'Region': 'invalid region!',
+                },
+                'ResponseMetadata': {'HTTPHeaders': {}},
+            },
+        )
+        with self.assertRaises(InvalidRegionError):
+            self.redirector.get_bucket_region('foo', response)
+
+    def test_get_region_validates_region_from_head_bucket(self):
+        self.set_client_response_headers(
+            {'x-amz-bucket-region': 'invalid region!'}
+        )
+        response = (
+            None,
+            {
+                'Error': {'Code': 'PermanentRedirect'},
+                'ResponseMetadata': {'HTTPHeaders': {}},
+            },
+        )
+        with self.assertRaises(InvalidRegionError):
+            self.redirector.get_bucket_region('foo', response)
 
 
 class TestArnParser(unittest.TestCase):

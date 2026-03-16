b30f328d4a679802cd16d4c30fcf4c5bbb9c1cde
aws-sdk-python-automation <github-aws-sdk-python-automation@amazon.com>
2026-03-16 18:15:01 +0000

    Merge customizations for Bedrock AgentCore

--- awscli/customizations/removals.py
+++ awscli/customizations/removals.py
@@ -88,8 +88,13 @@ def register_removals(event_handler):
             'retrieve-and-generate-stream',
         ],
     )
-    cmd_remover.remove(on_event='building-command-table.bedrock-agentcore',
-                         remove_commands=['invoke-code-interpreter'])
+    cmd_remover.remove(
+        on_event='building-command-table.bedrock-agentcore',
+        remove_commands=[
+            'invoke-code-interpreter',
+            'invoke-agent-runtime-command',
+        ],
+    )
     cmd_remover.remove(
         on_event='building-command-table.qbusiness', remove_commands=['chat']
     )

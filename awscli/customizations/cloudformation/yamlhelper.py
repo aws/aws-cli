0a5b393b15727f8671be283cfcef4da6e8fefdd6
EhteshamSid <gh-patchbot@example.com>
2026-04-05 16:19:46 -0400

    Use safe_load in yamlhelper

--- awscli/customizations/cloudformation/yamlhelper.py
+++ awscli/customizations/cloudformation/yamlhelper.py
@@ -79,10 +79,10 @@ def _dict_constructor(loader, node):
 
 
 class SafeLoaderWrapper(yaml.SafeLoader):
-    """Isolated safe loader to allow for customizations without global changes.
-    """
+    """Isolated safe loader to allow for customizations without global changes."""
 
     pass
+
 
 def yaml_parse(yamlstr):
     """Parse a yaml string"""
@@ -93,10 +93,11 @@ def yaml_parse(yamlstr):
         return json.loads(yamlstr, object_pairs_hook=OrderedDict)
     except ValueError:
         loader = SafeLoaderWrapper
-        loader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, 
-                               _dict_constructor)
+        loader.add_constructor(
+            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, _dict_constructor
+        )
         loader.add_multi_constructor("!", intrinsics_multi_constructor)
-        return yaml.load(yamlstr, loader)
+        return yaml.safe_load(yamlstr, loader)
 
 
 class FlattenAliasDumper(yaml.SafeDumper):

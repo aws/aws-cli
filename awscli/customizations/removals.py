5bbbc3eb51464f6b25a9f82be582c2cb70dfeabd
aws-sdk-python-automation <github-aws-sdk-python-automation@amazon.com>
2026-06-17 18:07:53 +0000

    Merge customizations for Bedrock Agent Runtime

--- awscli/customizations/removals.py
+++ awscli/customizations/removals.py
@@ -81,6 +81,7 @@ def register_removals(event_handler):
     cmd_remover.remove(
         on_event='building-command-table.bedrock-agent-runtime',
         remove_commands=[
+            'agentic-retrieve-stream',
             'invoke-agent',
             'invoke-flow',
             'invoke-inline-agent',

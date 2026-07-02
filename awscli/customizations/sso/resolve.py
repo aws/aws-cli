f56861de43f9bfa6a4c582e41cb0f075294d1119
Andrew Asseily <77591070+AndrewAsseily@users.noreply.github.com>
2026-07-02 13:00:00 -0400

    Fix SSO login failure for China and EUSC partition URLs (#10465)
    * Add China partition domains to SSO start URL resolver allowlist
    
    * Add changelog entry
    
    * Add EUSC, and DualStack partition domains to SSO start URL resolver allowlist
    
    * Update changelog

--- awscli/customizations/sso/resolve.py
+++ awscli/customizations/sso/resolve.py
@@ -26,10 +26,18 @@ LOG = logging.getLogger(__name__)
 _AWS_OWNED_SUFFIXES = (
     '.app.aws',
     '.portal.amazonaws.com',
+    '.portal.amazonaws.com.cn',
+    '.app.amazonwebservices.com.cn',
+    '.portal.amazonaws.eu',
+    '.api.amazonwebservices.eu',
     '.awsapps.com',
+    '.awsapps.cn',
 )
 
-_AWS_OWNED_EXACT = 'identitycenter.amazonaws.com'
+_AWS_OWNED_EXACT = (
+    'identitycenter.amazonaws.com',
+    'identitycenter.amazonaws.com.cn',
+)
 
 _REGION_PATTERNS = (
     # {idcInstanceId}.portal.{region}.app.aws
@@ -39,6 +47,26 @@ _REGION_PATTERNS = (
     # {idcInstanceId}.{region}.portal.amazonaws.com
     re.compile(
         r'^[^.]+\.(?P<region>[a-z0-9-]+)\.portal\.amazonaws\.com$',
+        re.IGNORECASE,
+    ),
+    # {idcInstanceId}.{region}.portal.amazonaws.com.cn
+    re.compile(
+        r'^[^.]+\.(?P<region>[a-z0-9-]+)\.portal\.amazonaws\.com\.cn$',
+        re.IGNORECASE,
+    ),
+    # {idcInstanceId}.portal.{region}.app.amazonwebservices.com.cn
+    re.compile(
+        r'^[^.]+\.portal\.(?P<region>[a-z0-9-]+)\.app\.amazonwebservices\.com\.cn$',
+        re.IGNORECASE,
+    ),
+    # {idcInstanceId}.{region}.portal.amazonaws.eu
+    re.compile(
+        r'^[^.]+\.(?P<region>[a-z0-9-]+)\.portal\.amazonaws\.eu$',
+        re.IGNORECASE,
+    ),
+    # {idcInstanceId}.portal.{region}.api.amazonwebservices.eu
+    re.compile(
+        r'^[^.]+\.portal\.(?P<region>[a-z0-9-]+)\.api\.amazonwebservices\.eu$',
         re.IGNORECASE,
     ),
 )
@@ -61,7 +89,7 @@ def _normalize_url(url):
 
 def is_aws_owned_domain(hostname):
     hostname = hostname.lower().rstrip('.')
-    if hostname == _AWS_OWNED_EXACT:
+    if hostname in _AWS_OWNED_EXACT:
         return True
     for suffix in _AWS_OWNED_SUFFIXES:
         if hostname == suffix.lstrip('.'):
